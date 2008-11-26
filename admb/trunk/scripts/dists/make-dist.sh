fce5bb31518588b9b3f8185e8a54524361335305
johnoel <johnoel@6fd93cbc-6849-4d5d-bcfc-d3e1d932c95a>
2008-11-26 19:57:32 +0000

    Put dists into a build folder

--- admb/trunk/scripts/dists/make-dist.sh
+++ admb/trunk/scripts/dists/make-dist.sh
@@ -1,33 +1,35 @@
 #!/bin/bash
 
-VERSION=mytest
+VERSION=9.0.xrc3
 
 rm -vf *.zip
 
 if [ -e admb_gcc411_fedora8 ] ; then
   mv admb_gcc411_fedora8 admb-$VERSION-linux64-gcc3.4
-  cp -vf ../scripts/bash/mygcc* admb-$VERSION-linux64-gcc3.4/bin 
+  cp -vf ../../scripts/bash/mygcc* admb-$VERSION-linux64-gcc3.4/bin 
 fi
 if [ -e admb_vc8_win32 ] ; then
   mv admb_vc8_win32 admb-$VERSION-win32-vc8
-  cp -vf ../scripts/vc/*.bat admb-$VERSION-win32-vc8/bin
+  cp -vf ../../scripts/vc/*.bat admb-$VERSION-win32-vc8/bin
 fi
-
-#cp -vf scripts/bash/mygcc* admb-$VERSION-win32-mingw-gcc3.4/bin
-#cp -vf scripts/bash/mygcc* $DISK/bin 
-#if [ -e -d ] ; then
-#cp -vf scripts/borland/*.bat admb-$VERSION-win32-bcc5.5/bin
-#fi
+if [ -e admb_gcc345_mingw32 ] ; then
+  mv admb_gcc345_mingw32  admb-$VERSION-win32-mingw-gcc3.4
+  cp -vf ../../scripts/vc/*.bat admb-$VERSION-win32-mingw-gcc3.4/bin
+fi
+if [ -e admb_b502_win32 ] ; then
+  mv admb_b502_win32 admb-$VERSION-win32-bcc5.5
+  cp -vf ../../scripts/borland/*.bat admb-$VERSION-win32-bcc5.5/bin
+fi
 
 
 for DIRECTORY in admb* ; do
   printf "%s\n" $DIRECTORY
-  cp -vf ../README $DIRECTORY
-  cp -vf ../LICENSE $DIRECTORY
+  cp -vf ../../README $DIRECTORY
+  cp -vf ../../LICENSE $DIRECTORY
   mkdir -p $DIRECTORY/docs/manuals
-  #cp -vf ../docs/manuals/autodif.pdf $DIRECTORY/docs/manuals
-  #cp -vf ../docs/manuals/admb.pdf $DIRECTORY/docs/manuals
-  #cp -vf ../docs/manuals/admb-re.pdf $DIRECTORY/docs/manuals
+  cp -vf ../../docs/manuals/autodif/autodif.pdf $DIRECTORY/docs/manuals
+  cp -vf ../../docs/manuals/admb/admb.pdf $DIRECTORY/docs/manuals
+  cp -vf ../../docs/manuals/admb-re/admb-re.pdf $DIRECTORY/docs/manuals
 done
 
 find admb-$VERSION-* -exec touch {} . \;

11a75793512ea237eb6da99772093700ecf9b456
johnoel <johnoel@6fd93cbc-6849-4d5d-bcfc-d3e1d932c95a>
2008-12-21 08:07:40 +0000

    fix mingw build

--- admb/trunk/mingw.bat
+++ admb/trunk/mingw.bat
@@ -2,3 +2,5 @@ set GNUWIN32_HOME=C:\Program Files\GnuWin32
 set MINGW_HOME=C:\MinGW
 set CYGWIN_HOME=C:\Cygwin
 set PATH="%GNUWIN32_HOME%\bin";"%MINGW_HOME%\bin";"%CYGWIN_HOME%\bin";%PATH%
+path
+which make

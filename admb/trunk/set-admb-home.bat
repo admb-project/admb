e7e2531589c402db219cee058469e04e85e09fba
johnoel <johnoel@6fd93cbc-6849-4d5d-bcfc-d3e1d932c95a>
2008-12-21 10:41:38 +0000

    fix mingw build

--- admb/trunk/set-admb-home.bat
+++ admb/trunk/set-admb-home.bat
@@ -1,3 +1,4 @@
-set ADMB_HOME=C:\DOCUME~1\JOHNOEL\DESKTOP\ADMB-T~1\BUILD\DISTS\ADMB_G~1
+REM set ADMB_HOME=C:\DOCUME~1\JOHNOEL\DESKTOP\ADMB-T~1\BUILD\DISTS\ADMB_G~1
+set ADMB_HOME=C:\cygwin\home\johnoel\buildbot-slave-basedir\admb-mingwb\build\build\dists\admb_gcc345_mingw32
 set PATH=%ADMB_HOME%\bin;%PATH%
 path

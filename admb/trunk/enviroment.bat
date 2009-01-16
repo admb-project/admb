f945d4acc4f5cc270f282ba983a9f4b50ff14d49
johnoel <johnoel@6fd93cbc-6849-4d5d-bcfc-d3e1d932c95a>
2009-01-16 05:15:43 +0000

    Begin Inno admb-windows-setup for issue 30

---
+++ admb/trunk/enviroment.bat
@@ -0,0 +1,9 @@
+set BORLAND_HOME=C:\Borland\BCC55
+set CYGWIN_HOME=C:\cygwin
+set GNUWIN32_HOME=%ProgramFiles%\GNUWIN32
+set MINGW_HOME=C:\MinGW
+set INNOSETUP_HOME=%ProgramFiles%\Inno^ Setup^ 5
+
+echo %CD%
+
+PATH=%INNOSETUP_HOME%;%GNUWIN32_HOME%\bin;%CYGWIN_HOME%\bin;%PATH%

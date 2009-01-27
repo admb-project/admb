20146d0560db8f4b1c1e0f56393b3a5e99f92b25
johnoel <johnoel@6fd93cbc-6849-4d5d-bcfc-d3e1d932c95a>
2009-01-27 03:00:10 +0000

    add Windows SDK path to build and link files

--- admb/trunk/vc8-test.bat
+++ admb/trunk/vc8-test.bat
@@ -2,4 +2,5 @@ call "c:\Progra~1\Mid05a~1\VC\vcvarsall.bat"
 cd build\dists\admb_vc8_win32
 set ADMB_HOME=%CD%
 set PATH=%ADMB_HOME%\bin;%PATH%
+set MSSDK=c:\Progra~1\MI2578~1\Windows\v6.0A
 nmake
--- admb/trunk/vc8.bat
+++ admb/trunk/vc8.bat
@@ -1,3 +1,4 @@
 call "c:\Progra~1\Mid05a~1\VC\vcvarsall.bat"
 call "enviroment.bat"
+set MSSDK=c:\Progra~1\MI2578~1\Windows\v6.0A
 make vc

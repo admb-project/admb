3732cd2966d5543a73079d8242044a130e1ab2d4
johnoel <johnoel@6fd93cbc-6849-4d5d-bcfc-d3e1d932c95a>
2009-02-16 20:18:16 +0000

    fix vc targets in Makefile

--- admb/trunk/enviroment.bat
+++ admb/trunk/enviroment.bat
@@ -3,6 +3,4 @@ set GNUWIN32_HOME=%ProgramFiles%\GNUWIN32
 set MINGW_HOME=C:\MinGW
 set INNOSETUP_HOME=%ProgramFiles%\Inno^ Setup^ 5
 
-echo %CD%
-
 PATH=%GNUWIN32_HOME%\bin;%MINGW_HOME%\bin;%CYGWIN_HOME%\bin;%INNOSETUP_HOME%;%PATH%

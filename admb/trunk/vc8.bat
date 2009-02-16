d600be94959e62eb308a8fa3b56dd882dda5b861
johnoel <johnoel@6fd93cbc-6849-4d5d-bcfc-d3e1d932c95a>
2009-02-16 22:40:11 +0000

    fix vc8 and vc9 targets in Makefile

--- admb/trunk/vc8.bat
+++ admb/trunk/vc8.bat
@@ -1,4 +1,4 @@
 call "%VS80COMNTOOLS%vsvars32.bat"
 call "enviroment.bat"
 set MSSDK=%ProgramFiles%\Microsoft^ Platform^ SDK^ for^ Windows^ Server^ 2003^ R2
-make vc
+make vc8
--- admb/trunk/vc9.bat
+++ admb/trunk/vc9.bat
@@ -1,3 +1,3 @@
 call "%VS90COMNTOOLS%vsvars32.bat"
 call "enviroment.bat"
-make vc
+make vc9

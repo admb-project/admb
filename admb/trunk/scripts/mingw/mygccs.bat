5b3b1e91206c4980091f3985e3d1a13c40f64dfd
johnoel <johnoel@6fd93cbc-6849-4d5d-bcfc-d3e1d932c95a>
2009-04-07 21:55:24 +0000

    [Issue 33] Add -Wno-deprecated to end users options.

--- admb/trunk/scripts/mingw/mygccs.bat
+++ admb/trunk/scripts/mingw/mygccs.bat
@@ -3,6 +3,6 @@ rem if [ ! -n "$ADMB_HOME" ]; then
 rem     echo you must define the environment string ADMB_HOME
 rem else
 
-g++ %1.cpp -I. -fpermissive -I%ADMB_HOME%/include -L%ADMB_HOME%/lib -DUSE_LAPLACE -D__GNUDOS__ -Dlinux -O3 -ldf1b2s -ladmod -ladt -lads -ldf1b2s -ladmod -ladt -lads -o%1
+g++ -Wno-deprecated %1.cpp -I. -fpermissive -I%ADMB_HOME%/include -L%ADMB_HOME%/lib -DUSE_LAPLACE -D__GNUDOS__ -Dlinux -O3 -ldf1b2s -ladmod -ladt -lads -ldf1b2s -ladmod -ladt -lads -o%1
 
 rem fi

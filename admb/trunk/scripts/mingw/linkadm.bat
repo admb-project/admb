10d065f0c58ba684173f3d0dcbc8b048cfae72ea
johnoel <johnoel@6fd93cbc-6849-4d5d-bcfc-d3e1d932c95a>
2009-04-07 21:10:04 +0000

    [Issue 52] Added missing random effects batch files.

--- admb/trunk/scripts/mingw/linkadm.bat
+++ admb/trunk/scripts/mingw/linkadm.bat
@@ -1,7 +1,7 @@
 rem # linking in libpthread first because of multithreading problems
 rem # you may not want this
 
-gcc  -Xlinker -s -Xlinker -L"%ADMB_HOME%"/lib  $1.obj -o $1  -ladmod -ladt -lado -lado -lpthread -lstdc++ -ldl -lm
+gcc  -Xlinker -s -Xlinker -L /admodel/lib  $1.obj -o $1  -ladmod -ladt -lado -lado -lpthread -lstdc++ -ldl -lm
 
 rem # if not try removing the -lpthread as in
 rem # gcc -O3 -Xlinker -s -Xlinker -L /admodel/lib  $1.obj -o $1 -ladmod -ladt -lado -lstdc++ -ldl -lm

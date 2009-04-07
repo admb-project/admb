53e5e9b1a67b5bdbfa003906be4a96a47be2a004
johnoel <johnoel@6fd93cbc-6849-4d5d-bcfc-d3e1d932c95a>
2009-04-07 21:36:54 +0000

    Fix license header

--- admb/trunk/df1b2-separable/df1b2test.h
+++ admb/trunk/df1b2-separable/df1b2test.h
@@ -6,8 +6,8 @@
  * 
  * ADModelbuilder and associated libraries and documentations are
  * provided under the general terms of the "BSD" license.
- * License:
  *
+ * License:
  * 
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are

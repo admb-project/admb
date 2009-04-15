b841e61fa1a89a8a15bcc04619c3dfdcef71fed6
johnoel <johnoel@6fd93cbc-6849-4d5d-bcfc-d3e1d932c95a>
2009-04-15 01:23:18 +0000

    Put vc8 binaries in separate folder.

--- admb/trunk/vc8.bat
+++ admb/trunk/vc8.bat
@@ -1,4 +1,9 @@
 call "%VS80COMNTOOLS%vsvars32.bat"
 call "enviroment.bat"
 set MSSDK=%ProgramFiles%\Microsoft^ Platform^ SDK^ for^ Windows^ Server^ 2003^ R2
+
+set DISK=build/dists/admb_vc8_win32
+set CCVERSION=vc8
+set OSVERSION=win32
+
 make vc

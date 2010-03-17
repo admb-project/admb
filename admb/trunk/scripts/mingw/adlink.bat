0789b58abef1649db5bc53aa86ef49373f41f32a
arnima <arnima@6fd93cbc-6849-4d5d-bcfc-d3e1d932c95a>
2010-03-17 23:41:09 +0000

    Updated 'mingw' scripts: adcomp, adlink, and admb

--- admb/trunk/scripts/mingw/adlink.bat
+++ admb/trunk/scripts/mingw/adlink.bat
@@ -5,56 +5,63 @@ if [%1]==[-help] goto HELP
 if [%1]==[--help] goto HELP
 REM #######################################################################################################################
 REM                                                                                                                       #
-REM Script:   adlink [-d] [-r] [-s] model                                                                                 #
+REM Script:   adlink [-d] [-g] [-r] [-s] model                                                                            #
 REM                                                                                                                       #
-REM Purpose:  Link ADMB object code to executable, using the MinGW GCC 3.4.5 compiler                                     #
+REM Purpose:  Link ADMB object code to executable, using the MinGW GCC compiler                                           #
 REM                                                                                                                       #
 REM Args:     -d creates DLL                                                                                              #
-REM           -r creates ADMB-RE                                                                                          #
-REM           -s uses safe bounds and debugging symbols                                                                   #
-REM           model is the filename prefix, e.g. simple                                                                   #
+REM           -g inserts debugging symbols                                                                                #
+REM           -r creates ADMB-RE model                                                                                    #
+REM           -s creates model that enforces safe array bounds during runtime                                             #
+REM           model is the filename with or without extension, e.g. simple or simple.o                                    #
 REM                                                                                                                       #
 REM Requires: ADMB libraries, g++, dllwrap                                                                                #
 REM                                                                                                                       #
 REM Returns:  Creates executable or DLL with same prefix                                                                  #
 REM                                                                                                                       #
 REM History:  24 May 2009  Arni Magnusson created                                                                         #
+REM            7 Jul 2009  Arni Magnusson added support for MinGW GCC 4.4.0                                               #
+REM           27 Nov 2009  Arni Magnusson added support for filename extension, e.g. simple.o                             #
+REM            8 Feb 2010  Johnoel Ancheta split the -s combo option into separate -g and -s options                      #
+REM           22 Feb 2010  Arni Magnusson merged forked versions                                                          #
 REM                                                                                                                       #
 REM #######################################################################################################################
 
 rem Pop args until model=%1
-set re=0
-set linker=g++
-set s=-s
-set def=
-set df1b2lib=-ldf1b2stub
 set adlib=-lado
+set linker=g++
+set re=0
+set s=-s
 :STARTLOOP
 if [%2]==[] goto ENDLOOP
 if %1==-d set linker=dllwrap& shift
+if %1==-g set s=& shift
 if %1==-r set re=1& shift
-if %1==-s set s=& set adlib=-lads& shift
+if %1==-s set adlib=-lads& shift
 goto STARTLOOP
 :ENDLOOP
-if %linker%==g++ (set out=-o %1) else (set def=-def %1.def --driver-name g++& set out=--output-lib lib%1.a -o %1.dll)
+
+set model=%~n1
+
+set def=
+set df1b2lib=-ldf1b2stub
+if %linker%==g++ (set out=-o %model%) else (set def=-def %model%.def --driver-name g++& set out=--output-lib lib%model%.a -o %model%.dll)
 if %re%==1 if %adlib%==-lado set df1b2lib=-ldf1b2o
 if %re%==1 if %adlib%==-lads set df1b2lib=-ldf1b2s
 
-echo %linker% %s% %def% -L%ADMB_HOME%/lib %1.o %df1b2lib% -ladmod -ladt %adlib% %df1b2lib% -ladmod -ladt %adlib% %out%
-     %linker% %s% %def% -L"%ADMB_HOME%\lib" %1.o %df1b2lib% -ladmod -ladt %adlib% %df1b2lib% -ladmod -ladt %adlib% %out%
-
-REM g++ -w -o%1 -g %1.cpp -I. -fpermissive -I"%ADMB_HOME%\include" -L"%ADMB_HOME%\lib" -DUSE_LAPLACE -D__GNUDOS__ -Dlinux -DOPT_LIB -O3 -ldf1b2stub -ladmod -ladt -lado -ldf1b2stub -ladmod -ladt -lado
-
+echo %linker% %s% -static %def% -L%ADMB_HOME%/lib %model%.o %df1b2lib% -ladmod -ladt %adlib% %df1b2lib% -ladmod -ladt %adlib% %out%
+     %linker% %s% -static %def% -L%ADMB_HOME%/lib %model%.o %df1b2lib% -ladmod -ladt %adlib% %df1b2lib% -ladmod -ladt %adlib% %out%
 goto EOF
 
 :HELP
-echo Usage: adlink [-d] [-r] [-s] model
+echo Usage: adlink [-d] [-g] [-r] [-s] model
 echo.
-echo Link AD Model Builder object code to executable, using the MinGW GCC 3.4.5 compiler.
+echo Link AD Model Builder object code to executable, using the MinGW GCC compiler.
 echo.
 echo   -d     Create DLL
+echo   -g     Insert debugging symbols
 echo   -r     Create ADMB-RE
-echo   -s     Use safe bounds and debugging symbols
+echo   -s     Enforce safe bounds
 echo   model  Filename prefix, e.g. simple
 echo.
 

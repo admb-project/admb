aed32cbc7add2313e35c3fed15711e3a75c874c8
johnoel <johnoel@6fd93cbc-6849-4d5d-bcfc-d3e1d932c95a>
2010-04-16 00:25:17 +0000

    Fix compile problem for mingw ADMB2R test.

--- admb/trunk/scripts/mingw/adlink.bat
+++ admb/trunk/scripts/mingw/adlink.bat
@@ -36,7 +36,7 @@ set g=
 :STARTLOOP
 if [%2]==[] goto ENDLOOP
 if %1==-d set linker=dllwrap& shift
-if %1==-g set g=-g& shift
+if %1==-g set g=-g& set s=& shift
 if %1==-r set re=1& shift
 if %1==-s set adlib=-lads& shift
 goto STARTLOOP
@@ -49,7 +49,6 @@ set df1b2lib=-ldf1b2stub
 if %linker%==g++ (set out=-o %model%) else (set def=-def %model%.def --driver-name g++& set out=--output-lib lib%model%.a -o %model%.dll)
 if %adlib%==-lado set df1b2lib=-ldf1b2o
 if %adlib%==-lads set df1b2lib=-ldf1b2s
-if %g%==-g set s=
 
 echo on
 %linker% %s% -static %def% -L%ADMB_HOME%/lib %model%.o %df1b2lib% -ladmod -ladt %adlib% %df1b2lib% -ladmod -ladt %adlib% %out%

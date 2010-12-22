165bcda418c4fba3536afac1e14edfcac6e0b344
johnoel <johnoel@6fd93cbc-6849-4d5d-bcfc-d3e1d932c95a>
2010-12-22 16:15:04 +0000

    Remove ununsed batch files.

--- admb/trunk/scripts/borland/lnkadmb-re.bat
+++
@@ -1,1 +0,0 @@
-bcc32 -v -L%BCC55_HOME%\lib -L"%ADMB_HOME%"\lib %1.obj df1b2o.lib admod32.lib adt32.lib ado32.lib
--- admb/trunk/scripts/borland/makeadm.bat
+++
@@ -1,2 +0,0 @@
-tpl2cpp %1
-bcc32 -O2 -5 -H -DDOS386;OPT_LIB -I. -f %1.cpp admod32.lib orl32.lib ado32.lib

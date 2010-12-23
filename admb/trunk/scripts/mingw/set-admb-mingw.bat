21d12a79fb8e82d4ca26ac82ec042e6862b56821
johnoel <johnoel@6fd93cbc-6849-4d5d-bcfc-d3e1d932c95a>
2010-12-23 13:46:39 +0000

    Adding in mingw.

--- admb/trunk/scripts/mingw/set-admb-mingw.bat
+++ admb/trunk/scripts/mingw/set-admb-mingw.bat
@@ -19,7 +19,8 @@ REM
 REM
 REM Adds the bin folder to ADMB_HOME to system path.
 REM
-@set PATH=.;%ADMB_HOME%\bin;%GNUWIN32_HOME%\bin;%MINGW_HOME%\bin;%PATH%
+REM set PATH=.;"%ADMB_HOME%"\bin;%GNUWIN32_HOME%\bin;%MINGW_HOME%\bin;%PATH%
+@set PATH=.;"%ADMB_HOME%"\bin;%MINGW_HOME%\bin;%PATH%
 
 @echo Set ADMB Home directory to %ADMB_HOME%
 

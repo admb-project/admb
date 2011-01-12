7ae52e0254eae453c85d4f8e956c10c43ebfb5d3
johnoel <johnoel@6fd93cbc-6849-4d5d-bcfc-d3e1d932c95a>
2011-01-12 23:59:02 +0000

    Update copyright to include 2011

--- admb/trunk/src/tools99/mycif.h
+++ admb/trunk/src/tools99/mycif.h
@@ -2,7 +2,7 @@
  * $Id$
  *
  * Author: David Fournier
- * Copyright (c) 2008, 2009, 2010 Regents of the University of California
+ * Copyright (c) 2008-2011 Regents of the University of California 
  * 
  * ADModelbuilder and associated libraries and documentations are
  * provided under the general terms of the "BSD" license.
--- admb/trunk/src/tools99/strcase2.cpp
+++ admb/trunk/src/tools99/strcase2.cpp
@@ -2,7 +2,7 @@
  * $Id$
  *
  * Author: John Sibert
- * Copyright (c) 2010 ADMB Foundation
+ * Copyright (c) 2010-2011 ADMB Foundation
  */
 #include "adstring.hpp"
 

6afa95fd7497849aba07eb85571261eabc307013
stevenmartell <stevenmartell@6fd93cbc-6849-4d5d-bcfc-d3e1d932c95a>
2011-07-14 23:10:46 +0000

    Contributed libraries from Steve Martell --directory contrib/statslib

---
+++ trunk/contrib/statslib/statsLib.h
@@ -0,0 +1,90 @@
+/*
+	TODO class object for an age-length key
+	TODO class object for a length transition matrix
+*/
+
+#ifndef ADMODEL_H
+#define ADMODEL_H
+	#include <admodel.h>
+#endif
+
+/**
+ * pi is a constant
+**/
+const double pi=3.1415926535897932384626433832795;
+
+#ifndef DNORM_H
+#define DNORM_H
+	#include "dnorm.cpp"
+#endif
+
+#ifndef DLNORM_H
+#define DLNORM_H
+	#include "dlnorm.cpp"
+#endif
+
+#ifndef DBINOM_H
+#define DBINOM_H
+	#include "dbinom.cpp"
+#endif
+
+#ifndef DNBINOM_H
+#define DNBINOM_H
+	#include "dnbinom.cpp"
+#endif
+
+#ifndef DPOIS_H
+#define DPOIS_H
+	#include "dpois.cpp"
+#endif
+
+#ifndef DGAMMA_H
+#define DGAMMA_H
+	#include "dgamma.cpp"
+#endif
+
+#ifndef LOGIS_H
+#define LOGIS_H
+	#include "logistic.cpp"
+#endif
+
+#ifndef DBETA_H
+#define DBETA_H
+	#include "dbeta.cpp"
+#endif
+
+#ifndef AGELENKEY_H
+#define AGELENKEY_H
+	#include "alk.cpp"
+#endif
+	
+#ifndef FILL_H
+#define FILL_H
+	#include "fill.cpp"
+#endif
+
+#ifndef BARANOV_H
+#define BARANOV_H
+	#include "baranov.cpp"
+#endif
+
+/**
+* @file statsLib.h
+* @brief Library of statistic functions
+* 
+* This library contains numerous statistical functions
+* that can easily be used in ADMB templates. This is the
+* header file that contains preprocessor directive to 
+* include all of the necessary files into the library.
+* 
+* @author Steven Martell
+* 
+* @date 2/05/2011
+*
+*
+*
+*
+*
+*/
+
+

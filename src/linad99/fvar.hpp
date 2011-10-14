782e35b8e9a2e3872912f877b1deff68d6a19ea6
casper <casper@6fd93cbc-6849-4d5d-bcfc-d3e1d932c95a>
2011-10-14 10:30:11 +0000

    Added function definition for solve(dvar_matrix,dvar_matrix) to fvar.hpp such that the function can be called from outside expm.cpp - Useful for inverting matrices in random effects programs.

--- src/linad99/fvar.hpp
+++ src/linad99/fvar.hpp
@@ -5085,6 +5085,7 @@ dmatrix choleski_decomp_positive(const dmatrix& MM,const int& ierr);
 dmatrix choleski_decomp_positive(const dmatrix& MM,double bound);
 dvar_matrix choleski_decomp(_CONST dvar_matrix& M);
 
+dvar_matrix solve(const dvar_matrix& aa, const dvar_matrix& zz);
 dmatrix expm(const dmatrix & A);
 dvar_matrix expm(const dvar_matrix & A);
 

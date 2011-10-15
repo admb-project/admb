3d834e370767eaa645048e75b2ff72a5ab01e6c9
johnoel <johnoel@6fd93cbc-6849-4d5d-bcfc-d3e1d932c95a>
2011-10-15 00:14:06 +0000

    Prepend 10.1 instead of 10.0

--- trunk/scripts/getVersionInfo.sh
+++ trunk/scripts/getVersionInfo.sh
@@ -3,4 +3,4 @@
 # svn stat -v $1 | sed -n 's/ˆ[ A-Z?\*|!]\{1,15\}/r/;s/ \{1,15\}/\/r/;s/ .*//p' 
 # invoking as getVersionInfo.sh linad99/fvar.hpp prepends 10.0 to the revision
 # for fvar.hpp
-svn stat -v $1 | sed -n 's/ˆ[ A-Z?\*|!]\{1,15\}/r/;s/ \{1,15\}/10.0./;s/ .*//p' 
+svn stat -v $1 | sed -n 's/ˆ[ A-Z?\*|!]\{1,15\}/r/;s/ \{1,15\}/10.1./;s/ .*//p' 

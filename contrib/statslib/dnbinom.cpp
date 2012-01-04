88ca51a134c7fb5380810e486dc4756912d28748
mbrooks <mbrooks@6fd93cbc-6849-4d5d-bcfc-d3e1d932c95a>
2012-01-04 19:14:52 +0000

    Combined dnbinom.cpp and dnbinom_eco.cpp, all now named dnbinom. Added dzinbinom.cpp zero inflated negative binomial negative log likelihood.

--- contrib/statslib/dnbinom.cpp
+++ contrib/statslib/dnbinom.cpp
@@ -1,11 +1,13 @@
 #include "statsLib.h"
+#include <df1b2fun.h>
+#include <adrndeff.h> 
 
 /**
 * 
 * \file dnbinom.cpp
 * \brief Negative binomial density functions.
 * \ingroup STATLIB
-* \author Steven Martell
+* \author Steven Martell and Mollie Brooks
 * \date 2/05/2011
 * 
 * 
@@ -40,7 +42,7 @@
 	\sa
 **/
 
-dvariable dnbinom(const double& x,const prevariable& size, const prevariable& mu)
+dvariable dnbinom(const double& x,const prevariable& mu, const prevariable& size)
 {
 	if (value(size)<0.0)
 	{
@@ -57,5 +59,187 @@ dvariable dnbinom(const double& x,const prevariable& size, const prevariable& mu
 	return(-tmp);
 }
 
+/** negative log likelihood of negative binomial with mean and size 
+\brief Negative binomial with size and mean
+\author Mollie Brooks
+\param x observed count
+\param mu is the predicted mean
+\param k is the overdispersion parameter, i.e. shape parameter of underlying heterogeneity (different from tau). should be >0
+\return negative log likelihood \f$ -( \ln(\Gamma(x+k))-\ln(\Gamma(k))-\ln(x!)+k\ln(k)+x\ln(\mu)-(k+x)\ln(k+\mu) )\f$
+**/
+df1b2variable dnbinom(const double& x, const df1b2variable& mu, const df1b2variable& k)
+{
+	//x is the observed count
+	//mu is the predicted mean
+	//k is the overdispersion parameter
+	if (value(k)<0.0)
+	{
+		cerr<<"k is <=0.0 in dnbinom()";
+		return(0.0);
+	}
+	RETURN_ARRAYS_INCREMENT();
+	df1b2variable loglike;
+
+	loglike = gammln(k+x)-gammln(k)-gammln(x+1)+k*log(k)-k*log(mu+k)+x*log(mu)-x*log(mu+k);
+
+	RETURN_ARRAYS_DECREMENT();
+	return(-loglike);
+}
+///** negative log likelihood of negative binomial with mean and size 
+//\brief Negative binomial with size and mean
+//\author Mollie Brooks
+//\param x observed count
+//\param mu is the predicted mean
+//\param k is the overdispersion parameter, i.e. shape parameter of underlying heterogeneity (different from tau). should be >0
+//\return negative log likelihood \f$ -( \ln(\Gamma(x+k))-\ln(\Gamma(k))-\ln(x!)+k\ln(k)+x\ln(\mu)-(k+x)\ln(k+\mu) )\f$
+//**/
+//dvariable dnbinom(const double& x, const prevariable& mu, const prevariable& k)
+//{
+//	//x is the observed count
+//	//mu is the predicted mean
+//	//k is the overdispersion parameter
+//	if (value(k)<0.0)
+//	{
+//		cerr<<"k is <=0.0 in dnbinom()";
+//		return(0.0);
+//	}
+//	RETURN_ARRAYS_INCREMENT();
+//	dvariable loglike;
+//
+//	loglike = gammln(k+x)-gammln(k)-gammln(x+1)+k*log(k)-k*log(mu+k)+x*log(mu)-x*log(mu+k);
+//
+//	RETURN_ARRAYS_DECREMENT();
+//	return(-loglike);
+//}
+
+/** negative log likelihood of negative binomial with mean and size 
+\brief Negative binomial with size and mean
+\author Mollie Brooks
+\param x observed counts
+\param mu is the predicted mean
+\param k is the overdispersion parameter, i.e. shape parameter of underlying heterogeneity (different from tau). should be >0
+\return negative log likelihood \f$ -( \ln(\Gamma(x+k))-\ln(\Gamma(k))-\ln(x!)+k\ln(k)+x\ln(\mu)-(k+x)\ln(k+\mu) )\f$
+**/
+df1b2variable dnbinom(const dvector& x, const df1b2vector& mu, const df1b2variable& k)
+{
+	//the observed counts are in x
+	//mu is the predicted mean
+	//k is the overdispersion parameter
+	if (value(k)<0.0)
+	{
+		cerr<<"k is <=0.0 in dnbinom()";
+		return(0.0);
+	}
+	RETURN_ARRAYS_INCREMENT();
+	int i,imin,imax;
+	imin=x.indexmin();
+	imax=x.indexmax();
+	df1b2variable loglike;
+	loglike=0.;
+	for(i = imin; i<=imax; i++)
+	{
+		loglike += gammln(k+x(i))-gammln(k)-gammln(x(i)+1)+k*log(k)-k*log(mu(i)+k)+x(i)*log(mu(i))-x(i)*log(mu(i)+k);
+	}
+	RETURN_ARRAYS_DECREMENT();
+	return(-loglike);
+}
+/** negative log likelihood of negative binomial with mean and size 
+\brief Negative binomial with size and mean
+\author Mollie Brooks
+\param x observed counts
+\param mu is the predicted mean
+\param k is the overdispersion parameter, i.e. size, i.e. shape parameter of underlying heterogeneity (different from tau). should be >0
+\return negative log likelihood \f$ -( \ln(\Gamma(x+k))-\ln(\Gamma(k))-\ln(x!)+k\ln(k)+x\ln(\mu)-(k+x)\ln(k+\mu) )\f$
+**/
+df1b2variable dnbinom(const dvector& x, const df1b2vector& mu, const df1b2vector& k)
+{
+	//the observed counts are in x
+	//mu is the predicted mean
+	//k is the overdispersion parameter
+
+	RETURN_ARRAYS_INCREMENT();
+	int i,imin,imax;
+	imin=x.indexmin();
+	imax=x.indexmax();
+	df1b2variable loglike;
+	loglike=0.;
+ 
+	for(i = imin; i<=imax; i++)
+	{
+		if (value(k(i))<0.0)
+		{
+			cerr<<"k("<<i<<") is <=0.0 in dnbinom()";
+			return(0.0);
+		}
+		loglike += gammln(k(i)+x(i))-gammln(k(i))-gammln(x(i)+1)+k(i)*log(k(i))-k(i)*log(mu(i)+k(i))+x(i)*log(mu(i))-x(i)*log(mu(i)+k(i));
+	}
+	RETURN_ARRAYS_DECREMENT();
+	return(-loglike);
+}
+/** negative log likelihood of negative binomial with mean and size 
+\brief Negative binomial with size and mean
+\author Mollie Brooks
+\param x observed counts
+\param mu is the predicted mean
+\param k is the overdispersion parameter, i.e. size, i.e. shape parameter of underlying heterogeneity (different from tau). should be >0
+\return negative log likelihood \f$ -( \ln(\Gamma(x+k))-\ln(\Gamma(k))-\ln(x!)+k\ln(k)+x\ln(\mu)-(k+x)\ln(k+\mu) )\f$
+**/
+
+dvariable dnbinom(const dvector& x, const dvar_vector& mu, const prevariable& k)
+{
+	//the observed counts are in x
+	//mu is the predicted mean
+	//k is the overdispersion parameter
+	if (value(k)<0.0)
+	{
+		cerr<<"k is <=0.0 in dnbinom()";
+		return(0.0);
+	}
+	RETURN_ARRAYS_INCREMENT();
+	int i,imin,imax;
+	imin=x.indexmin();
+	imax=x.indexmax();
+	dvariable loglike = 0.;
+
+	for(i = imin; i<=imax; i++)
+	{
+		loglike += gammln(k+x(i))-gammln(k)-gammln(x(i)+1)+k*log(k)-k*log(mu(i)+k)+x(i)*log(mu(i))-x(i)*log(mu(i)+k);
+	}
+	RETURN_ARRAYS_DECREMENT();
+	return(-loglike);
+}
+/** negative log likelihood of negative binomial with mean and size 
+\brief Negative binomial with size and mean
+\author Mollie Brooks
+\param x observed counts
+\param mu is the predicted mean
+\param k is the overdispersion parameter, i.e. size, i.e. shape parameter of underlying heterogeneity (different from tau). should be >0
+\return negative log likelihood \f$ -( \ln(\Gamma(x+k))-\ln(\Gamma(k))-\ln(x!)+k\ln(k)+x\ln(\mu)-(k+x)\ln(k+\mu) )\f$
+**/
+dvariable dnbinom(const dvector& x, const dvar_vector& mu, const dvar_vector& k)
+{
+	//the observed counts are in x
+	//mu is the predicted mean
+	//k is the overdispersion parameter
+	RETURN_ARRAYS_INCREMENT();
+	int i,imin,imax;
+	imin=x.indexmin();
+	imax=x.indexmax();
+	dvariable loglike;
+	loglike=0.;
+
+	for(i = imin; i<=imax; i++)
+	{
+		if (value(k(i))<0.0)
+		{
+			cerr<<"k("<<i<<") is <=0.0 in dnbinom()";
+			return(0.0);
+		}
+		
+		loglike += gammln(k(i)+x(i))-gammln(k(i))-gammln(x(i)+1)+k(i)*log(k(i))-k(i)*log(mu(i)+k(i))+x(i)*log(mu(i))-x(i)*log(mu(i)+k(i));
+	}
+	RETURN_ARRAYS_DECREMENT();
+	return(-loglike);
+}
 
 

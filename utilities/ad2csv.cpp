8e9306d71c0993960c346ab08ca7f3f7f275ff79
arnima <arnima@6fd93cbc-6849-4d5d-bcfc-d3e1d932c95a>
2012-03-16 21:24:20 +0000

    Add build instructions to utilities/ad2csv.cpp

--- utilities/ad2csv.cpp
+++ utilities/ad2csv.cpp
@@ -5,7 +5,9 @@
 //            (1) Converts file without user intervention
 //            (2) CSV lines do not end with a comma
 //          Binary files in ADMB include .cov, .hes, and .psv
-// History: 2012-03-13 arnima added -s and -t options
+//          To build the ad2csv executable: adcomp ad2csv
+// History: 2012-03-16 arnima added build instructions
+//          2012-03-13 arnima added -s and -t options
 //          2011-11-03 arnima fixed stream construction bug affecting GCC
 //          2003-08-30 arnima simplified UI and output
 //          1993-01-01 davef created

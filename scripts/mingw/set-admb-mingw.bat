90e2f98881ca38609ea9e03e2c3a7637b165fb4e
johnoel <johnoel@6fd93cbc-6849-4d5d-bcfc-d3e1d932c95a>
2012-06-29 01:57:31 +0000

    Add qoutes in case of spaces in folders.

--- scripts/mingw/set-admb-mingw.bat
+++ scripts/mingw/set-admb-mingw.bat
@@ -8,19 +8,13 @@ REM
 REM
 REM
 REM
-REM @set GNUWIN32_HOME=%ProgramFiles%\GNUWIN32
-
-REM
-REM
-REM
 @set MINGW_HOME=C:\MinGW
 
 
 REM
 REM Adds the bin folder to ADMB_HOME to system path.
 REM
-REM set PATH=.;"%ADMB_HOME%"\bin;%GNUWIN32_HOME%\bin;%MINGW_HOME%\bin;%PATH%
-@set PATH=.;"%ADMB_HOME%"\bin;%MINGW_HOME%\bin;%PATH%
+@set PATH=.;"%ADMB_HOME%"\bin;"%ADMB_HOME%"\utilities;%MINGW_HOME%\bin;%PATH%
 @echo Setting ADMB Home enviroment to "%ADMB_HOME%".
 
 @cd "%USERPROFILE%"

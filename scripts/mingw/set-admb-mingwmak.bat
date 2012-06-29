3900aa8e9c04be7bef9a4ad19e3b995da112bac4
johnoel <johnoel@6fd93cbc-6849-4d5d-bcfc-d3e1d932c95a>
2012-06-29 01:22:05 +0000

    Make mingw easier by including the compiler with installer.

--- scripts/mingw/set-admb-mingwmak.bat
+++ scripts/mingw/set-admb-mingwmak.bat
@@ -6,21 +6,9 @@ REM
 @set ADMB_HOME=%CD%
 
 REM
-REM
-REM
-REM @set GNUWIN32_HOME=%ProgramFiles%\GNUWIN32
-
-REM
-REM
-REM
-@set MINGW_HOME=C:\MinGW
-
-
-REM
 REM Adds the bin folder to ADMB_HOME to system path.
 REM
-REM set PATH=.;"%ADMB_HOME%"\bin;%GNUWIN32_HOME%\bin;%MINGW_HOME%\bin;%PATH%
-@set PATH=.;"%ADMB_HOME%"\bin;%MINGW_HOME%\bin;%PATH%
+@set PATH=.;"%ADMB_HOME%"\bin;%ADMB_HOME%\utilities;%ADMB_HOME%\MinGW-gcc4.6\bin;%PATH%
 @echo Setting ADMB Home enviroment to "%ADMB_HOME%".
 
 @cd "%USERPROFILE%"

7105059524cb77424e6b12625c5c1c253c17ecfc
johnoel <johnoel@6fd93cbc-6849-4d5d-bcfc-d3e1d932c95a>
2012-07-04 01:15:06 +0000

    Remove again.

--- tags/admb-11/contrib/ecolib/test/compare.R
+++
@@ -1,6 +0,0 @@
-r_results=scan("results_from_R.txt")
-ADMB_results=scan("results_from_ADMB.txt", nmax=length(r_results))
-
-r_results[which.max(abs((r_results- ADMB_results)/r_results))]
-ADMB_results[which.max(abs((r_results- ADMB_results)/r_results))]
-

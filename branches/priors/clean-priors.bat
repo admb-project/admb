5f980a3eaae44acad6004d4b4d372167746abf43
johnoel <johnoel@6fd93cbc-6849-4d5d-bcfc-d3e1d932c95a>
2012-07-10 20:01:11 +0000

    Already completed and merged to main trunk.

--- branches/priors/clean-priors.bat
+++
@@ -1,9 +0,0 @@
-del %ADMB32_HOME%\include\priorsLib.h
-
-del %ADMB32_HOME%\lib\priors.lib
-
-pushd "%~dp0"
-cd %ADMB32_HOME%
-del /q ..\objects\priors-slp\*.*
-del /q ..\objects\priors-olp\*.*
-popd
--- branches/priors/clean-stats.bat
+++
@@ -1,9 +0,0 @@
-del %ADMB32_HOME%\include\statsLib.h
-
-del %ADMB32_HOME%\lib\stats.lib
-
-pushd "%~dp0"
-cd %ADMB32_HOME%
-del /q ..\objects\stats-slp\*.*
-del /q ..\objects\stats-olp\*.*
-popd

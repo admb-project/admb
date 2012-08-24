d35a0ac0f7dc67db5bcd9f7f8f8a660575a54e51
johnoel <johnoel@6fd93cbc-6849-4d5d-bcfc-d3e1d932c95a>
2012-08-24 19:45:44 +0000

    Fix broken build.

--- trunk/scripts/cl/adlink.bat
+++ trunk/scripts/cl/adlink.bat
@@ -22,7 +22,7 @@ if not defined libs set libs="df1b2o.lib admod32.lib ado32.lib adt32.lib contrib
 set LIBPATH_MSSDK=/libpath:"%MSSDK%"\lib
 
 @echo on
-cl !objs! !libs! /link /libpath:"%ADMB_HOME%"\lib /libpath:"%ADMB_HOME%"\contrib
+cl %objs% %libs% /link /libpath:"%ADMB_HOME%"\lib /libpath:"%ADMB_HOME%"\contrib
 @echo off
 
 goto EOF

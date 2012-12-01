f803834aa8801ef6b28cb6a632022ba762dd5ea0
stevenmartell <stevenmartell@6fd93cbc-6849-4d5d-bcfc-d3e1d932c95a>
2012-12-01 23:19:50 +0000

    vectorized versions of dbinom in statsLib (request from Bolker)

--- trunk/contrib/statslib/dbinom.cpp
+++ trunk/contrib/statslib/dbinom.cpp
@@ -94,3 +94,106 @@ dvariable dbinom(const prevariable& x,const prevariable& n,const prevariable& p)
 	RETURN_ARRAYS_DECREMENT();
 	return(tmp);
 }
+
+/** 
+	\author Steven James Dean Martell
+	\date 2011-06-21
+	\param  x is a vector of the number of sucesses
+	\param  n is vector for the number of trials
+	\param  p is a vector for the binomial probability
+	\return returns the negative loglikelihood of the normal distribution
+	\sa
+**/
+dvariable dbinom(const dvar_vector& x,const dvar_vector& n,const dvar_vector& p)
+{
+	if(min(p)<=0. && max(p)>=1.0)
+	{
+		cerr<<"The probability p must be greater than 0 or less than 1 in "
+			"dbinom(const prevariable& x,const double& n,const double& p)\n";
+		return(0);
+	}
+	
+	int i;
+	int imin = x.indexmin();
+	int imax = x.indexmax();
+	
+	RETURN_ARRAYS_INCREMENT();
+	dvariable tmp = 0;
+	for(i=imin; i<=imax;i++)
+	{
+		tmp += log_comb(n(i),x(i))-x(i)*log(p(i))-(n(i)-x(i))*log(1.-p(i));
+	}
+	
+	RETURN_ARRAYS_DECREMENT();
+	return(tmp);
+}
+
+dvariable dbinom(const prevariable& x,const dvar_vector& n,const dvar_vector& p)
+{
+	if(min(p)<=0. && max(p)>=1.0)
+	{
+		cerr<<"The probability p must be greater than 0 or less than 1 in "
+			"dbinom(const prevariable& x,const double& n,const double& p)\n";
+		return(0);
+	}
+	
+	int i;
+	int imin = n.indexmin();
+	int imax = n.indexmax();
+	
+	RETURN_ARRAYS_INCREMENT();
+	dvariable tmp = 0;
+	for(i=imin; i<=imax;i++)
+	{
+		tmp += log_comb(n(i),x)-x*log(p(i))-(n(i)-x)*log(1.-p(i));
+	}
+	
+	RETURN_ARRAYS_DECREMENT();
+	return(tmp);
+}
+dvariable dbinom(const dvar_vector& x,const prevariable& n,const dvar_vector& p)
+{
+	if(min(p)<=0. && max(p)>=1.0)
+	{
+		cerr<<"The probability p must be greater than 0 or less than 1 in "
+			"dbinom(const prevariable& x,const double& n,const double& p)\n";
+		return(0);
+	}
+	
+	int i;
+	int imin = x.indexmin();
+	int imax = x.indexmax();
+	
+	RETURN_ARRAYS_INCREMENT();
+	dvariable tmp = 0;
+	for(i=imin; i<=imax;i++)
+	{
+		tmp += log_comb(n,x(i))-x(i)*log(p(i))-(n-x(i))*log(1.-p(i));
+	}
+	
+	RETURN_ARRAYS_DECREMENT();
+	return(tmp);
+}
+dvariable dbinom(const dvar_vector& x,const dvar_vector& n,const prevariable& p)
+{
+	if(p<=0. && p>=1.0)
+	{
+		cerr<<"The probability p must be greater than 0 or less than 1 in "
+			"dbinom(const prevariable& x,const double& n,const double& p)\n";
+		return(0);
+	}
+	
+	int i;
+	int imin = x.indexmin();
+	int imax = x.indexmax();
+	
+	RETURN_ARRAYS_INCREMENT();
+	dvariable tmp = 0;
+	for(i=imin; i<=imax;i++)
+	{
+		tmp += log_comb(n(i),x(i))-x(i)*log(p)-(n(i)-x(i))*log(1.-p);
+	}
+	
+	RETURN_ARRAYS_DECREMENT();
+	return(tmp);
+}

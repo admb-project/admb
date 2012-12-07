7d311cfcaca0c339b46741e9f18e1b7608667e6b
johnoel <johnoel@6fd93cbc-6849-4d5d-bcfc-d3e1d932c95a>
2012-12-07 01:28:58 +0000

    Add some code for c++11 threading.

--- tests/threaded/tests_threaded.cpp
+++ tests/threaded/tests_threaded.cpp
@@ -1,3 +1,5 @@
+#include <iostream>
+#include <thread>
 #include <gtest/gtest.h>
 
 class tests_threaded: public ::testing::Test 
@@ -26,6 +28,25 @@ TEST(tests_threaded, main4)
   main4();
 }
 */
+
+void call_from_thread(int tid) 
+{
+  std::cout << __FILE__ << ':' << __LINE__ << ' ' << tid << std::endl;
+}
+
+TEST(tests_threaded, example)
+{
+  static const int num_threads = 10;
+  std::thread t[num_threads];
+  for (int i = 0; i < num_threads; ++i) 
+  {
+    t[i] = std::thread(call_from_thread, i);
+  }
+  for (int i = 0; i < num_threads; ++i) 
+  {
+    t[i].join();
+  }
+}
 
 int main(int argc, char** argv)
 {

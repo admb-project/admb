00a0ffdc3f47d392eb15668afafb32b94fcbf93a
johnoel <johnoel@6fd93cbc-6849-4d5d-bcfc-d3e1d932c95a>
2013-02-23 01:11:00 +0000

    Fix broken build.

--- branches/threaded2/src/nh99/model7.cpp
+++ branches/threaded2/src/nh99/model7.cpp
@@ -595,7 +595,6 @@ void add_slave_suffix(const adstring& _tmpstring)
   }
 }
 
-/*
 #else if defined(USE_PTHREADS)
 
 void add_slave_suffix(const adstring& _tmpstring)
@@ -616,5 +615,4 @@ void add_slave_suffix(const adstring& _tmpstring)
     }
   }
 }
-*/
 #endif

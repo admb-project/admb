c9032f97fda4926ba5e3aa7fdca4adddde2498a4
johnoel <johnoel@6fd93cbc-6849-4d5d-bcfc-d3e1d932c95a>
2013-04-12 18:46:34 +0000

    If already installed, then just try to update.

--- trunk/utilities/get-mingw.bat
+++ trunk/utilities/get-mingw.bat
@@ -1,1 +1,5 @@
-pushd utilities & make --directory=mingw & popd
+IF NOT EXIST utilities\mingw\bin\gcc.exe (
+pushd utilities & make --directory=mingw install & popd
+) ELSE (
+pushd utilities & make --directory=mingw update & popd
+)

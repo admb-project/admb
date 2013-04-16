ff03bd6f303e7acc9f3d06959f196246087d1ef9
johnoel <johnoel@6fd93cbc-6849-4d5d-bcfc-d3e1d932c95a>
2013-04-16 00:09:44 +0000

    Fix up admb script to set admb_home correctly.

---
+++ trunk/scripts/admb/root-admb.bat
@@ -0,0 +1,20 @@
+@echo off
+
+setlocal EnableExtensions EnableDelayedExpansion
+
+for %%a in (%0.bat) do (
+  set HAS_PATH=%%~$PATH:a
+  if not "!HAS_PATH!"=="" (
+    set ADMB_PATH="%%~dp$PATH:a"
+  ) else (
+    set ADMB_PATH=%%~dpa
+  )
+  if exist !ADMB_PATH!build\dist\bin\admb.bat (
+    set ADMB_HOME=!ADMB_PATH!build\dist
+  ) else (
+    pushd !ADMB_PATH!\..
+    set ADMB_HOME=!CD!
+    popd
+  )
+  call !ADMB_HOME!\bin\admb.bat %*
+)

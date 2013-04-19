e352db9e37e47cb0f313f0519a0be9e8200b80a5
johnoel <johnoel@6fd93cbc-6849-4d5d-bcfc-d3e1d932c95a>
2013-04-19 19:57:19 +0000

    Update tests

--- trunk/tests/adstring/main.cpp
+++ trunk/tests/adstring/main.cpp
@@ -1,10 +1,12 @@
 #include <gtest/gtest.h>
 
 class test_adstring: public ::testing::Test {};
+class test_fmm_control: public ::testing::Test {};
 
 #include <fvar.hpp>
 #include <adstring.hpp>
-#include "tools99_strcase.cpp"
+#include "test_adstring_strcase.cpp"
+#include "test_fmm_control.cpp"
 
 int main(int argc, char** argv)
 {

a784e108689c6d1c7c02e39f03c26da386f90f37
cgrandin <cgrandin@6fd93cbc-6849-4d5d-bcfc-d3e1d932c95a>
2013-06-19 19:49:52 +0000

    Further code changes and doxygen documentation for contrib/gdbprintlib

--- contrib/gdbprintlib/gdbprintlib.cpp
+++ contrib/gdbprintlib/gdbprintlib.cpp
@@ -1,271 +1,537 @@
 #include "gdbprintlib.h"
 
-void pad(const adstring & v)
-{
-  cout << v << endl;
-}
-
-void pad(const dvar3_array & v)
-{
-  cout << v << endl;
-}
-
-void pad(const dvar3_array & v,int i)
-{
-  cout << v(i) << endl;
-}
-
-void pad(const dvar3_array & v,int i,int j)
-{
-  cout << v(i,j) << endl;
-}
-
-void pad(const dvar4_array & v)
-{
-  cout << v << endl;
-}
-
-void pad(const dvar4_array & v,int i)
-{
-  cout << v(i) << endl;
-}
-
-void pad(const dvar4_array & v,int i,int j)
-{
-  cout << v(i,j) << endl;
-}
-
-void pad(const dvar4_array & v,int i,int j,int k)
-{
-  cout << v(i,j,k) << endl;
-}
-
-void pad(const d4_array & v,int i,int j,int k)
-{
-  cout << v(i,j,k) << endl;
-}
-
-void pad(const d4_array & v)
-{
-  cout << v << endl;
-}
-
-void pad(const d4_array & v,int i)
-{
-  cout << v(i) << endl;
-}
-
-void pad(const d4_array & v,int i,int j)
-{
-  cout << v(i,j) << endl;
-}
-
-void pad(const dvar3_array & v,int i,int j,int k)
-{
-  cout << v(i,j,k) << endl;
-}
-
-void pad(const d3_array & v)
-{
-  cout << v << endl;
-}
-
-void pad(const d3_array & v,int i)
-{
-  cout << v(i) << endl;
-}
-
-void pad(const d3_array & v,int i,int j)
-{
-  cout << v(i,j) << endl;
-}
-
-void pad(const d3_array & v,int i,int j,int k)
-{
-  cout << v(i,j,k) << endl;
-}
-
-void pad(const i3_array & v)
-{
-  cout << v << endl;
-}
-
-void pad(const i3_array & v,int i)
-{
-  cout << v(i) << endl;
-}
-
-void pad(const i3_array & v,int i,int j)
-{
-  cout << v(i,j) << endl;
-}
-
-void pad(const i3_array & v,int i,int j,int k)
-{
-  cout << v(i,j,k) << endl;
-}
-
-void pad(const i4_array & v)
-{
-  cout << v << endl;
-}
-
-void pad(const i4_array & v,int i)
-{
-  cout << v(i) << endl;
-}
-void pad(const i4_array & v,int i,int j)
-{
-  cout << v(i,j) << endl;
-}
-
-void pad(const i4_array & v,int i,int j,int k)
-{
-  cout << v(i,j,k) << endl;
-}
-void pad(const i4_array & v,int i,int j,int k,int l)
-{
-  cout << v(i,j,k,l) << endl;
-}
-
-void pad(const dvar_vector & v,const char * s)
-{
-  if (!strcmp(s,"s"))
-  {
-    cout << sum(v) << endl;
-  }
-}
-
-void pad(const dvar_vector & v,int i)
-{
-  cout << v[i] << endl;
-}
-
-//void pad(const dvar_vector & v,int i,int j)
-//{
-//  cout << v(i,j) << endl;
-//}
-
-//void pad(const dvector & v,int i,int j)
-//{
-//  cout << v(i,j) << endl;
-//}
-
-void pads(const dvar_vector & v,int i)
-{
-  cout << setscientific() << setprecision(5) << v[i] << endl;
-}
-
-void pads(const dvar_vector & v)
-{
-  cout << setscientific() << setprecision(5) << v << endl;
-}
-
-void pad(const dvar_vector & v)
-{
-  cout << v << endl;
-}
-
+/**
+* Print a double precision number to the screen
+* @param v The double to print
+* @return Nothing
+**/
 void pad(double v)
 {
   cout << v << endl;
 }
 
-void pad(const prevariable & v)
+ /**
+* Print an adstring to the screen
+* @param v The adstring to print
+* @return Nothing
+**/
+void pad(const adstring & v)
 {
   cout << v << endl;
 }
 
+ /**
+* Print a dvar3_array to the screen
+* @param v The dvar3_array to print
+* @return Nothing
+**/
+void pad(const dvar3_array & v)
+{
+  cout << v << endl;
+}
+
+ /**
+* Print a matrix slice of a dvar3_array to the screen
+* @param v The dvar3_array to print
+* @param i 1-based index of the matrix to print
+* @return Nothing
+**/
+void pad(const dvar3_array & v,int i)
+{
+  cout << v(i) << endl;
+}
+
+ /**
+* Print a vector slice of a dvar3_array to the screen
+* @param v The dvar3_array to print
+* @param i 1-based index of the matrix to slice from
+* @param j 1-based index of the row of the matrix to print
+* @return Nothing
+**/
+void pad(const dvar3_array & v,int i,int j)
+{
+  cout << v(i,j) << endl;
+}
+
+ /**
+* Print a scalar of a dvar3_array to the screen
+* @param v The dvar3_array to print
+* @param i 1-based index of the matrix to slice from
+* @param j 1-based index of the row of the matrix to print
+* @param k 1-based index of the element of the vector to print
+* @return Nothing
+**/
+void pad(const dvar3_array & v,int i,int j,int k)
+{
+  cout << v(i,j,k) << endl;
+}
+
+ /**
+* Print a dvar4_array to the screen
+* @param v The dvar4_array to print
+* @return Nothing
+**/
+void pad(const dvar4_array & v)
+{
+  cout << v << endl;
+}
+
+ /**
+* Print a 3D slice of a dvar4_array to the screen
+* @param v The dvar4_array to print
+* @param i 1-based index of the 3D object to print
+* @return Nothing
+**/
+void pad(const dvar4_array & v,int i)
+{
+  cout << v(i) << endl;
+}
+
+ /**
+* Print a matrix slice of a dvar4_array to the screen
+* @param v The dvar4_array to print
+* @param i 1-based index of the 3D object to print
+* @param j 1-based index of the matrix to print
+* @return Nothing
+**/
+void pad(const dvar4_array & v,int i,int j)
+{
+  cout << v(i,j) << endl;
+}
+
+ /**
+* Print a vector slice of a dvar4_array to the screen
+* @param v The dvar4_array to print
+* @param i 1-based index of the 3D object to print
+* @param j 1-based index of the matrix to print
+* @param k 1-based index of the vector to print
+* @return Nothing
+**/
+void pad(const dvar4_array & v,int i,int j,int k)
+{
+  cout << v(i,j,k) << endl;
+}
+
+ /**
+* Print a d4_array to the screen
+* @param v The d4_array to print
+* @return Nothing
+**/
+void pad(const d4_array & v)
+{
+  cout << v << endl;
+}
+
+ /**
+* Print a 3D slice of a d4_array to the screen
+* @param v The d4_array to print
+* @param i 1-based index of the 3D object to print
+* @return Nothing
+**/void pad(const d4_array & v,int i)
+{
+  cout << v(i) << endl;
+}
+
+ /**
+* Print a matrix slice of a d4_array to the screen
+* @param v The d4_array to print
+* @param i 1-based index of the 3D object to print
+* @param j 1-based index of the matrix to print
+* @return Nothing
+**/
+void pad(const d4_array & v,int i,int j)
+{
+  cout << v(i,j) << endl;
+}
+
+ /**
+* Print a vector slice of a d4_array to the screen
+* @param v The d4_array to print
+* @param i 1-based index of the 3D object to print
+* @param j 1-based index of the matrix to print
+* @param k 1-based index of the vector to print
+* @return Nothing
+**/
+void pad(const d4_array & v,int i,int j,int k)
+{
+  cout << v(i,j,k) << endl;
+}
+
+ /**
+* Print a d3_array to the screen
+* @param v The d3_array to print
+* @return Nothing
+**/
+void pad(const d3_array & v)
+{
+  cout << v << endl;
+}
+
+ /**
+* Print a matrix slice of a d3_array to the screen
+* @param v The d3_array to print
+* @param i 1-based index of the matrix to print
+* @return Nothing
+**/
+void pad(const d3_array & v,int i)
+{
+  cout << v(i) << endl;
+}
+
+ /**
+* Print a vector slice of a d3_array to the screen
+* @param v The d3_array to print
+* @param i 1-based index of the matrix to print
+* @param j 1-based index of the vector to print
+* @return Nothing
+**/
+void pad(const d3_array & v,int i,int j)
+{
+  cout << v(i,j) << endl;
+}
+
+/**
+* Print a scalar of a d3_array to the screen
+* @param v The d3_array to print
+* @param i 1-based index of the matrix to print
+* @param j 1-based index of the vector to print
+* @param k 1-based index of the element to print
+* @return Nothing
+**/
+void pad(const d3_array & v,int i,int j,int k)
+{
+  cout << v(i,j,k) << endl;
+}
+ /**
+* Print a i3_array to the screen
+* @param v The i3_array to print
+* @return Nothing
+**/
+void pad(const i3_array & v)
+{
+  cout << v << endl;
+}
+
+ /**
+* Print a matrix slice of a i3_array to the screen
+* @param v The i3_array to print
+* @param i 1-based index of the matrix to print
+* @return Nothing
+**/
+void pad(const i3_array & v,int i)
+{
+  cout << v(i) << endl;
+}
+
+ /**
+* Print a vector slice of a i3_array to the screen
+* @param v The i3_array to print
+* @param i 1-based index of the matrix to print
+* @param j 1-based index of the vector to print
+* @return Nothing
+**/
+void pad(const i3_array & v,int i,int j)
+{
+  cout << v(i,j) << endl;
+}
+
+/**
+* Print a scalar of a i3_array to the screen
+* @param v The i3_array to print
+* @param i 1-based index of the matrix to print
+* @param j 1-based index of the vector to print
+* @param k 1-based index of the element to print
+* @return Nothing
+**/
+void pad(const i3_array & v,int i,int j,int k)
+{
+  cout << v(i,j,k) << endl;
+}
+
+ /**
+* Print a i4_array to the screen
+* @param v The i4_array to print
+* @return Nothing
+**/
+void pad(const i4_array & v)
+{
+  cout << v << endl;
+}
+
+ /**
+* Print a 3D slice of a i4_array to the screen
+* @param v The i4_array to print
+* @param i 1-based index of the 3D object to print
+* @return Nothing
+**/
+void pad(const i4_array & v,int i)
+{
+  cout << v(i) << endl;
+}
+
+ /**
+* Print a matrix slice of a i4_array to the screen
+* @param v The i4_array to print
+* @param i 1-based index of the 3D object to print
+* @param j 1-based index of the matrix to print
+* @return Nothing
+**/
+void pad(const i4_array & v,int i,int j)
+{
+  cout << v(i,j) << endl;
+}
+
+ /**
+* Print a matrix slice of a i4_array to the screen
+* @param v The i4_array to print
+* @param i 1-based index of the 3D object to print
+* @param j 1-based index of the matrix to print
+* @return Nothing
+**/
+void pad(const i4_array & v,int i,int j,int k)
+{
+  cout << v(i,j,k) << endl;
+}
+
+ /**
+* Print a scalar of a i4_array to the screen
+* @param v The i4_array to print
+* @param i 1-based index of the 3D object to print
+* @param j 1-based index of the matrix to print
+* @param k 1-based index of the vector to print
+* @param l 1-based index of the element to print
+* @return Nothing
+**/
+void pad(const i4_array & v,int i,int j,int k,int l)
+{
+  cout << v(i,j,k,l) << endl;
+}
+
+/**
+* Print a dvar_vector to the screen
+* @param v The dvar_vector
+* @return Nothing
+**/
+void pad(const dvar_vector & v)
+{
+  cout << v << endl;
+}
+
+/**
+* Print an element of a dvar_vector to the screen
+* @param v The dvar_vector
+* @param i 1-based index of the element to be printed
+* @return Nothing
+**/
+void pad(const dvar_vector & v,int i)
+{
+  cout << v[i] << endl;
+}
+
+/**
+* Print a prevariable to the screen
+* @param v The prevariable to print
+* @return Nothing
+**/void pad(const prevariable & v)
+{
+  cout << v << endl;
+}
+
+/**
+* Print a dvector to the screen
+* @param v The dvector to print
+* @return Nothing
+**/
 void pad(const dvector & v)
 {
   cout << v << endl;
 }
 
+/**
+* Print an ivector to the screen
+* @param v The ivector to print
+* @return Nothing
+**/
 void pad(const ivector & v)
 {
   cout << v << endl;
 }
 
-void padp(const dmatrix & v,int i)
-{
-  cout << setprecision(12) << v(i) << endl;
-}
-
-void padp(const dvar_matrix & v,int i)
-{
-  cout << setprecision(12) << v(i) << endl;
-}
-
-void pad(const dvar_matrix & v,int i)
-{
-  cout << v(i) << endl;
-}
-
-void pad(const dvar_matrix & v,int i,int j)
-{
-  cout << v(i,j) << endl;
-}
-
+/**
+* Print a dvar_matrix to the screen
+* @param v The dvar_matrix to print
+* @return Nothing
+**/
 void pad(const dvar_matrix & v)
 {
   cout << v << endl;
 }
 
+/**
+* Print a vector slice of a dvar_matrix to the screen
+* @param v The dvar_matrix to print
+* @param i 1-based index of the vector to be printed
+* @return Nothing
+**/
+void pad(const dvar_matrix & v,int i)
+{
+  cout << v(i) << endl;
+}
+
+/**
+* Print a scalar of a dvar_matrix to the screen
+* @param v The dvar_matrix to print
+* @param i 1-based index of the vector to be printed
+* @param j 1-based index of the element to be printed
+* @return Nothing
+**/
+void pad(const dvar_matrix & v,int i,int j)
+{
+  cout << v(i,j) << endl;
+}
+
+/**
+* Print a dmatrix to the screen
+* @param v The dmatrix to print
+* @return Nothing
+**/
 void pad(const dmatrix & v)
 {
   cout << v << endl;
 }
 
+/**
+* Print a imatrix to the screen
+* @param v The imatrix to print
+* @return Nothing
+**/
 void pad(const imatrix & v)
 {
   cout << v << endl;
 }
 
+/**
+* Print a vector slice of an imatrix to the screen
+* @param v The imatrix to print
+* @param i 1-based index of the vector to be printed
+* @return Nothing
+**/
 void pad(const imatrix & v,int i)
 {
   cout << v(i) << endl;
 }
+
+/**
+* Print a scalar of an imatrix to the screen
+* @param v The imatrix to print
+* @param i 1-based index of the vector to be printed
+* @param j 1-based index of the element to be printed
+* @return Nothing
+**/
 void pad(const imatrix & v,int i,int j)
 {
   cout << v(i,j) << endl;
 }
 
-void sumprint(const dvar_vector & v)
+/**
+* Print an element of a dvar_vector to the screen in scientific notation
+* @param v The dvar_vector
+* @param i 1-based index of the element to be printed
+* @return Nothing
+**/
+void pads(const dvar_vector & v,int i)
+{
+  cout << setscientific() << setprecision(5) << v[i] << endl;
+}
+
+/**
+* Print a dvar_vector to the screen in scientific notation, with 5 significant figures
+* @param v The dvar_vector
+* @return Nothing
+**/
+void pads(const dvar_vector & v)
+{
+  cout << setscientific() << setprecision(5) << v << endl;
+}
+
+/**
+* Print a vector slice of a dmatrix to the screen, with 12 significant figures.
+* @param v The dmatrix to print
+* @param i 1-based index of the vector to be printed
+* @return Nothing
+**/
+void padp(const dmatrix & v,int i)
+{
+  cout << setprecision(12) << v(i) << endl;
+}
+
+/**
+* Print a vector slice of a dvar_matrix to the screen, with 12 significant figures.
+* @param v The dvar_matrix to print
+* @param i 1-based index of the vector to be printed
+* @return Nothing
+**/
+void padp(const dvar_matrix & v,int i)
+{
+  cout << setprecision(12) << v(i) << endl;
+}
+
+ /**
+* Print the sum of a dvar_vector to the screen
+* @param v The dvar_vector
+* @return Nothing
+**/
+void padsum(const dvar_vector & v)
 {
   cout << sum(v) << endl;
 }
 
-void sumprint(const dvector & v)
+ /**
+* Print the sum of a dvector to the screen
+* @param v The dvector
+* @return Nothing
+**/
+void padsum(const dvector & v)
 {
   cout << sum(v) << endl;
 }
 
-void sumprint(const ivector & v)
+ /**
+* Print the sum of an ivector to the screen
+* @param v The ivector
+* @return Nothing
+**/
+void padsum(const ivector & v)
 {
   cout << sum(v) << endl;
 }
 
-void sumprint(const dvar_matrix & v,int i)
-{
-  cout << v(i) << endl;
-}
-
-void sumprint(const dvar_matrix & v,int i,int j)
-{
-  cout << v(i,j) << endl;
-}
-
-void sumprint(const dvar_matrix & v)
+ /**
+* Print the sum of a dvar_matrix to the screen
+* @param v The dvar_matrix
+* @return Nothing
+**/
+void padsum(const dvar_matrix & v)
 {
   cout << sum(v) << endl;
 }
 
-void sumprint(const dmatrix & v)
+ /**
+* Print the sum of a dmatrix to the screen
+* @param v The dmatrix
+* @return Nothing
+**/
+void padsum(const dmatrix & v)
 {
   cout << sum(v) << endl;
 }
 
-void sumprint(const imatrix & v)
+ /**
+* Print the sum of an imatrix to the screen
+* @param v The imatrix
+* @return Nothing
+**/
+void padsum(const imatrix & v)
 {
   cout << sum(v) << endl;
 }
--- contrib/gdbprintlib/gdbprintlib.h
+++ contrib/gdbprintlib/gdbprintlib.h
@@ -1,3 +1,20 @@
+/**
+* @file gdbprint.h
+* @brief Contains function prototypes for printing ADMB objects during GDB sessions
+* \ingroup GDBPRINTLIB
+* This file contains function prototypes for printing out the various ADMB
+* variable types to the screen. This functioniality was added to allow easy
+* viewing of ADMB variables of all types during a GDB debugging session, but
+* the functions can be used in code.
+* To use the functions in a GDB session, ADMB must have been built from source
+* in debug mode, typically using the command 'make debug'. Also, the model
+* TPL must have been compiled using the debug flag, typically using the command
+* 'admb -g '.
+*
+* @author Chris Grandin, Dave Fournier
+* @date 6/18/2013
+**/
+
 #ifndef _GDBPRINT_H_
 #define _GDBPRINT_H_
 
@@ -5,72 +22,69 @@
 #include <admodel.h>
 #include <df1b2fun.h>
 
+void pad(double v);
+
 void pad(const adstring & v);
+
 void pad(const dvar3_array & v);
 void pad(const dvar3_array & v,int i);
 void pad(const dvar3_array & v,int i,int j);
+void pad(const dvar3_array & v,int i,int j,int k);
+
 void pad(const dvar4_array & v);
 void pad(const dvar4_array & v,int i);
 void pad(const dvar4_array & v,int i,int j);
 void pad(const dvar4_array & v,int i,int j,int k);
-void pad(const d4_array & v,int i,int j,int k);
+
 void pad(const d4_array & v);
 void pad(const d4_array & v,int i);
 void pad(const d4_array & v,int i,int j);
-void pad(const dvar3_array & v,int i,int j,int k);
+void pad(const d4_array & v,int i,int j,int k);
+
 void pad(const d3_array & v);
 void pad(const d3_array & v,int i);
 void pad(const d3_array & v,int i,int j);
 void pad(const d3_array & v,int i,int j,int k);
+
 void pad(const i3_array & v);
 void pad(const i3_array & v,int i);
 void pad(const i3_array & v,int i,int j);
 void pad(const i3_array & v,int i,int j,int k);
+
 void pad(const i4_array & v);
 void pad(const i4_array & v,int i);
 void pad(const i4_array & v,int i,int j);
 void pad(const i4_array & v,int i,int j,int k);
 void pad(const i4_array & v,int i,int j,int k,int l);
-void pad(const dvar_vector & v,const char * s);
-void pad(const dvar_vector & v,int i);
-//void pad(const dvar_vector & v,int i,int j);
-//void pad(const dvector & v,int i,int j);
-void pads(const dvar_vector & v,int i);
-void pads(const dvar_vector & v);
+
 void pad(const dvar_vector & v);
-void pad(double v);
+void pad(const dvar_vector & v,int i);
+
 void pad(const prevariable & v);
 void pad(const dvector & v);
 void pad(const ivector & v);
-void padp(const dmatrix & v,int i);
-void padp(const dvar_matrix & v,int i);
+
+void pad(const dvar_matrix & v);
 void pad(const dvar_matrix & v,int i);
 void pad(const dvar_matrix & v,int i,int j);
-void pad(const dvar_matrix & v);
+
 void pad(const dmatrix & v);
+
 void pad(const imatrix & v);
 void pad(const imatrix & v,int i);
 void pad(const imatrix & v,int i,int j);
-void sumprint(const dvar_vector & v);
-void sumprint(const dvector & v);
-void sumprint(const ivector & v);
-void sumprint(const dvar_matrix & v,int i);
-void sumprint(const dvar_matrix & v,int i,int j);
-void sumprint(const dvar_matrix & v);
-void sumprint(const dmatrix & v);
-void sumprint(const imatrix & v);
 
-/**
-* @file gdbPrint.h
-* @brief Library of statistic functions
-* \ingroup STATLIB
-* This library contains declarations for
-* print functions that can be used during a
-* gdb debugging session.
-* 
-* @author Chris Grandin, Dave Fournier
-* 
-* @date 05/06/2013
-*/
+void pads(const dvar_vector & v);
+void pads(const dvar_vector & v,int i);
+
+void padp(const dmatrix & v,int i);
+void padp(const dvar_matrix & v,int i);
+
+void padsum(const dvar_vector & v);
+void padsum(const dvector & v);
+void padsum(const ivector & v);
+void padsum(const dvar_matrix & v);
+void padsum(const dmatrix & v);
+void padsum(const imatrix & v);
 
 #endif

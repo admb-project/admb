4d1eef7197a12c257d84f1317d9fb54fad395ae8
johnoel <johnoel@6fd93cbc-6849-4d5d-bcfc-d3e1d932c95a>
2013-10-09 20:39:38 +0000

    Test threading.

--- branches/threaded2merge/tests/threads/msimple/tests_adthread_manager.cpp
+++ branches/threaded2merge/tests/threads/msimple/tests_adthread_manager.cpp
@@ -75,10 +75,61 @@ TEST_F(tests_adthread_manager, create_all)
     data[i].thread_no = i;
   }
 
+  //This starts thread after a unknown delay
   ad_comm::pthread_manager->create_all(data);
+  //ASSERT_EQ(0, test_attach_code_value);
+
   //Need to join it
   ad_comm::pthread_manager->pthread_join_all();
   ASSERT_EQ(1, test_attach_code_value);
+
+  delete [] data;
+  data = 0;
+
+  delete ad_comm::pthread_manager;
+  ad_comm::pthread_manager = 0;
+}
+static int test_attach_code_value_5threads = 0;
+pthread_mutex_t test_attach_code_value_5threads_mutex = PTHREAD_MUTEX_INITIALIZER;
+void* test_attach_code_5threads(void* ptr)
+{
+  pthread_mutex_lock(&test_attach_code_value_5threads_mutex);
+  test_attach_code_value_5threads++;
+  pthread_mutex_unlock(&test_attach_code_value_5threads_mutex);
+  return ptr;
+}
+TEST_F(tests_adthread_manager, create_all_5threads)
+{
+  int nthread = 5;
+  int ngroups = 1;
+  ivector ng(1, ngroups);
+  // number of threads in group 1
+  ng(1)=nthread;
+  // create instance of pthread_manager class 
+  // third argument is number of bytes in the transfer buffer
+  ad_comm::pthread_manager = new adpthread_manager(ngroups, ng, 32005);
+  if (ad_comm::pthread_manager == 0)
+  {
+    FAIL();
+  }
+
+  ASSERT_EQ(0, test_attach_code_value_5threads);
+  ad_comm::pthread_manager->attach_code(test_attach_code_5threads);
+  ASSERT_EQ(0, test_attach_code_value_5threads);
+
+  new_thread_data* data = new new_thread_data[nthread + 1];
+  for (int i = 1; i <= nthread; i++)
+  {
+    data[i].thread_no = i;
+  }
+
+  //This starts thread after a unknown delay
+  ad_comm::pthread_manager->create_all(data);
+  //ASSERT_EQ(0, test_attach_code_value_5threads);
+
+  //Need to join it
+  ad_comm::pthread_manager->pthread_join_all();
+  ASSERT_EQ(5, test_attach_code_value_5threads);
 
   delete [] data;
   data = 0;

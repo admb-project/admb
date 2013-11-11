8e64dfea601a18a1b74e200b719f6899001c448c
johnoel <johnoel@6fd93cbc-6849-4d5d-bcfc-d3e1d932c95a>
2013-11-11 03:09:20 +0000

    Fix pragma warnings.

--- trunk/src/df1b2-separable/df1b2h.cpp
+++ trunk/src/df1b2-separable/df1b2h.cpp
@@ -8,12 +8,10 @@
  * \file
  * Description not yet available.
  */
-#if defined(__GNUDOS__)
-#  pragma implementation "df1b2fun.h"
+#if defined(__GNUC__) && (__GNUC__ < 3)
+  #pragma implementation "df1b2fun.h"
 #endif
+
 #define HOME_VERSION
 #include "df1b2fun.h"
-
-int make_sure_df1b2fun_gets_linked=0;
-
 #undef HOME_VERSION

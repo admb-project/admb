3a6ae774adc77d40d647abb6d047049b957d910f
johnoel <johnoel@6fd93cbc-6849-4d5d-bcfc-d3e1d932c95a>
2013-11-14 17:24:32 +0000

    Add more tests for DF_FILE class.

--- trunk/tests/gtests/test_df_file.cpp
+++ trunk/tests/gtests/test_df_file.cpp
@@ -37,6 +37,11 @@ TEST_F(test_df_file, instance_INT_MAX)
 {
   ASSERT_NO_THROW(DF_FILE instance(INT_MAX));
 }
+TEST_F(test_df_file, instance_INT_MAX_plus_1)
+{
+  ASSERT_NO_THROW(DF_FILE instance(INT_MAX + 1));
+}
+/*
 TEST_F(test_df_file, allocate_INT_MAX_plus_1)
 {
   ASSERT_LT(INT_MAX, LONG_MAX);
@@ -44,10 +49,79 @@ TEST_F(test_df_file, allocate_INT_MAX_plus_1)
   ASSERT_EQ(SIZE_MAX, ULONG_MAX);
   size_t size = INT_MAX + 1;
   ASSERT_LT(size, SIZE_MAX);
-  char* a = new char[size];
-  if (a != 0)
+  //Cannot allocate for size > INT_MAX 
+  try
   {
-    delete [] a;
-    a = 0;
+    new char[size];
   }
+  catch(...)
+  {
+    return;
+  }
+  FAIL();
+}
+*/
+TEST_F(test_df_file, write_read_int)
+{
+  DF_FILE buffer(sizeof(int));
+  const int expected = __LINE__;
+  buffer.fwrite(expected);
+  int integer = 0;
+  buffer.fread(integer);
+  ASSERT_EQ(expected, integer);
+}
+TEST_F(test_df_file, write_read_int_3x)
+{
+  DF_FILE buffer(sizeof(int));
+
+  const int expected = __LINE__;
+  buffer.fwrite(expected);
+
+  const int expected2 = __LINE__;
+  buffer.fwrite(expected2);
+
+  const int expected3 = __LINE__;
+  buffer.fwrite(expected3);
+
+  int integer3 = 0;
+  buffer.fread(integer3);
+  ASSERT_EQ(expected3, integer3);
+
+  int integer2 = 0;
+  buffer.fread(integer2);
+  ASSERT_EQ(expected2, integer2);
+
+  int integer = 0;
+  buffer.fread(integer);
+  ASSERT_EQ(expected, integer);
+}
+TEST_F(test_df_file, write_read_double)
+{
+  DF_FILE buffer(sizeof(double));
+  const double expected = __LINE__;
+  buffer.fwrite(expected);
+
+  double number = 0;
+  buffer.fread(number);
+  ASSERT_EQ(expected, number);
+}
+TEST_F(test_df_file, write_read_char_array11)
+{
+  DF_FILE buffer(50);
+  const char expected[11] = "12345-6789";
+  buffer.fwrite(expected, 11);
+
+  char array[11] = "\0";
+  buffer.fread(&array[0], 11);
+  ASSERT_STREQ(expected, array);
+}
+TEST_F(test_df_file, write_read_ptr)
+{
+  DF_FILE buffer(50);
+  int expected = __LINE__;
+  int* ptr = &expected;
+  int* number = 0;
+  //buffer.fwrite(ptr);
+  //buffer.fread(number);
+  ASSERT_EQ(&expected, number);
 }

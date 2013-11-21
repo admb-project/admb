adf001c54eaa3d9ece0d6a2d5b33598b02bb51e9
johnoel <johnoel@6fd93cbc-6849-4d5d-bcfc-d3e1d932c95a>
2013-11-21 03:33:51 +0000

    MinGW scripts cleanups.

--- scripts/admb/root-admb.bat
+++ scripts/admb/root-admb.bat
@@ -5,20 +5,22 @@
 
 setlocal EnableExtensions EnableDelayedExpansion
 
-for %%a in (%0.bat) do (
+for %%a in (%0) do (
   set HAS_PATH=%%~$PATH:a
-  if not "!HAS_PATH!"=="" (
+  if defined HAS_PATH (
     set ADMB_PATH=%%~dp$PATH:a
   ) else (
     set ADMB_PATH=%%~dpa
   )
-)
-if exist !ADMB_PATH!\bin\admb.bat (
-  set ADMB_HOME=!ADMB_PATH!
-  call "!ADMB_HOME!\bin\admb.bat" %*
-) else (
-  if exist !ADMB_PATH!build\dist\bin\admb.bat (
-    set ADMB_HOME=!ADMB_PATH!build\dist
-    call "!ADMB_HOME!\bin\admb.bat" %*
+  if exist !ADMB_PATH!\bin\admb.cmd (
+    set ADMB_HOME=!ADMB_PATH!
+    call "!ADMB_HOME!\bin\admb" %*
+  ) else (
+    if exist !ADMB_PATH!build\dist\bin\admb.cmd (
+      set ADMB_HOME=!ADMB_PATH!build\dist
+      call "!ADMB_HOME!\bin\admb" %*
+    )
   )
+  goto EOF
 )
+:EOF

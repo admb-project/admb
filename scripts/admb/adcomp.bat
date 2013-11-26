ac51b59066c77ed92f820eabeff2bc1ee1b3adf8
johnoel <johnoel@6fd93cbc-6849-4d5d-bcfc-d3e1d932c95a>
2013-11-26 09:41:26 +0000

    Add back batch adcomp and adlink.

--- scripts/admb/adcomp.bat
+++ scripts/admb/adcomp.bat
@@ -1,66 +1,19 @@
 @echo off
+@REM
+@REM  Copyright 2013 (c) ADMB Foundation
+@REM
 setlocal EnableExtensions EnableDelayedExpansion
-if [%1]==[] goto HELP
-if [%1]==[-help] goto HELP
-if [%1]==[--help] goto HELP
-
-rem Pop args until model=%1
-set dll=
-set safopt=-DSAFE_ALL
-set sym=-O3
-set i=0
-:STARTLOOP
-if [%2]==[] goto ENDLOOP
-if %1==-d set dll=-DBUILDING_DLL & shift
-if %1==-g set sym=-g& shift
-if %1==-r shift
-if %1==-f set safopt=-DOPT_LIB& shift
-set /a i=%i%+1
-if %i%==100 shift & set i=0 & echo.&echo Warning: illegal option %1 (discarded)
-goto STARTLOOP
-:ENDLOOP
-
-set model=%~n1
-
-if exist "%ADMB_HOME%"\bin\admb-cfg.bat call "%ADMB_HOME%"\bin\admb-cfg.bat
-if defined CXXFLAGS (
-  set CXXFLAGS=%CXXFLAGS% %ADMB_CFG_CXXFLAGS%
-) else (
-  set CXXFLAGS=%ADMB_CFG_CXXFLAGS%
+set args=
+for %%a in (%*) do (
+  set arg=%%a
+  if "!arg:~0,1!"=="-" (
+      set args=!args! !arg!
+  ) else (
+    if "%%~xa"=="" (
+      set args=!args! !arg!.cpp
+    ) else (
+      set args=!args! !arg!
+    )
+  )
 )
-
-set CMD=g++ -c %CXXFLAGS% %sym% -D__GNUDOS__ %dll% -Dlinux %safopt% -DUSE_LAPLACE -fpermissive -I. -I"%ADMB_HOME%\include" -I"%ADMB_HOME%\contrib\include" -o %model%.obj %model%.cpp
-echo %CMD%
-%CMD%
-
-goto EOF
-
-:HELP
-echo Usage: adcomp [-d] [-g] [-r] [-f] model
-echo.
-echo Compile AD Model Builder C++ to object code, using the MinGW GCC compiler.
-echo.
-echo   -d     Build a dynamic library (dll).
-echo   -g     Build with debug symbols.
-echo   -r     Build Random effects program (ADMB-RE).
-echo   -f     Build with Fast optimized mode (no bounds checking).
-echo          By default, admb script builds with bounds checking.
-echo   model  TPL file (ie 'simple.tpl' or the filename 'simple' with no .tpl extension)
-echo.
-
-:EOF
-
-REM r982 [2011-02-16] arnima  rewrite, fixed bug when user option is not
-REM                           recognized, added CXXFLAGS, fixed spaces
-REM r917 [2010-12-24] johnoel pruned 'mingw' dir
-REM r914 [2010-12-24] johnoel changed \\include to \include and echo off to
-REM                           @echo off, moved to 'g++' dir
-REM r629 [2010-05-20] johnoel changed .o to .obj
-REM r623 [2010-05-20] johnoel changed /include to \\include
-REM r593 [2010-04-20] johnoel quoted ADMB_HOME
-REM r563 [2010-04-15] johnoel simplified echo, added -DSAFE_ALL
-REM r525 [2009-08-07] arnima  added support for filename extension like
-REM                           simple.cpp, unquoted ADMB_HOME, enabled warnings
-REM                   johnoel split -s option into separate -g and -s options,
-REM                           if -g then not -O3
-REM r244 [2009-05-28] arnima  created
+call admb -c $args
--- scripts/admb/adlink.bat
+++ scripts/admb/adlink.bat
@@ -1,62 +1,19 @@
 @echo off
-@REM r244 [2009-05-28] arnima  created
-
+@REM
+@REM  Copyright 2013 (c) ADMB Foundation
+@REM
 setlocal EnableExtensions EnableDelayedExpansion
-
-if [%1]==[] goto HELP
-if [%1]==[-help] goto HELP
-if [%1]==[--help] goto HELP
-
-rem Pop args until model=%1
-set objects=
-set sym=-s & rem space
-set linker=g++
+set args=
 for %%a in (%*) do (
-if %%~xa==.obj (
-set objects=!objects! %%a
-if not defined model set model=%%~na
+  set arg=%%a
+  if "!arg:~0,1!"=="-" (
+      set args=!args! !arg!
+  ) else (
+    if "%%~xa"=="" (
+      set args=!args! !arg!.obj
+    ) else (
+      set args=!args! !arg!
+    )
+  )
 )
-if %%~xa==.o (
-set objects=!objects! %%a
-if not defined model set model=%%~na
-)
-if %%a==-d set linker=dllwrap
-if %%a==-g set sym=
-if %%a==-r set r=
-if %%a==-f (
-  set libs="%ADMB_HOME%\contrib\lib\libcontribo.a" "%ADMB_HOME%\lib\libadmbo.a" 
-)
-)
-if not defined libs set libs="%ADMB_HOME%\contrib\lib\libcontrib.a" "%ADMB_HOME%\lib\libadmb.a" 
-
-if %linker%==g++ (set out=-o %model%) else (set def=-def %model%.def^
- --driver-name g++ --output-lib %model%.dll & set out=--output-lib lib%model%.a -o %model%.dll)
-
-if exist "%ADMB_HOME%"\bin\admb-cfg.bat call "%ADMB_HOME%"\bin\admb-cfg.bat
-if defined LDFLAGS (
-  set LDFLAGS=%LDFLAGS% %ADMB_CFG_LDFLAGS%
-) else (
-  set LDFLAGS=%ADMB_CFG_LDFLAGS%
-)
-
-set CMD=%linker% %LDFLAGS% %sym% -static %def% -o %model%.exe %objects% %libs%
-echo %CMD%
-%CMD%
-
-goto EOF
-
-:HELP
-echo Usage: adlink [-d] [-g] [-r] [-f] model
-echo.
-echo Link AD Model Builder object code to executable, using the MinGW GCC^
- compiler.
-echo.
-echo   -d     Build a dynamic library (dll).
-echo   -g     Build with debug symbols.
-echo   -r     Build Random effects program (ADMB-RE).
-echo   -f     Build with Fast optimized mode (no bounds checking).
-echo          By default, admb script builds with bounds checking.
-echo   model  TPL file (ie 'simple.tpl' or the filename 'simple' with no .tpl extension)
-echo.
-
-:EOF
+call admb $args

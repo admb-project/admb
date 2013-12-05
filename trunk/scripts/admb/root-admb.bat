f876ab8b45ae5652fcc472458a71b1a7a8c16427
johnoel <johnoel@6fd93cbc-6849-4d5d-bcfc-d3e1d932c95a>
2013-12-05 22:17:10 +0000

    Add error checks into batch scripts.

--- trunk/scripts/admb/root-admb.bat
+++ trunk/scripts/admb/root-admb.bat
@@ -5,25 +5,32 @@
 
 setlocal EnableExtensions EnableDelayedExpansion
 
-for %%a in (%0) do (
-  set HAS_PATH=%%~$PATH:a
-  if defined HAS_PATH (
-    set ADMB_PATH=%%~dp$PATH:a
-  ) else (
-    set ADMB_PATH=%%~dpa
+set SCRIPT_PATH=%~dp0
+if not defined SCRIPT_PATH (
+  for %%a in (%0) do (
+    set SCRIPT_PATH=%%~dp$PATH:a
   )
-  if exist !ADMB_PATH!bin\admb.cmd (
-    set ADMB_HOME=!ADMB_PATH:~0,-1!
-    call "!ADMB_HOME!\bin\admb" %*
-  ) else (
-    if exist !ADMB_PATH!build\dist\bin\admb.cmd (
-      set ADMB_HOME=!ADMB_PATH!build\dist
-      call "!ADMB_HOME!\bin\admb" %*
-    ) else (
-      echo "Error: Unable to find ADMB_HOME\bin\admb.cmd."
-      goto EOF
-    )
-  )
-  goto EOF
+) 
+if not defined SCRIPT_PATH (
+  echo "SCRIPT_PATH: !SCRIPT_PATH!"
+  goto ERROR
 )
+pushd !SCRIPT_PATH!
+set ADMB_PATH=!CD!
+popd
+if exist !ADMB_PATH!\bin\admb.cmd (
+  set ADMB_HOME=!ADMB_PATH!
+) else (
+  if exist !ADMB_PATH!\build\dist\bin\admb.cmd (
+    set ADMB_HOME=!ADMB_PATH!\build\dist
+  )
+)
+if not defined ADMB_HOME (
+  echo "ADMB_HOME: !ADMB_HOME!"
+  goto ERROR
+)
+call "!ADMB_HOME!\bin\admb" %*
+goto EOF
+:ERROR
+echo Error: Unable to find 'bin\admb.cmd'.
 :EOF

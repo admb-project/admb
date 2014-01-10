4d51cfda8a03362f21a1250f89124748cb79f8c5
johnoel <johnoel@6fd93cbc-6849-4d5d-bcfc-d3e1d932c95a>
2014-01-10 00:22:54 +0000

    Add test for scalefactor.

--- trunk/tests/gtests/test_scalefactor.cpp
+++ trunk/tests/gtests/test_scalefactor.cpp
@@ -1,45 +1,23 @@
 #include <gtest/gtest.h>
-#include <admodel.h>
-#include <contrib.h>
 #include <df1b2fun.h>
-#include <adrndeff.h>
-#include "scalefactor.htp"
-
-extern "C"  {
-  void ad_boundf(int i);
-}
-extern long int arrmblsize;
 
 class test_scalefactor: public ::testing::Test {};
 
 TEST_F(test_scalefactor, run)
 {
-  int scalefactor(int argc, char* argv[]);
-  //scalefactor(argc, argv);
-}
+  adpool* save = df1b2variable::pool;
+  df1b2variable::pool = 0;
+  {
+    adpool pool;
+    df1b2variable::pool = &pool;
 
-TEST_F(test_scalefactor, run2)
-{
-  ad_set_new_handler();
-  ad_exit=&ad_boundf;
-  gradient_structure::set_NO_DERIVATIVES();
-  gradient_structure::set_YES_SAVE_VARIABLES_VALUES();
-#if defined(__GNUDOS__) || defined(DOS386) || defined(__DPMI32__)  || defined(__MSVC32__)
-  if (!arrmblsize) arrmblsize=150000;
-#else
-  if (!arrmblsize) arrmblsize=25000;
-#endif
-  int argc = 1;
-  char* argv[] = { (char*)"./scalefactor" };
+    int argc = 1;
+    char* argv[] = { (char*)"./scalefactor" };
 
-  df1b2variable::noallocate=1;
-  df1b2_parameters mp(arrmblsize,argc,argv);
-  mp.iprint=10;
+    int scalefactor(int argc, char* argv[]);
+    scalefactor(argc, argv);
 
-  function_minimizer::random_effects_flag=1;
-  df1b2variable::noallocate=0;
-  mp.preliminary_calculations();
-  mp.userfunction();
-
-  //mp.computations(argc,argv);
+    df1b2variable::pool = 0;
+  }
+  df1b2variable::pool = save;
 }

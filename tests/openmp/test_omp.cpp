441ff8d778cb583845e98a2384e8a3358b10cf51
johnoel <johnoel@6fd93cbc-6849-4d5d-bcfc-d3e1d932c95a>
2014-01-27 22:08:44 +0000

    Add omp hello world test.

--- tests/openmp/test_omp.cpp
+++ tests/openmp/test_omp.cpp
@@ -6,6 +6,8 @@
 
 class test_omp: public ::testing::Test {};
 
-TEST_F(test_omp, example)
+TEST_F(test_omp, hello_world)
 {
+  #pragma omp parallel
+  printf("hello_world: thread %d, nthreads %d\n", omp_get_thread_num(), omp_get_num_threads());
 }

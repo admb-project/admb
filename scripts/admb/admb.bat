d6ae3c86e2c86a165ff182f21fdc6498ff7193f0
cgrandin <cgrandin@6fd93cbc-6849-4d5d-bcfc-d3e1d932c95a>
2014-02-13 16:31:17 +0000

    Changes to admb scripts to ensure the debug flag is passed to tpl2cpp.

--- scripts/admb/admb.bat
+++ scripts/admb/admb.bat
@@ -251,7 +251,7 @@ for %%a in (!tpls!) do (
   if not defined parser (
     set parser=tpl2cpp
   )
-  set CMD=!parser!!dll! !tpl!
+  set CMD=!parser! !debug! !dll! !tpl!
   echo.&echo *** Parsing !tpl!.tpl:
   echo !CMD!
   call !CMD!

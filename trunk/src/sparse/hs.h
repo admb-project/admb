7fbdec0428ec56bc07c9a12315add020fb67e9ea
johnoel <johnoel@6fd93cbc-6849-4d5d-bcfc-d3e1d932c95a>
2014-03-04 21:36:53 +0000

    Coding standard: Replace tabs with spaces.

--- trunk/src/sparse/hs.h
+++ trunk/src/sparse/hs.h
@@ -69,7 +69,6 @@ class dvar_compressed_triplet;
 class hs_smatrix    // matrix in compressed-column, 0 offset
 {
     public:
-
     int nzmax ;     // maximum number of entries; in practice nzmax= # non-zero
     int m ;         // number of rows
     int n ;         // number of columns; m=n for symmetric matrix
@@ -102,7 +101,6 @@ class hs_smatrix    // matrix in compressed-column, 0 offset
 class dvar_hs_smatrix    // matrix in compressed-column, 0 offset
 {
     public:
-
     int nzmax ;     // maximum number of entries; in practice nzmax= # non-zero
     int m ;         // number of rows
     int n ;         // number of columns; m=n for symmetric matrix
@@ -136,10 +134,13 @@ static int cs_wclear (int mark, int lemax, ivector &w, int n);
 void cs_transpose (XCONST hs_smatrix &A, int values, hs_smatrix &AT);
 void cs_transpose (XCONST dvar_hs_smatrix &A, int values, dvar_hs_smatrix &AT);
 cs* hs_convert(XCONST hs_smatrix & CC);
-int cs_ereach (XCONST hs_smatrix &A, int k, XCONST ivector &parent, ivector &s, ivector &w);
-int cs_ereach (XCONST dvar_hs_smatrix &A, int k, XCONST ivector &parent, ivector &s, ivector &w);
+int cs_ereach (XCONST hs_smatrix &A, int k, XCONST ivector &parent, ivector &s,
+  ivector &w);
+int cs_ereach (XCONST dvar_hs_smatrix &A, int k, XCONST ivector &parent,
+  ivector &s, ivector &w);
 void hs_symperm(XCONST hs_smatrix &A, XCONST ivector &pinv, hs_smatrix &C);
-void hs_symperm(XCONST dvar_hs_smatrix &A, XCONST ivector &pinv, dvar_hs_smatrix &C);
+void hs_symperm(XCONST dvar_hs_smatrix &A, XCONST ivector &pinv,
+  dvar_hs_smatrix &C);
 int chol(XCONST hs_smatrix &A, XCONST hs_symbolic &S, hs_smatrix &L);
 int chol(XCONST dvar_hs_smatrix &A, XCONST hs_symbolic &S, dvar_hs_smatrix &L);
 dvector cs_ipvec(XCONST ivector &p, XCONST dvector &b);
@@ -149,13 +150,19 @@ dvector cs_lsolve (XCONST hs_smatrix &L, XCONST dvector &y);
 dvar_vector cs_lsolve (XCONST dvar_hs_smatrix &L, XCONST dvar_vector &y);
 dvector cs_ltsolve (XCONST hs_smatrix &L, XCONST dvector &y);
 dvar_vector cs_ltsolve (XCONST dvar_hs_smatrix &L, XCONST dvar_vector &y);
-int cs_fkeep (hs_smatrix &A, int (*fkeep) (int, int, double, void *), void *other);
-int cs_fkeep (dvar_hs_smatrix &A, int (*fkeep) (int, int, const prevariable& , void *), void *other);
+int cs_fkeep (hs_smatrix &A, int (*fkeep) (int, int, double, void *),
+  void *other);
+int cs_fkeep (dvar_hs_smatrix &A, int (*fkeep) (int, int, const prevariable& ,
+  void *), void *other);
 int cs_scatter(XCONST hs_smatrix &A, int j, double beta, ivector &w, dvector &x, int mark,hs_smatrix &C, int nz);
-int cs_scatter(XCONST dvar_hs_smatrix &A, int j, double beta, ivector &w, dvar_vector &x, int mark,hs_smatrix &C, int nz);
-hs_smatrix cs_add(XCONST hs_smatrix &A, XCONST hs_smatrix &B, double alpha=1.0, double beta=1.0);
-dvar_hs_smatrix cs_add(XCONST dvar_hs_smatrix &A, XCONST dvar_hs_smatrix &B, double alpha=1.0, double beta=1.0);
-int cs_tdfs (int j, int k, ivector &head, XCONST ivector &next, ivector &post, ivector &stack);
+int cs_scatter(XCONST dvar_hs_smatrix &A, int j, double beta, ivector &w,
+  dvar_vector &x, int mark,hs_smatrix &C, int nz);
+hs_smatrix cs_add(XCONST hs_smatrix &A, XCONST hs_smatrix &B, double alpha=1.0,
+  double beta=1.0);
+dvar_hs_smatrix cs_add(XCONST dvar_hs_smatrix &A, XCONST dvar_hs_smatrix &B,
+  double alpha=1.0, double beta=1.0);
+int cs_tdfs (int j, int k, ivector &head, XCONST ivector &next, ivector &post,
+  ivector &stack);
 ivector cs_amd (XCONST hs_smatrix &A);
 ivector cs_amd (XCONST dvar_hs_smatrix &A);
 ivector cs_amd (int order,XCONST hs_smatrix &A);
@@ -163,9 +170,12 @@ ivector cs_amd (int order,XCONST dvar_hs_smatrix &A);
 ivector cs_etree (XCONST hs_smatrix &A);
 ivector cs_etree (XCONST dvar_hs_smatrix &A);
 ivector cs_post (XCONST ivector &parent, int n);
-int cs_leaf (int i, int j, XCONST ivector &first, ivector &maxfirst, ivector &prevleaf,ivector &ancestor, int *jleaf);
-ivector cs_counts (XCONST hs_smatrix &A, XCONST ivector &parent, XCONST ivector &post);
-ivector cs_counts (XCONST dvar_hs_smatrix &A, XCONST ivector &parent, XCONST ivector &post);
+int cs_leaf (int i, int j, XCONST ivector &first, ivector &maxfirst,
+  ivector &prevleaf,ivector &ancestor, int *jleaf);
+ivector cs_counts (XCONST hs_smatrix &A, XCONST ivector &parent,
+  XCONST ivector &post);
+ivector cs_counts (XCONST dvar_hs_smatrix &A, XCONST ivector &parent,
+  XCONST ivector &post);
 ivector cs_pinv (XCONST ivector &p, int n);
 
 dvariable ln_det(XCONST dvar_hs_smatrix & M);

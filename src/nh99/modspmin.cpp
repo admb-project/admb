e2361a943a7023ce62e2b5d035e42a14b53c707d
johnoel <johnoel@6fd93cbc-6849-4d5d-bcfc-d3e1d932c95a>
2014-03-05 22:15:29 +0000

    Remove unused variables.

--- src/nh99/modspmin.cpp
+++ src/nh99/modspmin.cpp
@@ -116,11 +116,12 @@ int kill_flag_107=901;
     ad_exit(0);
 #endif
     //int *  xflag_105=0;
-    int f03=503;
 #if defined(ADMB_REDEMO)
+    int f03=503;
     kill_address=&stupid_flag;
-#endif
     int two=2;
+    int* xflag_105=&something_105;
+#endif
     tracing_message(traceflag,"A1");
     //if (option_match(argc,argv,"-gui")>-1)
     //{
@@ -130,7 +131,6 @@ int kill_flag_107=901;
 #if defined (AD_DEMO)
      write_banner_stuff();
 #endif
-    int* xflag_105=&something_105;
     if (option_match(argc,argv,"-mceval") == -1)
     {
       //negative_eigenvalue_flag=0;

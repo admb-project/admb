9807721f87d546bdf49ae5e142467c8361f0a9b7
johnoel <johnoel@6fd93cbc-6849-4d5d-bcfc-d3e1d932c95a>
2014-03-06 23:33:00 +0000

    Remove unununsed variables.

--- trunk/src/nh99/hybmcmc.cpp
+++ trunk/src/nh99/hybmcmc.cpp
@@ -106,7 +106,7 @@ void function_minimizer::hybrid_mcmc_routine(int nmcmc,int iseed0,double dscale,
   int no_sd_mcmc=0;
 
   int on2=-1;
-  int nvar1=0;
+  //int nvar1=0;
   if ( (on2=option_match(ad_comm::argc,ad_comm::argv,"-nosdmcmc"))>-1)
   {
     no_sd_mcmc=1;
@@ -114,7 +114,7 @@ void function_minimizer::hybrid_mcmc_routine(int nmcmc,int iseed0,double dscale,
   if (mcmc2_flag==1)
   {
     initial_params::restore_start_phase();
-    nvar1=initial_params::nvarcalc(); // get the number of active parameters
+    //nvar1=initial_params::nvarcalc(); // get the number of active parameters
   }
 
   if (stddev_params::num_stddev_params==0)
@@ -137,18 +137,14 @@ void function_minimizer::hybrid_mcmc_routine(int nmcmc,int iseed0,double dscale,
     //double total_spread=2500;
     //uostream * pofs_sd = NULL;
 
-
-    int nvar_x=0;
 #if defined(USE_LAPLACE)
     initial_params::set_inactive_random_effects();
-    nvar_x=initial_params::nvarcalc();
+    //int nvar_x=initial_params::nvarcalc();
     initial_params::set_active_random_effects();
     int nvar_re=initial_params::nvarcalc();
 #endif
 
-
     int nvar=initial_params::nvarcalc(); // get the number of active parameters
-    int scov_option=0;
     dmatrix s_covar;
     dvector s_mean;
     //int ncsim=25000;
@@ -159,10 +155,8 @@ void function_minimizer::hybrid_mcmc_routine(int nmcmc,int iseed0,double dscale,
     //int ncor=0;
     //double bfsum=0;
     //int ibfcount=0;
-    double llbest;
-    double lbmax;
+    //double lbmax;
 
-    scov_option=1;
     s_covar.allocate(1,nvar,1,nvar);
     s_mean.allocate(1,nvar);
     s_mean.initialize();
@@ -531,8 +525,8 @@ void function_minimizer::hybrid_mcmc_routine(int nmcmc,int iseed0,double dscale,
 
       z=x0+chd*y;
       /*double llc=*/get_hybrid_monte_carlo_value(nvar,z,g);
-      llbest=get_hybrid_monte_carlo_value(nvar,z,g);
-      lbmax=llbest;
+      /*double llbest=*/get_hybrid_monte_carlo_value(nvar,z,g);
+      //lbmax=llbest;
 
 
        int number_sims;

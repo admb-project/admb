bc04b3359bd6da05b3e4865306c438e8586fe708
johnoel <johnoel@6fd93cbc-6849-4d5d-bcfc-d3e1d932c95a>
2014-04-08 22:36:23 +0000

    Coding standard: Line width is 80.

--- trunk/src/linad99/xgradclc.cpp
+++ trunk/src/linad99/xgradclc.cpp
@@ -126,7 +126,8 @@ void funnel_gradcalc(void)
     * (double*) (gradient_structure::GRAD_LIST->dlink_addresses[i]) = 0;
   }
 
-  double_and_int* tmp = (double_and_int*)gradient_structure::ARRAY_MEMBLOCK_BASE;
+  double_and_int* tmp =
+    (double_and_int*)gradient_structure::ARRAY_MEMBLOCK_BASE;
 
      unsigned long int max_last_offset =
        gradient_structure::ARR_LIST1->get_max_last_offset();

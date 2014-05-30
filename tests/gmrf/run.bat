2843217db4ffde034e814a9d2cd20115856a5b37
johnoel <johnoel@6fd93cbc-6849-4d5d-bcfc-d3e1d932c95a>
2014-05-30 03:49:48 +0000

    Fix more test build files.

---
+++ tests/gmrf/run.bat
@@ -0,0 +1,1 @@
+gmrf -est -ilmn 5 -ind gmrf100.dat -shess -ndi 10000000  -gbs 500000000 -noinit
---
+++ tests/gmrf/run2.bat
@@ -0,0 +1,1 @@
+gmrf -est -ilmn 5 -ind gmrf200.dat -shess -ndi 10000000  -gbs 1500000000 -noinit -nl1 1000000

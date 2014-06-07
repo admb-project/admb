bf38706f533408396f86451e790945dd25553c89
johnoel <johnoel@6fd93cbc-6849-4d5d-bcfc-d3e1d932c95a>
2014-06-07 01:23:31 +0000

    Update installers for mingw 32 and 64 bit.

--- scripts/mingw/set-admb-mingw.bat
+++ scripts/mingw/set-admb-mingw.bat
@@ -1,23 +1,9 @@
 @echo off
 
-REM
-REM Sets the ADMB Home directory.
-REM
-@set ADMB_HOME=%CD%
-
-REM
-REM
-REM
-@set MINGW_HOME=C:\MinGW
-
-
-REM
-REM Adds the bin folder to ADMB_HOME to system path.
-REM
-@set PATH=.;"%ADMB_HOME%"\bin;"%ADMB_HOME%"\utilities;%MINGW_HOME%\bin;%PATH%
-@echo Setting ADMB Home enviroment to "%ADMB_HOME%".
+@set "PATH=%CD%\bin;%CD%\utilities;%CD%\utilities\mingw32\bin;%PATH%"
 
 @cd "%USERPROFILE%"
 
 @echo.
+@echo ADMB Command Prompt for MinGW 32Bit
 @echo For help, type command "admb".
---
+++ scripts/mingw/set-admb-mingw64.bat
@@ -0,0 +1,9 @@
+@echo off
+
+@set "PATH=%CD%\bin;%CD%\utilities;%CD%\utilities\mingw64\bin;%PATH%"
+
+@cd "%USERPROFILE%"
+
+@echo.
+@echo ADMB Command Prompt for MinGW 64Bit
+@echo For help, type command "admb".

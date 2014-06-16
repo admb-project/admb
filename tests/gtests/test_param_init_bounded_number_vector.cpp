75e07c011d5b8131f78ad6c31bbb30e3a3a5326f
johnoel <johnoel@6fd93cbc-6849-4d5d-bcfc-d3e1d932c95a>
2014-06-16 22:04:18 +0000

    Fix reported bug http://lists.admb-project.org/pipermail/developers/2014-June/000982.html

---
+++ tests/gtests/test_param_init_bounded_number_vector.cpp
@@ -0,0 +1,45 @@
+#include <gtest/gtest.h>
+#include <admodel.h>
+
+class test_param_init_bounded_number_vector: public ::testing::Test {};
+
+TEST_F(test_param_init_bounded_number_vector, default_constructor)
+{
+  param_init_bounded_number_vector p;
+  EXPECT_EQ(0, p.allocated());
+  EXPECT_NE(0, p.indexmin());
+  EXPECT_NE(0, p.indexmax());
+}
+TEST_F(test_param_init_bounded_number_vector, set_initial_value)
+{
+  gradient_structure gs(1500);
+
+  param_init_bounded_number_vector p;
+  const int min = 1;
+  const int max = 3;
+  dvector mins(min, max);
+  mins.initialize();
+  dvector maxs(min, max);
+  maxs = 5;
+  std::string s("AString"); 
+  p.allocate(min, max, mins, maxs, s.c_str());
+  EXPECT_NE(0, p.allocated());
+  EXPECT_EQ(min, p.indexmin());
+  EXPECT_EQ(max, p.indexmax());
+
+  dvector v(min, max); 
+  v.initialize();
+
+  p.set_initial_value(v);
+  for (int i = min; i <= max; i++)
+  {
+    EXPECT_EQ(0, p[i]);
+    v[i] = i;
+  }
+
+  p.set_initial_value(v);
+  for (int i = min; i <= max; i++)
+  {
+    EXPECT_EQ(i, p[i]);
+  }
+}

07af6322ed9951a30af150b660d5dc3d59c6c484
johnoel <johnoel@6fd93cbc-6849-4d5d-bcfc-d3e1d932c95a>
2014-07-18 00:41:58 +0000

    Copy script instead of soft link since vbox shared drive does not allow it.

--- scripts/admb/root-admb.sh
+++ scripts/admb/root-admb.sh
@@ -6,17 +6,17 @@ shopt -s expand_aliases
 
 SCRIPT_DIR=$(cd `dirname $0` && pwd)
 
-if [ -f "$SCRIPT_DIR/build/dist/bin/admb" ]; then
+if [ -f "$SCRIPT_DIR/build/dist/bin/admb.sh" ]; then
   declare -rx ADMB_HOME="$SCRIPT_DIR/build/dist"
-  $ADMB_HOME/bin/admb $*
+  $ADMB_HOME/bin/admb.sh $*
   exit 0
 fi
 
-if [ -f "$SCRIPT_DIR/bin/admb" ]; then
+if [ -f "$SCRIPT_DIR/bin/admb.sh" ]; then
   declare -rx ADMB_HOME="$SCRIPT_DIR"
-  $ADMB_HOME/bin/admb $*
+  $ADMB_HOME/bin/admb.sh $*
   exit 0
 fi
 
-echo "Error: Unable to find 'admb'."
+echo "Error: Unable to find 'admb.sh'."
 exit 0

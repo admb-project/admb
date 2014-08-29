44d9c234e61bf3f03221432692aed4c0b0a6ddd4
johnoel <johnoel@6fd93cbc-6849-4d5d-bcfc-d3e1d932c95a>
2014-08-29 23:08:22 +0000

    Use streamsize for return type.

--- trunk/src/linad99/d3_io.cpp
+++ trunk/src/linad99/d3_io.cpp
@@ -38,8 +38,9 @@ ostream& operator<<(const ostream& _s, const d3_array& z)
 {
   ostream& s = (ostream&) _s;
 #ifdef __USE_IOSTREAM__
-  int new_w = s.width();
-  int new_p = s.precision();
+  using std::streamsize;
+  streamsize new_w = s.width();
+  streamsize new_p = s.precision();
 #if !defined(__cplusplus)
   long new_form = s.flags();
 #else

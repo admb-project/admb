5eea331be16d66654ba8391a8a2ed6b98129fa39
johnoel <johnoel@6fd93cbc-6849-4d5d-bcfc-d3e1d932c95a>
2014-09-03 21:31:33 +0000

    Remove comment code which is in dmat41.cpp.

--- trunk/src/linad99/fvar_m40.cpp
+++ trunk/src/linad99/fvar_m40.cpp
@@ -70,24 +70,6 @@ dvar_vector banded_lower_triangular_dvar_matrix::operator () (int i)
 }
 #endif
 
-/*
-void banded_symmetric_dmatrix::initialize(void)
-{
-  for (int i=rowmin();i<=rowmax();i++)
-  {
-    (*this)(i).initialize();
-  }
-}
-
-void banded_lower_triangular_dmatrix::initialize(void)
-{
-  for (int i=rowmin();i<=rowmax();i++)
-  {
-    (*this)(i).initialize();
-  }
-}
-*/
-
 /**
  * Description not yet available.
  * \param

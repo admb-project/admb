003aa0df7e8f973bcf0022ee3043d7b68b683784
johnoel <johnoel@6fd93cbc-6849-4d5d-bcfc-d3e1d932c95a>
2014-09-05 22:17:34 +0000

    Fix val function for hexadecimal strings.

--- trunk/src/tools99/val.cpp
+++ trunk/src/tools99/val.cpp
@@ -11,7 +11,7 @@ void val(const adstring& s, int& v, int& code)
 {
   int z;
   if ( (s.size() > 2) && (s(1,2) == adstring("0x") ))
-    z = sscanf((const char*)s, "%x", &v);
+    z = sscanf((const char*)s, "%i", &v);
   else
     z = sscanf((const char*)s, "%d", &v);
 

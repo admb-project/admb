07334446e29375cc961d743cd22a6261dd002181
johnoel <johnoel@6fd93cbc-6849-4d5d-bcfc-d3e1d932c95a>
2014-09-05 21:43:28 +0000

    Add checks for ptrdiff_t and memcpy.

--- tests/gtests/test_idea.cpp
+++ tests/gtests/test_idea.cpp
@@ -1,4 +1,5 @@
 #include <gtest/gtest.h>
+#include <inttypes.h>
 
 class test_idea: public ::testing::Test {};
 
@@ -88,4 +89,42 @@ TEST_F(test_idea, casting)
   long actual = long(-sizeof(off_t));
   long expected = -((long)sizeof(off_t));
   ASSERT_EQ(expected, actual);
+}
+TEST_F(test_idea, totalbytes)
+{
+  char s[10];
+  char* _a = &s[0];
+  char* _b = &s[9];
+
+  uintptr_t a = (uintptr_t)_a;
+  uintptr_t b = (uintptr_t)_b;
+  ptrdiff_t actual = b - a;
+  ASSERT_EQ(9, actual);
+
+  char* _c = &s[3];
+  char* _d = &s[7];
+  uintptr_t c = (uintptr_t)_c;
+  uintptr_t d = (uintptr_t)_d;
+  ptrdiff_t actual2 = d - c;
+  ASSERT_EQ(4, actual2);
+}
+TEST_F(test_idea, memcpy)
+{
+  char s[] = "12XX567YZ89\0";
+  char* source = &s[7];
+  char* destination = &s[2];
+  size_t num = 2;
+  memcpy(destination, source, num);
+  ASSERT_EQ('1', s[0]);
+  ASSERT_EQ('2', s[1]);
+  ASSERT_EQ('Y', s[2]);
+  ASSERT_EQ('Z', s[3]);
+  ASSERT_EQ('5', s[4]);
+  ASSERT_EQ('6', s[5]);
+  ASSERT_EQ('7', s[6]);
+  ASSERT_EQ('Y', s[7]);
+  ASSERT_EQ('Z', s[8]);
+  ASSERT_EQ('8', s[9]);
+  ASSERT_EQ('9', s[10]);
+  ASSERT_EQ('\0', s[11]);
 }

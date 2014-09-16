65dea9dc81d5a055aa84296f4e44db5697e3c03e
johnoel <johnoel@6fd93cbc-6849-4d5d-bcfc-d3e1d932c95a>
2014-09-16 23:46:30 +0000

    Simplify to_upper and to_lower.

--- trunk/src/tools99/strcase.cpp
+++ trunk/src/tools99/strcase.cpp
@@ -9,21 +9,17 @@
 void adstring::to_lower(void)
 {
   for (unsigned int i=1; i <= size(); i++)
-#ifdef _MSC_VER
-    s[i] = tolower(s[i]);
-#else
+  {
     s[i] = (unsigned char)std::tolower(s[i]);
-#endif
+  }
 }
 
 void adstring::to_upper(void)
 {
   for (unsigned int i=1; i <= size(); i++)
-#ifdef _MSC_VER
-    s[i] = toupper(s[i]);
-#else
+  {
     s[i] = (unsigned char)std::toupper(s[i]);
-#endif
+  }
 }
 
 adstring to_lower(adstring& s)

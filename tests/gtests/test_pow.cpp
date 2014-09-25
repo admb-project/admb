750db9cec432fb3bc825c5ae2ec062f0f1bb78b1
johnoel <johnoel@6fd93cbc-6849-4d5d-bcfc-d3e1d932c95a>
2014-09-25 22:14:30 +0000

    Skip some tests for windows.

--- tests/gtests/test_pow.cpp
+++ tests/gtests/test_pow.cpp
@@ -28,6 +28,7 @@ TEST_F(test_pow, exponent_ivector)
   ASSERT_EQ(2, ret(2));
   ASSERT_EQ(4, ret(3));
 }
+#ifndef _WIN32
 TEST_F(test_pow, base_is_int_max)
 {
   const int base = INT_MAX;
@@ -46,3 +47,4 @@ TEST_F(test_pow, exponent_is_int_max)
   exponent(3) = INT_MAX;
   EXPECT_DEATH(pow(base, exponent), "Assertion");
 }
+#endif

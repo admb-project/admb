8d65585cdc45655c6b338336cb43ff6d72b039aa
johnoel <johnoel@6fd93cbc-6849-4d5d-bcfc-d3e1d932c95a>
2014-10-01 19:35:01 +0000

    Fix up ::hess_determinant and added tests.

--- src/nh99/modhdet.cpp
+++ src/nh99/modhdet.cpp
@@ -5,52 +5,54 @@
  * Copyright (c) 2008-2012 Regents of the University of California
  */
 #include <admodel.h>
+#include <cassert>
 
 double function_minimizer::hess_determinant(int underflow_flag)
 {
- int sgn=0;
- double lndet=0.0;
- if (!underflow_flag)
- {
-  uistream ifs("admodel.hes");
-  if (!ifs)
+  double lndet=0.0;
+  if (!underflow_flag)
   {
-    cerr << "Error opening file admodel.hes" << endl;
-  }
-  int nvar = 0;
-  ifs >> nvar;
-  {
-    if (nvar != initial_params::nvarcalc())
-    {
-      cout << "the number of independent variables is wrong in admodel.hes"
-         << endl;
-    }
-    dmatrix h(1,nvar,1,nvar);
-    ifs >> h;
+    uistream ifs("admodel.hes");
     if (!ifs)
     {
-      cerr << "Error reading the hessian from file admodel.hes" << endl;
+      cerr << "Error opening file admodel.hes" << endl;
     }
-
-    for (int i=1;i<=nvar;i++)
+    int sgn = 0;
+    int nvar = 0;
+    ifs >> nvar;
+    if (nvar > 0)
     {
-      for (int j=1;j<i;j++)
+      if ((size_t)nvar != initial_params::nvarcalc())
       {
-        double tmp=(h(i,j)+h(j,i))/2.;
-        h(i,j)=tmp;
-        h(j,i)=tmp;
+        cout << "the number of independent variables"
+             << " is wrong in admodel.hes" << endl;
       }
+      dmatrix h(1,nvar,1,nvar);
+      ifs >> h;
+      if (!ifs)
+      {
+        cerr << "Error reading the hessian from file admodel.hes" << endl;
+      }
+
+      for (int i=1;i<=nvar;i++)
+      {
+        for (int j=1;j<i;j++)
+        {
+          double tmp=(h(i,j)+h(j,i))/2.;
+          h(i,j)=tmp;
+          h(j,i)=tmp;
+        }
+      }
+      lndet=ln_det(h,sgn);
     }
-    lndet=ln_det(h,sgn);
+    if (sgn <= 0)
+    {
+      cerr << "Error restricted Hessian is not positive definite" << endl;
+    }
   }
-  if (sgn <= 0)
+  else
   {
-    cerr << "Error restricted Hessian is not positive definite" << endl;
+    lndet=50.0;
   }
- }
- else
- {
-   lndet=50.0;
- }
- return lndet;
+  return lndet;
 }
---
+++ tests/gtests/finance.tpl
@@ -0,0 +1,36 @@
+GLOBALS_SECTION
+  double global_hess_determinant = 0;
+DATA_SECTION
+  init_int T
+  init_vector r(0,T)
+  vector sub_r(1,T)
+  number h0
+INITIALIZATION_SECTION
+  a0 .1
+  a1 .1
+  a2 .1
+PARAMETER_SECTION
+  init_bounded_number a0(0.0,1.0)
+  init_bounded_number a1(0.0,1.0,2)
+  init_bounded_number a2(0.0,1.0,3)
+  init_number Mean
+  vector eps2(1,T)
+  vector h(1,T)
+  objective_function_value log_likelihood
+PRELIMINARY_CALCS_SECTION
+  h0=square(std_dev(r));   // square forms the element-wise square 
+  sub_r=r(1,T);    // form a subvector so we can use vector operations
+  Mean=mean(r);    // calculate the mean of the vector r 
+PROCEDURE_SECTION
+  eps2=square(sub_r-Mean);   
+  h(1)=a0+a2*h0;
+  for (int t=2;t<=T;t++)
+  {
+    h(t)=a0+a1*eps2(t-1)+a2*h(t-1);
+  }
+  // calculate minus the log-likelihood function and assign it to
+  // the object of type objective_function_value
+  log_likelihood=.5*sum(log(h)+elem_div(eps2,h));  // elem_div performs  
+          // element-wise division of vectors
+FINAL_SECTION
+  global_hess_determinant = hess_determinant(0);

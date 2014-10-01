161c00dd1180c1a8cd2b25fdfbe5a850148b4c04
johnoel <johnoel@6fd93cbc-6849-4d5d-bcfc-d3e1d932c95a>
2014-10-01 20:21:59 +0000

    Use ASSERT_NEAR, just needs to near 1e-6

--- trunk/tests/gtests/test_finance.cpp
+++ trunk/tests/gtests/test_finance.cpp
@@ -18,16 +18,16 @@ TEST_F(test_finance, hess_determinant)
     finance(argc, argv);
 
     extern double global_hess_determinant;
-    ASSERT_DOUBLE_EQ(43.839847514172448, global_hess_determinant);
+    ASSERT_NEAR(43.839847514172448, global_hess_determinant, 1e-6);
 
     extern double global_projected_hess_determinant;
-    ASSERT_DOUBLE_EQ(28.314961861611934, global_projected_hess_determinant);
+    ASSERT_NEAR(28.314961861611934, global_projected_hess_determinant, 1e-6);
 
     extern double global_projected_hess_determinant_xscale;
-    ASSERT_DOUBLE_EQ(42.13010376845299, global_projected_hess_determinant_xscale);
+    ASSERT_NEAR(42.13010376845299, global_projected_hess_determinant_xscale, 1e-6);
 
     extern double global_ln_det_proj_jac;
-    ASSERT_DOUBLE_EQ(-0.00036865112372874741, global_ln_det_proj_jac);
+    ASSERT_NEAR(-0.00036865112372874741, global_ln_det_proj_jac, 1e-6);
 
     df1b2variable::pool = 0;
   }

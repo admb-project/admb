62723027684eec7e8fd672530ad56d58ec731213
johnoel <johnoel@6fd93cbc-6849-4d5d-bcfc-d3e1d932c95a>
2014-10-31 22:25:07 +0000

    Must throw.

--- trunk/tests/gtests/test_imatrix.cpp
+++ trunk/tests/gtests/test_imatrix.cpp
@@ -11,6 +11,8 @@ class test_imatrix: public ::testing::Test {};
 
 TEST_F(test_imatrix, rowmax_plus_1)
 {
+  ad_exit=&test_ad_exit;
+
   imatrix m(1, 4, 1, 3);
   m.initialize();
   ASSERT_THROW(m[5],int);

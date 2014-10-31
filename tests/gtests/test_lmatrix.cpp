6c2c303e8a1476164948dda776e68879b10d5e5d
johnoel <johnoel@6fd93cbc-6849-4d5d-bcfc-d3e1d932c95a>
2014-10-31 22:25:07 +0000

    Must throw.

--- tests/gtests/test_lmatrix.cpp
+++ tests/gtests/test_lmatrix.cpp
@@ -11,6 +11,8 @@ class test_lmatrix: public ::testing::Test {};
 
 TEST_F(test_lmatrix, rowmax_plus_1)
 {
+  ad_exit=&test_ad_exit;
+
   lmatrix m(1, 4, 1, 3);
   m.initialize();
   ASSERT_THROW(m[5],int);

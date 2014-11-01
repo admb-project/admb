7b592e1109c06a3f066f001e7aca4f032feb00a9
johnoel <johnoel@6fd93cbc-6849-4d5d-bcfc-d3e1d932c95a>
2014-11-01 08:34:09 +0000

    Should match types.

--- trunk/src/linad99/fvar.hpp
+++ trunk/src/linad99/fvar.hpp
@@ -741,8 +741,8 @@ class dependent_variables_information
    lvector cmpdif_buffer_position;
    lvector grad_file_position;
    lvector cmpdif_file_position;
-   lvector grad_file_count;
-   lvector cmpdif_file_count;
+  ivector grad_file_count;
+  ivector cmpdif_file_count;
    dependent_variables_information(int ndv);
    friend class gradient_structure;
 };

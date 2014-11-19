f05f7b1b744a13bcb2a6d34e1c356f2cf7015e43
johnoel <johnoel@6fd93cbc-6849-4d5d-bcfc-d3e1d932c95a>
2014-11-19 20:01:57 +0000

    Some cleanups to avoid msvc complaint.

--- trunk/src/linad99/fvar_m19.cpp
+++ trunk/src/linad99/fvar_m19.cpp
@@ -56,32 +56,30 @@ dvar_matrix operator*(const dvar_matrix& m1, const dmatrix& cm2)
      cerr << "Error: Unable to allocate array.\n";
      ad_exit(21);
    }
-
-   temp_col-=cm2.rowmin();
-
-   for (int j=cm2.colmin(); j<=cm2.colmax(); j++)
+   else
    {
-     for (int k=cm2.rowmin(); k<=cm2.rowmax(); k++)
+     temp_col-=cm2.rowmin();
+     for (int j=cm2.colmin(); j<=cm2.colmax(); j++)
      {
-       temp_col[k] = cm2.elem(k,j);
-     }
-
-     for (int i=cm1.rowmin(); i<=cm1.rowmax(); i++)
-     {
-       double sum=0.0;
-       dvector& temp_row = cm1(i);
-       for (int k=cm1.colmin(); k<=cm1.colmax(); k++)
+       for (int k=cm2.rowmin(); k<=cm2.rowmax(); k++)
        {
-          sum+=temp_row(k) * (temp_col[k]);
-         // sum+=temp_row(k) * cm2(k,j);
+         temp_col[k] = cm2.elem(k,j);
        }
-       tmp(i,j)=sum;
+       for (int i=cm1.rowmin(); i<=cm1.rowmax(); i++)
+       {
+         double sum=0.0;
+         dvector& temp_row = cm1(i);
+         for (int k=cm1.colmin(); k<=cm1.colmax(); k++)
+         {
+           sum+=temp_row(k) * (temp_col[k]);
+           // sum+=temp_row(k) * cm2(k,j);
+         }
+         tmp(i,j)=sum;
+       }
      }
+     temp_col+=cm2.rowmin();
+     free(temp_col);
    }
-
-
-   temp_col+=cm2.rowmin();
-   free ((char*)temp_col);
    dvar_matrix vtmp=nograd_assign(tmp);
    save_identifier_string("TEST1");
    //m1.save_dvar_matrix_value();

ca71f620b93ca864f4171f56a50d784d72a49e30
johnoel <johnoel@admb-project.org>
2015-05-29 12:39:30 -1000

    Add more ids for download counter.

--- scripts/admb-download-counter.py
+++ scripts/admb-download-counter.py
@@ -4,6 +4,14 @@ import json
 
 print datetime.date.today()
 
+data = urllib2.urlopen("https://api.github.com/repos/admb-project/admb/releases/1355046/assets").read()
+for o in json.loads(data):
+  print o['name'] + ': ' + str(o['download_count'])
+
+data = urllib2.urlopen("https://api.github.com/repos/admb-project/admb/releases/1355094/assets").read()
+for o in json.loads(data):
+  print o['name'] + ': ' + str(o['download_count'])
+
 data = urllib2.urlopen("https://api.github.com/repos/admb-project/admb/releases/1328565/assets").read()
 for o in json.loads(data):
   print o['name'] + ': ' + str(o['download_count'])

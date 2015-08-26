2301e89c2739840cd5fd01dd5b7e0d5f999b6a5d
John Sibert <sibert@hawaii.edu>
2015-08-26 09:30:44 -1000

    add dox for colsum and rowsum to dvect12.cpp

--- src/linad99/dvect12.cpp
+++ src/linad99/dvect12.cpp
@@ -24,9 +24,12 @@ double sum(const dvector &v)
     return tmp;
   }
 
-/**
- * Description not yet available.
- * \param
+/** \ingroup matop
+  Sum of the elements in each row of a matrix; constant object.
+  \param v Reference to a matrix.
+  \return A dvector, \f$s\f$, of length equal to the number of columns 
+   in the matrix.
+   \f$s_j=\sum_i v_{ij}\f$
  */
 dvector colsum(const dmatrix &v)
   {
@@ -48,9 +51,12 @@ dvector colsum(const dmatrix &v)
     return tmp;
   }
 
-/**
- * Description not yet available.
- * \param
+/** \ingroup matop
+  Sum of the elements in each column of a matrix; constant object.
+  \param v Reference to a matrix.
+  \return A dvector, \f$s\f$, of length equal to the number of rows 
+   in the matrix.
+   \f$s_i=\sum_j v_{ij}\f$
  */
 dvector rowsum(const dmatrix &v)
   {

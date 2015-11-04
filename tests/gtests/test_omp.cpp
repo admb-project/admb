329e5cf5b10e943e0e8930d5f6e710de0c9630d7
johnoel <johnoel@admb-project.org>
2015-11-04 12:47:24 -1000

    Modify omp test.

--- tests/gtests/test_omp.cpp
+++ tests/gtests/test_omp.cpp
@@ -10,7 +10,7 @@ class test_omp: public ::testing::Test {};
 TEST_F(test_omp, hello_world)
 {
   #pragma omp parallel
-  printf("hello_world: thread %d, nthreads %d\n", omp_get_thread_num(), omp_get_num_threads());
+  printf("thread: thread %d, nthreads %d\n", omp_get_thread_num(), omp_get_num_threads());
 }
 /*
 TEST_F(test_omp, dvector)

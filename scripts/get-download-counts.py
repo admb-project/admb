5476fe659181256f0f521d54e8c7e7589b101efa
johnoel <johnoel@admb-project.org>
2015-12-03 11:03:25 -1000

    Count admb-11.5pre.

--- scripts/get-download-counts.py
+++ scripts/get-download-counts.py
@@ -17,4 +17,8 @@ for o in json.loads(data):
 data = urllib2.urlopen("https://api.github.com/repos/admb-project/admb/releases/1328565/assets").read()
 for o in json.loads(data):
   print o['name'] + ': ' + str(o['download_count'])
+
+data = urllib2.urlopen("https://api.github.com/repos/admb-project/admb/releases/2223621/assets").read()
+for o in json.loads(data):
+  print o['name'] + ': ' + str(o['download_count'])
  

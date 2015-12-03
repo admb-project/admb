374db49fdef266381df9319853b815fefcfdeb01
johnoel <johnoel@admb-project.org>
2015-12-03 13:53:48 -1000

    Improve the download counting...older was limiting output.

--- scripts/get-download-counts.py
+++ scripts/get-download-counts.py
@@ -6,19 +6,27 @@ import json
 print datetime.date.today()
 print time.strftime("%H:%M:%S")
 
-data = urllib2.urlopen("https://api.github.com/repos/admb-project/admb/releases/2223621/assets").read()
-for o in json.loads(data):
-  print o['name'] + ': ' + str(o['download_count'])
+data = urllib2.urlopen("https://api.github.com/repos/admb-project/admb/releases/2223621").read()
+jsondata = json.loads(data)
+assets = jsondata['assets']
+for o in assets:
+  print o['browser_download_url'] + ': ' + str(o['download_count'])
 
-data = urllib2.urlopen("https://api.github.com/repos/admb-project/admb/releases/1355046/assets").read()
-for o in json.loads(data):
-  print o['name'] + ': ' + str(o['download_count'])
+data = urllib2.urlopen("https://api.github.com/repos/admb-project/admb/releases/1355046").read()
+jsondata = json.loads(data)
+assets = jsondata['assets']
+for o in assets:
+  print o['browser_download_url'] + ': ' + str(o['download_count'])
 
-data = urllib2.urlopen("https://api.github.com/repos/admb-project/admb/releases/1355094/assets").read()
-for o in json.loads(data):
-  print o['name'] + ': ' + str(o['download_count'])
+data = urllib2.urlopen("https://api.github.com/repos/admb-project/admb/releases/1355094").read()
+jsondata = json.loads(data)
+assets = jsondata['assets']
+for o in assets:
+  print o['browser_download_url'] + ': ' + str(o['download_count'])
 
-data = urllib2.urlopen("https://api.github.com/repos/admb-project/admb/releases/1328565/assets").read()
-for o in json.loads(data):
-  print o['name'] + ': ' + str(o['download_count'])
+data = urllib2.urlopen("https://api.github.com/repos/admb-project/admb/releases/1328565").read()
+jsondata = json.loads(data)
+assets = jsondata['assets']
+for o in assets:
+  print o['browser_download_url'] + ': ' + str(o['download_count'])
 

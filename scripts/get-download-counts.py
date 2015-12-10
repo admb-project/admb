db157afe88a207693d9e60909c1ef03a8bef4e0e
johnoel <johnoel@users.noreply.github.com>
2015-12-10 13:42:21 -1000

    Update get-download-counts.py

--- scripts/get-download-counts.py
+++ scripts/get-download-counts.py
@@ -2,31 +2,16 @@ import datetime
 import time
 import urllib2
 import json
+from array import array
+
+ids = array("i", [2268268, 2223621, 1355046, 1355094])
 
 print datetime.date.today()
 print time.strftime("%H:%M:%S")
 
-data = urllib2.urlopen("https://api.github.com/repos/admb-project/admb/releases/2223621").read()
-jsondata = json.loads(data)
-assets = jsondata['assets']
-for o in assets:
-  print o['browser_download_url'] + ': ' + str(o['download_count'])
-
-data = urllib2.urlopen("https://api.github.com/repos/admb-project/admb/releases/1355046").read()
-jsondata = json.loads(data)
-assets = jsondata['assets']
-for o in assets:
-  print o['browser_download_url'] + ': ' + str(o['download_count'])
-
-data = urllib2.urlopen("https://api.github.com/repos/admb-project/admb/releases/1355094").read()
-jsondata = json.loads(data)
-assets = jsondata['assets']
-for o in assets:
-  print o['browser_download_url'] + ': ' + str(o['download_count'])
-
-data = urllib2.urlopen("https://api.github.com/repos/admb-project/admb/releases/1328565").read()
-jsondata = json.loads(data)
-assets = jsondata['assets']
-for o in assets:
-  print o['browser_download_url'] + ': ' + str(o['download_count'])
-
+for id in ids:
+  data = urllib2.urlopen("https://api.github.com/repos/admb-project/admb/releases/" + str(id)).read()
+  jsondata = json.loads(data)
+  assets = jsondata['assets']
+  for o in assets:
+    print o['browser_download_url'] + ': ' + str(o['download_count'])

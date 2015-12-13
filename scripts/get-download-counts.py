9e49d3b6c1af443c76704344a5821f6fc58f5138
johnoel <johnoel@admb-project.org>
2015-12-13 11:49:51 -1000

    Use python string formating.

--- scripts/get-download-counts.py
+++ scripts/get-download-counts.py
@@ -4,13 +4,13 @@ import urllib2
 import json
 from array import array
 
-ids = array("i", [2275083, 2268268, 2223621, 1355046, 1355094, 1328565])
+ids = array("i", [2275083, 2223621, 1355046, 1355094, 1328565])
 
 print datetime.date.today()
 print time.strftime("%H:%M:%S")
 
 for id in ids:
-  data = urllib2.urlopen("https://api.github.com/repos/admb-project/admb/releases/" + str(id)).read()
+  data = urllib2.urlopen("https://api.github.com/repos/admb-project/admb/releases/{}".format(id)).read()
   jsondata = json.loads(data)
   assets = jsondata['assets']
   for o in assets:

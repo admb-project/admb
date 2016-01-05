7115334579d199ae247d31b33a5e86976a39f8bf
johnoel <johnoel@admb-project.org>
2016-01-05 11:11:27 -1000

    Add tests for objective_function_value.

--- src/nh99/model8.cpp
+++ src/nh99/model8.cpp
@@ -4,20 +4,26 @@
  * Author: David Fournier
  * Copyright (c) 2008-2012 Regents of the University of California
  */
-  #include <admodel.h>
+#include <admodel.h>
 
-  objective_function_value * objective_function_value::pobjfun=NULL;
-  double objective_function_value::gmax=0;
+objective_function_value* objective_function_value::pobjfun = NULL;
+double objective_function_value::gmax = 0;
 
-  objective_function_value::objective_function_value() {pobjfun=this;}
-  objective_function_value& objective_function_value::operator =
-    (const prevariable& v)
-  {
-    prevariable::operator = (v);
-    return *this;
-  }
+/**
+Default constructor
+*/
+objective_function_value::objective_function_value()
+{
+  pobjfun = this;
+}
+objective_function_value& objective_function_value::operator=
+  (const prevariable& v)
+{
+  prevariable::operator=(v);
+  return *this;
+}
 objective_function_value& objective_function_value::operator=(const double v)
-  {
-    prevariable::operator = (v);
-    return *this;
-  }
+{
+  prevariable::operator=(v);
+  return *this;
+}

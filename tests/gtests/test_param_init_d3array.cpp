ccd63077402e2b8a7ac0888a4c80c428b26ede01
johnoel <johnoel@admb-project.org>
2016-01-07 11:26:29 -1000

    Fix other typos.

--- tests/gtests/test_param_init_d3array.cpp
+++ tests/gtests/test_param_init_d3array.cpp
@@ -24,10 +24,10 @@ TEST_F(test_param_init_d3array, allocate_phase_start)
   ad_integer sl = 1;
   ad_integer sh = 2;
   index_type nrl = 3;
-  index_type nrh = 3;
-  index_type ncl = 4;
-  index_type nch = 5;
-  int expected_phase_start = 6;
+  index_type nrh = 4;
+  index_type ncl = 5;
+  index_type nch = 6;
+  int expected_phase_start = 7;
   
   p.allocate(sl, sh, nrl, nrh, ncl, nch, expected_phase_start, "p");
 
@@ -62,9 +62,9 @@ TEST_F(test_param_init_d3array, allocate_phase_start_default)
   ad_integer sl = 1;
   ad_integer sh = 2;
   index_type nrl = 3;
-  index_type nrh = 3;
-  index_type ncl = 4;
-  index_type nch = 5;
+  index_type nrh = 4;
+  index_type ncl = 5;
+  index_type nch = 6;
   int expected_phase_start = 1;
   
   p.allocate(sl, sh, nrl, nrh, ncl, nch, "p");
@@ -81,9 +81,9 @@ TEST_F(test_param_init_d3array, allocate_phase_start_int_default)
   int sl = 1;
   int sh = 2;
   int nrl = 3;
-  int nrh = 3;
-  int ncl = 4;
-  int nch = 5;
+  int nrh = 4;
+  int ncl = 5;
+  int nch = 6;
   int expected_phase_start = 1;
   
   p.allocate(sl, sh, nrl, nrh, ncl, nch, "p");

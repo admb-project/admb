a4cd9d62e6d0e60e248ad44900d5cd2f2c223ad7
johnoel <johnoel@admb-project.org>
2016-04-05 13:08:51 -1000

    Add document and tests for d3_array division operator.

--- tests/gtests/test_d3_array.cpp
+++ tests/gtests/test_d3_array.cpp
@@ -35,3 +35,67 @@ TEST_F(test_d3_array, cube)
   ASSERT_EQ(8, ret(2, 2, 1));
   ASSERT_EQ(8, ret(2, 2, 2));
 }
+TEST_F(test_d3_array, division)
+{
+  d3_array a(1, 2, 1, 2, 1, 2);
+
+  double d = 2.0;
+  for (int i = 1; i <= 2; ++i)
+  {
+    for (int j = 1; j <= 2; ++j)
+    {
+      for (int k = 1; k <= 2; ++k)
+      {
+        a(i, j, k) = d;
+        d += 2.0;
+      }
+    }
+  }
+
+  d3_array ret = a / 2.0;
+
+  d = 2.0;
+  for (int i = 1; i <= 2; ++i)
+  {
+    for (int j = 1; j <= 2; ++j)
+    {
+      for (int k = 1; k <= 2; ++k)
+      {
+        ASSERT_DOUBLE_EQ(d / 2.0, ret(i, j, k));
+        d += 2.0;
+      }
+    }
+  }
+}
+TEST_F(test_d3_array, division_member)
+{
+  d3_array a(1, 2, 1, 2, 1, 2);
+
+  double d = 2.0;
+  for (int i = 1; i <= 2; ++i)
+  {
+    for (int j = 1; j <= 2; ++j)
+    {
+      for (int k = 1; k <= 2; ++k)
+      {
+        a(i, j, k) = d;
+        d += 2.0;
+      }
+    }
+  }
+
+   a /= 2.0;
+
+  d = 2.0;
+  for (int i = 1; i <= 2; ++i)
+  {
+    for (int j = 1; j <= 2; ++j)
+    {
+      for (int k = 1; k <= 2; ++k)
+      {
+        ASSERT_DOUBLE_EQ(d / 2.0, a(i, j, k));
+        d += 2.0;
+      }
+    }
+  }
+}

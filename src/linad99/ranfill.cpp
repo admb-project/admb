aada085f8f2c9c0e218e2f522caf0522cc2feebf
johnoel <johnoel@admb-project.org>
2017-04-28 12:49:02 -1000

    Use constexpr in ranfill.cpp, added docs and fix conversion warnings.

--- src/linad99/ranfill.cpp
+++ src/linad99/ranfill.cpp
@@ -22,17 +22,17 @@
 
 #include <math.h>
 
-#define M1 714025
+constexpr int M1 = 714025;
 #define IA1 1366
 #define IC1 150889
-#define RM1 (1.0/M1)
+constexpr float RM1 = 1.0f / M1;
 #define M3 134456
 #define IA3 8121
 #define IC3 28411
-#define M2 243000
+constexpr int M2 = 243000;
 #define IA2 4561
 #define IC2 51349
-#define RM2 (1.0/M2)
+constexpr float RM2 = 1.0f / M2;
 
 double auto_rand(long int& idum, int reset);
 void reinitialize_auto_rand();
@@ -44,27 +44,26 @@ double randn(long int& n);
  */
 void reinitialize_auto_rand()
 {
-  long int One=1;
+  long int One = 1;
   auto_rand(One, -1);
 }
 
-/** Random number generator.
+/**
+Random number generator.
 
-    \n\n The implementation of this algorithm was inspired by
-    "Numerical Recipes in C", 2nd edition,
-    Press, Teukolsky, Vetterling, Flannery, chapter 7
+\n\n The implementation of this algorithm was inspired by
+"Numerical Recipes in C", 2nd edition,
+Press, Teukolsky, Vetterling, Flannery, chapter 7
 */
 double auto_rand(long int& idum, int reset)
 {
   static long ix1,ix2,ix3;
   static float r[108];
-  double temp;
   static int iff=0;
-  int j;
 
   if (reset < 0)
   {
-    iff =0;
+    iff = 0;
     return .5;
   }
 
@@ -82,7 +81,7 @@ double auto_rand(long int& idum, int reset)
     ix1=(IA1*ix1+IC1);
     ix1=ix1 % M1;
     ix3=ix1 % M3;
-    for (j=1;j<=107;j++)
+    for (int j=1;j<=107;j++)
     {
       ix2=(IA2*ix2+IC2) % M2;
       ix1=(IA1*ix1+IC1);
@@ -100,31 +99,27 @@ double auto_rand(long int& idum, int reset)
   ix1=ix1 % M1;
   ix2=(IA2*ix2+IC2) % M2;
   ix2=ix2 % M2;
-  j=1 + ((107*ix3)/M3);
+  long j = 1 + ((107*ix3)/M3);
   if (j > 107 || j < 1) cerr << " Error in random number generator\n";
-  temp=r[j];
+  double temp = r[j];
   r[j]=ix2*RM2;
   r[j]=(ix1+r[j]);
   r[j]=r[j]*RM1;
   return temp;
 }
 
-#undef M1
 #undef IA1
 #undef IC1
-#undef RM1
-#undef M2
 #undef IA2
 #undef IC2
-#undef RM2
 #undef M3
 #undef IA3
 #undef IC3
 
 /**
- * Description not yet available.
- * \param
- */
+Returns a random number.
+\param n seed
+*/
 double randn(long int& n)
 {
   long int nn=n;
@@ -133,174 +128,170 @@ double randn(long int& n)
   double u=sqrt(-2*log(x))*cos(2*PI*y);
   return(u);
 }
-
 /**
- * Description not yet available.
- * \param
- */
-  void dvector::fill_randbi(long int& n, double p)
+Fill vector with binary random numbers.  If random number is
+less than p, vector element is set to 1, otherwise it is set to
+0.
+\param n seed
+\param p value between 0 and 1
+*/
+void dvector::fill_randbi(long int& n, double p)
+{
+  if (p < 0.0 || p > 1.0)
   {
-    if ( p<0 || p>1)
-    {
-      cerr << "Error in dvar_vector::fill_randbi proportions of"
-       " successes must lie between 0 and 1\n";
-      ad_exit(1);
-    }
-    long int nn=n;
-    for (int i=indexmin(); i<=indexmax(); i++)
-    {
-      if (auto_rand(nn,1)<=p)
-      {
-        elem(i)=1;
-      }
-      else
-      {
-        elem(i)=0;
-      }
-    }
-    reinitialize_auto_rand();
+    cerr << "Error in dvar_vector::fill_randbi proportions of"
+            " successes must lie between 0 and 1.\n";
+    ad_exit(1);
   }
-
-/**
- * Description not yet available.
- * \param
- */
-  void dvector::fill_randu(long int& n)
+  long int nn = n;
+  for (int i = indexmin(); i <= indexmax(); ++i)
   {
-    long int nn=n;
-    for (int i=indexmin(); i<=indexmax(); i++)
+    if (auto_rand(nn, 1) <= p)
     {
-      elem(i)=auto_rand(nn,1);
+      elem(i) = 1.0;
     }
-    reinitialize_auto_rand();
+    else
+    {
+      elem(i) = 0.0;
+    }
   }
-
+  reinitialize_auto_rand();
+}
 /**
- * Description not yet available.
- * \param
- */
-void dmatrix::colfill_randu(const int &j, long int &n)
+Fill vector with random numbers.
+\param n seed
+*/
+void dvector::fill_randu(long int& n)
+{
+  long int nn=n;
+  for (int i = indexmin(); i <= indexmax(); ++i)
   {
-    long int nn=n;
-    for (int i=rowmin(); i<=rowmax(); i++)
-    {
-      elem(i,j)=auto_rand(nn,1);
-    }
-    reinitialize_auto_rand();
+    elem(i) = auto_rand(nn, 1);
   }
-
+  reinitialize_auto_rand();
+}
 /**
- * Description not yet available.
- * \param
- */
+Fill random numbers into specified column j of matrix.
+\param j specified column
+\param n seed
+*/
+void dmatrix::colfill_randu(const int& j, long int& n)
+{
+  long int nn = n;
+  for (int i = rowmin(); i <= rowmax(); ++i)
+  {
+      elem(i, j) = auto_rand(nn, 1);
+  }
+  reinitialize_auto_rand();
+}
+/**
+Fill random numbers into specified row i of matrix.
+\param i specified row
+\param n seed
+*/
 void dmatrix::rowfill_randu(const int& i, long int& n)
+{
+  long int nn = n;
+  for (int j = colmin(); j <= colmax(); ++j)
   {
-    long int nn=n;
-    for (int j=colmin(); j<=colmax(); j++)
-    {
-      elem(i,j)=auto_rand(nn,1);
-    }
-    reinitialize_auto_rand();
+    elem(i, j) = auto_rand(nn, 1);
   }
-
+  reinitialize_auto_rand();
+}
 /**
- * Description not yet available.
- * \param
- */
-  void dvector::fill_randn(long int& n)
+Fill vector with random numbers.
+\param n seed
+*/
+void dvector::fill_randn(long int& n)
+{
+  long int nn = n;
+  for (int i = indexmin(); i <= indexmax(); ++i)
   {
-    long int nn=n;
-    for (int i=indexmin(); i<=indexmax(); i++)
-    {
-      (*this)(i)=randn(nn);
-    }
-    reinitialize_auto_rand();
+    (*this)(i) = randn(nn);
   }
-
+  reinitialize_auto_rand();
+}
 /**
- * Description not yet available.
- * \param
- */
-  void dmatrix::fill_randn(long int& n)
+Fill matrix with random numbers.
+\param n seed
+*/
+void dmatrix::fill_randn(long int& n)
+{
+  long int nn = n;
+  for (int i = rowmin(); i <= rowmax(); ++i)
   {
-    long int nn=n;
-    for (int i=rowmin(); i<=rowmax(); i++)
-    {
-      elem(i).fill_randn_ni(nn);
-      nn+=2;
-    }
-    reinitialize_auto_rand();
+    elem(i).fill_randn_ni(nn);
+    nn += 2;
   }
-
+  reinitialize_auto_rand();
+}
 /**
- * Description not yet available.
- * \param
- */
-  void d3_array::fill_randn(long int& n)
+Fill multi array with random numbers.
+\param n seed
+*/
+void d3_array::fill_randn(long int& n)
+{
+  long int nn = n;
+  for (int i = slicemin(); i <= slicemax(); ++i)
   {
-    long int nn=n;
-    for (int i=slicemin(); i<=slicemax(); i++)
-    {
-      elem(i).fill_randn_ni(nn);
-      nn+=2;
-    }
-    reinitialize_auto_rand();
+    elem(i).fill_randn_ni(nn);
+    nn += 2;
   }
-
+  reinitialize_auto_rand();
+}
 /**
- * Description not yet available.
- * \param
- */
-  void d3_array::fill_randu(long int& n)
+Fill multi array with random numbers.
+\param n seed
+*/
+void d3_array::fill_randu(long int& n)
+{
+  long int nn = n;
+  for (int i = slicemin(); i <= slicemax(); ++i)
   {
-    long int nn=n;
-    for (int i=slicemin(); i<=slicemax(); i++)
-    {
-      elem(i).fill_randu_ni(nn);
-      nn+=2;
-    }
-    reinitialize_auto_rand();
+    elem(i).fill_randu_ni(nn);
+    nn += 2;
   }
-
+  reinitialize_auto_rand();
+}
 /**
- * Description not yet available.
- * \param
- */
-  void dmatrix::fill_randu(long int& n)
+Fill matrix with random numbers.
+\param n seed
+*/
+void dmatrix::fill_randu(long int& n)
+{
+  long int nn = n;
+  for (int i = rowmin(); i <= rowmax(); ++i)
   {
-    long int nn=n;
-    for (int i=rowmin(); i<=rowmax(); i++)
-    {
-      elem(i).fill_randu_ni(nn);
-      nn+=2;
-    }
-    reinitialize_auto_rand();
+    elem(i).fill_randu_ni(nn);
+    nn += 2;
   }
-
+  reinitialize_auto_rand();
+}
 /**
- * Description not yet available.
- * \param
- */
-void dmatrix::colfill_randn(const int &j,long int &n)
+Fill random numbers into specified column j of matrix.
+\param j specified column
+\param n seed
+*/
+void dmatrix::colfill_randn(const int& j,long int& n)
+{
+  long int nn=n;
+  for (int i = rowmin(); i <= rowmax(); ++i)
   {
-    long int nn=n;
-    for (int i=rowmin(); i<=rowmax(); i++)
-    {
-      elem(i,j)=randn(nn);
-    }
-    reinitialize_auto_rand();
+    elem(i, j) = randn(nn);
   }
-
+  reinitialize_auto_rand();
+}
 /**
- * Description not yet available.
- * \param
- */
+Fill random numbers into specified row i of matrix.
+\param i specified row
+\param n seed
+*/
 void dmatrix::rowfill_randn(const int& i, long int& n)
+{
+  long int nn = n;
+  for (int j = colmin(); j <= colmax(); ++j)
   {
-    long int nn=n;
-    for (int j=colmin(); j<=colmax(); j++)
-    {
-      elem(i,j)=randn(nn);
-    }
-    reinitialize_auto_rand();
+    elem(i, j) = randn(nn);
   }
+  reinitialize_auto_rand();
+}

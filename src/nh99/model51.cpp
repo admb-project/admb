63352cad60b0eea0152f1dde07fec14a83379efc
johnoel <johnoel@admb-project.org>
2017-05-04 10:20:23 -1000

    Add docs and fix vc C4458 warning by replacing parameter name.

--- src/nh99/model51.cpp
+++ src/nh99/model51.cpp
@@ -5,22 +5,22 @@
 #include <admodel.h>
 
 /**
-Allocate init_adstring with id name, then assign input string from
+Allocate init_adstring with id name_tag, then assign input string from
 opened ad_comm::global_datafile.
 */
-void init_adstring::allocate(const char* name)
+void init_adstring::allocate(const char* name_tag)
 {
   check_datafile_pointer(ad_comm::global_datafile);
-  model_name_tag::allocate(name);
+  model_name_tag::allocate(name_tag);
   *(ad_comm::global_datafile) >> *this;
 }
 /**
-Allocate init_line_adstring with id name, then assign input string from
-opened ad_comm::global_datafile.
+Allocate init_line_adstring with id name_tag, then assign input string
+from opened ad_comm::global_datafile.
 */
-void init_line_adstring::allocate(const char* name)
+void init_line_adstring::allocate(const char* name_tag)
 {
   check_datafile_pointer(ad_comm::global_datafile);
-  model_name_tag::allocate(name);
+  model_name_tag::allocate(name_tag);
   *(ad_comm::global_datafile) >> (const init_line_adstring&)(*this);
 }

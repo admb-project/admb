b318ff51734c7d0ce8b04af154deb06ebb1a4c0d
johnoel <johnoel@admb-project.org>
2017-05-10 09:44:18 -1000

    Add MSVC++ 2017 scripts.

---
+++ scripts/cl/set-admb-vc15-32bit.bat
@@ -0,0 +1,11 @@
+@echo off
+
+@pushd "%VS150COMNTOOLS%\..\..\VC" & call vcvarsall.bat x86 & popd
+
+@set "PATH=%CD%\bin;%CD%\utilities;%PATH%"
+
+@cd "%USERPROFILE%"
+
+@echo.
+@echo ADMB Command Prompt for Microsoft Visual C++ 2017 (32 Bit)
+@echo For help, type command "admb".

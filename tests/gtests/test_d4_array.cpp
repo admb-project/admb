ca78f9745c6ae95112cbf90cf95e1862b3b1dc54
johnoel <johnoel@admb-project.org>
2017-05-22 14:21:23 -1000

    Add more docs and tests for d4_array functions.

--- tests/gtests/test_d4_array.cpp
+++ tests/gtests/test_d4_array.cpp
@@ -118,7 +118,7 @@ TEST_F(test_d4_array, mfexp)
     }
   }
 }
-TEST_F(test_d4_array, log)
+TEST_F(test_d4_array, division_equal_operator)
 {
   d4_array a(1, 2, 1, 2, 1, 2, 1, 2);
 
@@ -138,7 +138,7 @@ TEST_F(test_d4_array, log)
     }
   }
 
-  d4_array ret = log(a);
+  a /= 0.5;
 
   d = 2.0;
   for (int i = 1; i <= 2; ++i)
@@ -149,7 +149,83 @@ TEST_F(test_d4_array, log)
       {
         for (int l = 1; l <= 2; ++l)
         {
-          ASSERT_DOUBLE_EQ(log(d), ret(i, j, k, l));
+          ASSERT_DOUBLE_EQ(d / 0.5, a(i, j, k, l));
+          d += 2.0;
+        }
+      }
+    }
+  }
+}
+TEST_F(test_d4_array, division_equal_operator2)
+{
+  d4_array a(1, 2, 1, 2, 1, 2, 1, 2);
+
+  double d = 2.0;
+  for (int i = 1; i <= 2; ++i)
+  {
+    for (int j = 1; j <= 2; ++j)
+    {
+      for (int k = 1; k <= 2; ++k)
+      {
+        for (int l = 1; l <= 2; ++l)
+        {
+          a(i, j, k, l) = d;
+          d += 2.0;
+        }
+      }
+    }
+  }
+
+  d4_array result = a / 0.5;
+
+  d = 2.0;
+  for (int i = 1; i <= 2; ++i)
+  {
+    for (int j = 1; j <= 2; ++j)
+    {
+      for (int k = 1; k <= 2; ++k)
+      {
+        for (int l = 1; l <= 2; ++l)
+        {
+          ASSERT_DOUBLE_EQ(d / 0.5, result(i, j, k, l));
+          d += 2.0;
+        }
+      }
+    }
+  }
+}
+TEST_F(test_d4_array, division_equal_operator3)
+{
+  d4_array a(1, 2, 1, 2, 1, 2, 1, 2);
+
+  double d = 2.0;
+  for (int i = 1; i <= 2; ++i)
+  {
+    for (int j = 1; j <= 2; ++j)
+    {
+      for (int k = 1; k <= 2; ++k)
+      {
+        for (int l = 1; l <= 2; ++l)
+        {
+          a(i, j, k, l) = d;
+          d += 2.0;
+        }
+      }
+    }
+  }
+
+  d4_array result = 0.5 / a;
+
+  d = 2.0;
+  for (int i = 1; i <= 2; ++i)
+  {
+    for (int j = 1; j <= 2; ++j)
+    {
+      for (int k = 1; k <= 2; ++k)
+      {
+        for (int l = 1; l <= 2; ++l)
+        {
+          ASSERT_DOUBLE_EQ(0.5 / d , result(i, j, k, l));
           d += 2.0;
         }
       }

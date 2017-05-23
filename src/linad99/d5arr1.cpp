abc11a5e1bf25ec396351bf7559a8a597889405f
johnoel <johnoel@admb-project.org>
2017-05-22 14:47:05 -1000

    Add docs and coverage tests for d5_array.

--- src/linad99/d5arr1.cpp
+++ src/linad99/d5arr1.cpp
@@ -1,38 +1,34 @@
-/*
- * $Id$
- *
+/**
  * Author: David Fournier
  * Copyright (c) 2008-2012 Regents of the University of California
- */
-/**
- * \file
- * Description not yet available.
  */
 #include "fvar.hpp"
 
 /**
- * Description not yet available.
- * \param
- */
-d5_array operator/(const d5_array& m, const double d)
-   {
-     d5_array tmp;
-     tmp.allocate(m);
-     for (int i=tmp.indexmin();i<=tmp.indexmax();i++)
-     {
-       tmp(i)=m(i)/d;
-     }
-     return tmp;
-   }
+Returns d5_array with all elements arr5 divided by d.
 
+\param arr5  numerator
+\param d denominator
+*/
+d5_array operator/(const d5_array& arr5, const double d)
+{
+  d5_array results;
+  results.allocate(arr5);
+  for (int i = results.indexmin(); i <= results.indexmax(); ++i)
+  {
+    results(i) = arr5(i) / d;
+  }
+  return results;
+}
 /**
- * Description not yet available.
- * \param
- */
+Divides all elements of d5_array by d.
+
+\param d denominator
+*/
 void d5_array::operator/=(const double d)
-   {
-     for (int i=indexmin();i<=indexmax();i++)
-     {
-       (*this)(i)/=d;
-     }
-   }
+{
+  for (int i = indexmin(); i <= indexmax(); ++i)
+  {
+    (*this)(i) /= d;
+  }
+}
--- tests/gtests/test_d5_array.cpp
+++ tests/gtests/test_d5_array.cpp
@@ -33,3 +33,91 @@ TEST_F(test_d5_array, allocate_vector)
   ASSERT_EQ(a.indexmin(), 3);
   ASSERT_EQ(a.indexmax(), 6);
 }
+TEST_F(test_d5_array, division_equal_operator)
+{
+  d5_array a(1, 2, 1, 2, 1, 2, 1, 2, 1, 2);
+
+  double d = 2.0;
+  for (int i = 1; i <= 2; ++i)
+  {
+    for (int j = 1; j <= 2; ++j)
+    {
+      for (int k = 1; k <= 2; ++k)
+      {
+        for (int l = 1; l <= 2; ++l)
+        {
+          for (int m = 1; m <= 2; ++m)
+          {
+            a(i, j, k, l, m) = d;
+            d += 2.0;
+          }
+        }
+      }
+    }
+  }
+
+  d5_array result = a / 0.5;
+
+  d = 2.0;
+  for (int i = 1; i <= 2; ++i)
+  {
+    for (int j = 1; j <= 2; ++j)
+    {
+      for (int k = 1; k <= 2; ++k)
+      {
+        for (int l = 1; l <= 2; ++l)
+        {
+          for (int m = 1; m <= 2; ++m)
+          {
+            ASSERT_DOUBLE_EQ(d / 0.5 , result(i, j, k, l, m));
+            d += 2.0;
+          }
+        }
+      }
+    }
+  }
+}
+TEST_F(test_d5_array, division_equal_operator2)
+{
+  d5_array a(1, 2, 1, 2, 1, 2, 1, 2, 1, 2);
+
+  double d = 2.0;
+  for (int i = 1; i <= 2; ++i)
+  {
+    for (int j = 1; j <= 2; ++j)
+    {
+      for (int k = 1; k <= 2; ++k)
+      {
+        for (int l = 1; l <= 2; ++l)
+        {
+          for (int m = 1; m <= 2; ++m)
+          {
+            a(i, j, k, l, m) = d;
+            d += 2.0;
+          }
+        }
+      }
+    }
+  }
+
+  a /= 0.5;
+
+  d = 2.0;
+  for (int i = 1; i <= 2; ++i)
+  {
+    for (int j = 1; j <= 2; ++j)
+    {
+      for (int k = 1; k <= 2; ++k)
+      {
+        for (int l = 1; l <= 2; ++l)
+        {
+          for (int m = 1; m <= 2; ++m)
+          {
+            ASSERT_DOUBLE_EQ(d / 0.5 , a(i, j, k, l, m));
+            d += 2.0;
+          }
+        }
+      }
+    }
+  }
+}

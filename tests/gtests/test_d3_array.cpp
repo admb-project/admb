7946793dabdb2f20489680d7a55acfe087416bb8
johnoel <johnoel@admb-project.org>
2017-06-29 12:18:06 -1000

    Add test and improvements for square(d3_array).

--- tests/gtests/test_d3_array.cpp
+++ tests/gtests/test_d3_array.cpp
@@ -763,3 +763,26 @@ TEST_F(test_d3_array, norm)
 
   ASSERT_DOUBLE_EQ(std::sqrt(204.0), ret);
 }
+TEST_F(test_d3_array, square)
+{
+  d3_array a(1, 2, 1, 2, 1, 2);
+  a(1, 1, 1) = 1;
+  a(1, 1, 2) = 2;
+  a(1, 2, 1) = 3;
+  a(1, 2, 2) = 4;
+  a(2, 1, 1) = 5;
+  a(2, 1, 2) = 6;
+  a(2, 2, 1) = 7;
+  a(2, 2, 2) = 8;
+
+  d3_array ret = square(a);
+
+  ASSERT_DOUBLE_EQ(std::pow(a(1, 1, 1), 2.0), ret(1, 1, 1));
+  ASSERT_DOUBLE_EQ(std::pow(a(1, 1, 2), 2.0), ret(1, 1, 2));
+  ASSERT_DOUBLE_EQ(std::pow(a(1, 2, 1), 2.0), ret(1, 2, 1));
+  ASSERT_DOUBLE_EQ(std::pow(a(1, 2, 2), 2.0), ret(1, 2, 2));
+  ASSERT_DOUBLE_EQ(std::pow(a(2, 1, 1), 2.0), ret(2, 1, 1));
+  ASSERT_DOUBLE_EQ(std::pow(a(2, 1, 2), 2.0), ret(2, 1, 2));
+  ASSERT_DOUBLE_EQ(std::pow(a(2, 2, 1), 2.0), ret(2, 2, 1));
+  ASSERT_DOUBLE_EQ(std::pow(a(2, 2, 2), 2.0), ret(2, 2, 2));
+}

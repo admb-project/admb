10d8df2ba5e66c873e969d98fd5f11806c34f959
johnoel <johnoel@admb-project.org>
2017-07-13 16:12:05 -1000

    Readd test for init_adstring.

--- tests/gtests/test_init_adstring.cpp
+++ tests/gtests/test_init_adstring.cpp
@@ -11,21 +11,24 @@ TEST_F(test_init_adstring, constructor)
   ASSERT_EQ(0, s.size());
   ASSERT_STREQ("", (char*)s);
 }
-/*
 TEST_F(test_init_adstring, allocate)
 {
-  init_adstring s;
+  ofstream ofs("test_init_adstring.dat");
+  ofs << "## ignore\n";
+  ofs << "__test_init_adstring__\n";
+  ofs.close();
 
   ASSERT_EQ(NULL, ad_comm::global_datafile);
 
   cifstream cif("test_init_adstring.dat");
   ad_comm::global_datafile = &cif;
 
+  init_adstring s;
   s.allocate("name");
   ASSERT_STREQ("name", s.get_name());
   ASSERT_EQ(22, s.size());
   ASSERT_STREQ("__test_init_adstring__", (char*)s);
 
+  cif.close();
   ad_comm::global_datafile = NULL;
 }
-*/

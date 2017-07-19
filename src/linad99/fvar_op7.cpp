14ea7cb0002138bf7df281bc7d316f517f9a1a9c
johnoel <johnoel@hawaii.edu>
2017-07-19 07:30:06 -1000

    Doc and improve variable square function.

--- src/linad99/fvar_op7.cpp
+++ src/linad99/fvar_op7.cpp
@@ -1,50 +1,41 @@
-/*
- * $Id$
- *
+/**
  * Author: David Fournier
  * Copyright (c) 2008-2012 Regents of the University of California
  */
-/**
- * \file
- * Description not yet available.
- */
 #include "fvar.hpp"
 
-/**
- * Description not yet available.
- * \param
- */
-double upick(double x)
-{
-  return x*x;
-}
+// double upick(double x) { return x*x; } 
 
 /**
- * Description not yet available.
- * \param
- */
-dvariable& square(const prevariable& v1)
+Return the computed square of variable, then set gradient_structure.
+
+\param variable prevariable
+*/
+dvariable& square(const prevariable& variable)
 {
-  double& x=value(v1);
   if (++gradient_structure::RETURN_PTR > gradient_structure::MAX_RETURN)
     gradient_structure::RETURN_PTR = gradient_structure::MIN_RETURN;
-  gradient_structure::RETURN_PTR->v->x=x*x;
-   gradient_structure::GRAD_STACK1->set_gradient_stack(default_evaluation2,
-     &(gradient_structure::RETURN_PTR->v->x), &(v1.v->x),2*x );
-   return(*gradient_structure::RETURN_PTR);
- }
 
+  double& x = value(variable);
+  gradient_structure::RETURN_PTR->v->x = x * x;
+
+  gradient_structure::GRAD_STACK1->set_gradient_stack(default_evaluation2,
+     &(gradient_structure::RETURN_PTR->v->x), &(variable.v->x), 2 * x );
+
+  return *gradient_structure::RETURN_PTR;
+}
 /**
- * Description not yet available.
- * \param
- */
-dvar_matrix square(const dvar_matrix& m)
-   {
-     dvar_matrix tmp;
-     tmp.allocate(m);
-     for (int i=tmp.rowmin();i<=tmp.rowmax();i++)
-     {
-       tmp(i)=square(m(i));
-     }
-     return tmp;
-   }
+Return results of computing the square of variable matrix.
+
+\param matrix variable matrix
+*/
+dvar_matrix square(const dvar_matrix& matrix)
+{
+  dvar_matrix results;
+  results.allocate(matrix);
+  for (int i = matrix.rowmin(); i <= matrix.rowmax(); ++i)
+  {
+    results(i) = square(matrix(i));
+  }
+  return results;
+}

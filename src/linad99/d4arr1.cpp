eba42cdf3ededd56a2732c78e3c6a8949d0ec3c5
johnoel <johnoel@admb-project.org>
2017-07-21 11:48:17 -1000

    Cleanups

--- src/linad99/d4arr1.cpp
+++ src/linad99/d4arr1.cpp
@@ -7,65 +7,65 @@
 
 /**
 Returns results in a d4_array with the computed square roots of the elements in
-arr4.
+darray.
 
-\param arr4 d4_array
+\param darray d4_array
 */
-d4_array sqrt(const d4_array& arr4)
+d4_array sqrt(const d4_array& darray)
 {
   d4_array results;
-  results.allocate(arr4);
+  results.allocate(darray);
   for (int i = results.hslicemin(); i <= results.hslicemax(); ++i)
   {
-    results(i) = sqrt(arr4(i));
+    results(i) = sqrt(darray(i));
   }
   return results;
 }
 /**
 Returns results in a d4_array with the computed exponential of the elements in
-arr4.
+darray.
 
-\param arr4 d4_array
+\param darray d4_array
 */
-d4_array exp(const d4_array& arr4)
+d4_array exp(const d4_array& darray)
 {
   d4_array results;
-  results.allocate(arr4);
+  results.allocate(darray);
   for (int i = results.hslicemin(); i <= results.hslicemax(); ++i)
   {
-    results(i) = exp(arr4(i));
+    results(i) = exp(darray(i));
   }
   return results;
 }
 /**
 Returns results in a d4_array with the computed exponential of the elements in
-arr4.
+darray.
 
-\param arr4 d4_array
+\param darray d4_array
 */
-d4_array mfexp(const d4_array& arr4)
+d4_array mfexp(const d4_array& darray)
 {
   d4_array results;
-  results.allocate(arr4);
+  results.allocate(darray);
   for (int i = results.hslicemin(); i <= results.hslicemax(); ++i)
   {
-    results(i) = mfexp(arr4(i));
+    results(i) = mfexp(darray(i));
   }
   return results;
 }
 /**
 Returns results in a d4_array with the computed log of the elements in
-arr4.
+darray.
 
-\param arr4 d4_array
+\param darray d4_array
 */
-d4_array log(const d4_array& arr4)
+d4_array log(const d4_array& darray)
 {
   d4_array results;
-  results.allocate(arr4);
+  results.allocate(darray);
   for (int i = results.hslicemin(); i <= results.hslicemax(); ++i)
   {
-    results(i) = log(arr4(i));
+    results(i) = log(darray(i));
   }
   return results;
 }

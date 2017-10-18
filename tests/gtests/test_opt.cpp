8865a3a5435fa1dc3be1f5c0c3d368cdf796f4f0
johnoel <johnoel@admb-project.org>
2017-10-17 14:47:42 -1000

    Opt dmatrix * dvector.

---
+++ tests/gtests/test_opt.cpp
@@ -0,0 +1,91 @@
+#include <vector>
+#include <gtest/gtest.h>
+#include <fvar.hpp>
+
+class test_opt: public ::testing::Test {};
+
+TEST_F(test_opt, carray_to_vector)
+{
+  double values[3];
+  values[0] = 4.1;
+  values[1] = 3.5;
+  values[2] = 1.4;
+
+  std::vector<double> v;
+  v.assign(&values[0], &values[0] + 3);
+  ASSERT_DOUBLE_EQ(values[0], *v.begin());
+  ASSERT_DOUBLE_EQ(values[1], v[1]);
+  ASSERT_DOUBLE_EQ(values[2], v[2]);
+}
+TEST_F(test_opt, dvector_to_vector)
+{
+  dvector values(1, 3);
+  values[1] = 4.1;
+  values[2] = 3.5;
+  values[3] = 1.4;
+
+  std::vector<double> v;
+  v.assign(values.begin(), values.end());
+  ASSERT_DOUBLE_EQ(values[1], *v.begin());
+  ASSERT_DOUBLE_EQ(values[2], v[1]);
+  ASSERT_DOUBLE_EQ(values[3], v[2]);
+}
+TEST_F(test_opt, dmatrix_times_dvector)
+{
+  dmatrix m(1, 3, 1, 3);
+  m(1, 1) = 1;
+  m(1, 2) = 2;
+  m(1, 3) = 3;
+  m(2, 1) = 4;
+  m(2, 2) = 5;
+  m(2, 3) = 6;
+  m(3, 1) = 7;
+  m(3, 2) = 8;
+  m(3, 3) = 9;
+  dvector v(1, 3);
+  v(1) = 1;
+  v(2) = 2;
+  v(3) = 3;
+  dvector results = m * v;
+  ASSERT_DOUBLE_EQ(14, results(1));
+  ASSERT_DOUBLE_EQ(32, results(2));
+  ASSERT_DOUBLE_EQ(50, results(3));
+}
+TEST_F(test_opt, dmatrix_times_dvector_opt)
+{
+  dmatrix m(1, 3, 1, 3);
+  m(1, 1) = 1;
+  m(1, 2) = 2;
+  m(1, 3) = 3;
+  m(2, 1) = 4;
+  m(2, 2) = 5;
+  m(2, 3) = 6;
+  m(3, 1) = 7;
+  m(3, 2) = 8;
+  m(3, 3) = 9;
+  dvector v(1, 3);
+  v(1) = 1;
+  v(2) = 2;
+  v(3) = 3;
+
+  dvector results(1, 3);
+  results.initialize();
+  dvector* ptr_m = m.begin();
+  double* ptr_r = results.begin();
+  for (int i = 1; i <= 3; ++i)
+  {
+    double* ptr_mv = ptr_m->begin();
+    double* ptr_v = v.begin();
+    for (int j = 1; j <= 3; ++j)
+    {
+      *ptr_r += *ptr_mv * *ptr_v;
+      ++ptr_mv;
+      ++ptr_v;
+    }
+    ++ptr_r;
+    ++ptr_m;
+  }
+  ASSERT_DOUBLE_EQ(14, results(1));
+  ASSERT_DOUBLE_EQ(32, results(2));
+  ASSERT_DOUBLE_EQ(50, results(3));
+}

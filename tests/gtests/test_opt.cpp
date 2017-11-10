6d02612c7e7b61f801ee02ac4bcb080c901af5b3
johnoel <johnoel@admb-project.org>
2017-11-09 14:28:57 -1000

    Try async...

--- tests/gtests/test_opt.cpp
+++ tests/gtests/test_opt.cpp
@@ -1,6 +1,7 @@
 #include <vector>
 #include <gtest/gtest.h>
 #include <thread>
+#include <future>
 #include <fvar.hpp>
 
 class test_opt: public ::testing::Test {};
@@ -70,7 +71,6 @@ TEST_F(test_opt, dmatrix_times_dvector_opt)
   v(3) = 3;
 
   dvector results(1, 3);
-  results.initialize();
   dvector* ptr_m = m.begin();
   double* ptr_r = results.begin();
   for (int i = 1; i <= 3; ++i)
@@ -90,45 +90,162 @@ TEST_F(test_opt, dmatrix_times_dvector_opt)
   ASSERT_DOUBLE_EQ(32, results(2));
   ASSERT_DOUBLE_EQ(50, results(3));
 }
-TEST_F(test_opt, dmatrix_times_dvector_large_ptrs_threaded)
+void myrange(const int imin, const int imax, dvector& v, dmatrix& m)
+{
+  dvector results(imin, imax);
+  for (int i = imin; i <= imax; ++i)
+  {
+    int j = v.indexmin();
+    double* pmi = &m(i)(j);
+    double* pv = &v(j);
+    double sum = *pmi * *pv;
+    for (; j <= v.indexmax(); ++j)
+    {
+      sum += *(++pmi) * *(++pv);
+    }
+    results[i] = sum;
+  }
+}
+TEST_F(test_opt, dmatrix_times_dvector_large_ptrs_async5)
 {
   int size = 10000;
   dmatrix m(1, size, 1, size);
   dvector v(1, size);
 
   dvector results(1, size);
-  results.initialize();
 
-  int imin = m.rowmin();
-  int imax = m.rowmax();
-  int imid = (m.rowmax() - m.rowmin()) / 2;
+  std::future<void>a(std::async(
+    &myrange, 1, 2000, std::ref(v), std::ref(m)));
+  std::future<void> b(std::async(
+    &myrange, 2001, 4000, std::ref(v), std::ref(m)));
+  std::future<void> c(std::async(
+    &myrange, 4001, 6000, std::ref(v), std::ref(m)));
+  std::future<void> d(std::async(
+    &myrange, 6001, 8000, std::ref(v), std::ref(m)));
+  std::future<void> e(std::async(
+    &myrange, 8001, 10000, std::ref(v), std::ref(m)));
+  a.get();
+  b.get();
+  c.get();
+  d.get();
+  e.get();
+}
+TEST_F(test_opt, dmatrix_times_dvector_large_ptrs_sequential5)
+{
+  int size = 10000;
+  dmatrix m(1, size, 1, size);
+  dvector v(1, size);
 
-  auto range = [&results](const int imin, const int imax, const dvector& v, const dmatrix& m)
-  {
-    dvector* ptr_m = m.begin() + (imin - 1);
-    double* ptr_r = results.begin() + (imin - 1);
-    for (int i = imin; i <= imax; ++i)
-    {
-      double* ptr_mv = ptr_m->begin();
-      double* ptr_v = v.begin();
-      double sum = 0.0;
-      for (int j = v.indexmin(); j <= v.indexmax(); ++j)
-      {
-        sum += *ptr_mv * *ptr_v;
-        ++ptr_mv;
-        ++ptr_v;
-      }
-      *ptr_r = sum;
-      ++ptr_r;
-      ++ptr_m;
-    }
-  };
+  dvector results(1, size);
 
-  std::thread lower(range, imin, imid, v, m);
-  std::thread upper(range, imid + 1, imax, v, m);
+  myrange(1, 2000, v, m);
+  myrange(2001, 4000, v, m);
+  myrange(4001, 6000, v, m);
+  myrange(6001, 8000, v, m);
+  myrange(8001, 10000, v, m);
+}
+TEST_F(test_opt, dmatrix_times_dvector_large_ptrs_threaded5)
+{
+  int size = 10000;
+  dmatrix m(1, size, 1, size);
+  dvector v(1, size);
 
-  lower.join();
-  upper.join();
+  dvector results(1, size);
+
+  std::thread a(&myrange, 1, 2000, std::ref(v), std::ref(m));
+  std::thread b(&myrange, 2001, 4000, std::ref(v), std::ref(m));
+  std::thread c(&myrange, 4001, 6000, std::ref(v), std::ref(m));
+  std::thread d(&myrange, 6001, 8000, std::ref(v), std::ref(m));
+  std::thread e(&myrange, 8001, 10000, std::ref(v), std::ref(m));
+  a.join();
+  b.join();
+  c.join();
+  d.join();
+  e.join();
+}
+TEST_F(test_opt, dmatrix_times_dvector_large_ptrs_async2)
+{
+  int size = 10000;
+  dmatrix m(1, size, 1, size);
+  dvector v(1, size);
+
+  dvector results(1, size);
+
+  std::future<void>a(std::async(&myrange,
+    1, 5000, std::ref(v), std::ref(m)));
+  std::future<void> b(std::async(&myrange,
+    5001, 10000, std::ref(v), std::ref(m)));
+  a.get();
+  b.get();
+}
+TEST_F(test_opt, dmatrix_times_dvector_large_ptrs_sequential2)
+{
+  int size = 10000;
+  dmatrix m(1, size, 1, size);
+  dvector v(1, size);
+
+  dvector results(1, size);
+
+  myrange(1, 5000, v, m);
+  myrange(5001, 10000, v, m);
+}
+TEST_F(test_opt, dmatrix_times_dvector_large_ptrs_threaded2)
+{
+  int size = 10000;
+  dmatrix m(1, size, 1, size);
+  dvector v(1, size);
+
+  dvector results(1, size);
+
+  std::thread a(&myrange, 1, 5000, std::ref(v), std::ref(m));
+  std::thread b(&myrange, 5001, 10000, std::ref(v), std::ref(m));
+  a.join();
+  b.join();
+}
+TEST_F(test_opt, dmatrix_times_dvector_large_ptrs_async3)
+{
+  int size = 10000;
+  dmatrix m(1, size, 1, size);
+  dvector v(1, size);
+
+  dvector results(1, size);
+
+  std::future<void>a(std::async(&myrange,
+    1, 3333, std::ref(v), std::ref(m)));
+  std::future<void> b(std::async(&myrange,
+    3334, 6666, std::ref(v), std::ref(m)));
+  std::future<void> c(std::async(&myrange,
+    6667, 10000, std::ref(v), std::ref(m)));
+  a.get();
+  b.get();
+  c.get();
+}
+TEST_F(test_opt, dmatrix_times_dvector_large_ptrs_sequential3)
+{
+  int size = 10000;
+  dmatrix m(1, size, 1, size);
+  dvector v(1, size);
+
+  dvector results(1, size);
+
+  myrange(1, 3333, v, m);
+  myrange(3334, 6666, v, m);
+  myrange(6666, 10000, v, m);
+}
+TEST_F(test_opt, dmatrix_times_dvector_large_ptrs_threaded3)
+{
+  int size = 10000;
+  dmatrix m(1, size, 1, size);
+  dvector v(1, size);
+
+  dvector results(1, size);
+
+  std::thread a(&myrange, 1, 3333, std::ref(v), std::ref(m));
+  std::thread b(&myrange, 3334, 6666, std::ref(v), std::ref(m));
+  std::thread c(&myrange, 6667, 10000, std::ref(v), std::ref(m));
+  a.join();
+  b.join();
+  c.join();
 }
 TEST_F(test_opt, dmatrix_times_dvector_large_ptrs)
 {
@@ -137,31 +254,27 @@ TEST_F(test_opt, dmatrix_times_dvector_large_ptrs)
   dvector v(1, size);
 
   dvector results(1, size);
-  results.initialize();
   dvector* ptr_m = m.begin();
-  double* ptr_r = results.begin();
   for (int i = 1; i <= size; ++i)
   {
     double* ptr_mv = ptr_m->begin();
     double* ptr_v = v.begin();
+    double sum = *ptr_mv * *ptr_v;
     for (int j = 1; j <= size; ++j)
     {
-      *ptr_r += *ptr_mv * *ptr_v;
-      ++ptr_mv;
-      ++ptr_v;
+      sum += *(++ptr_mv) * *(++ptr_v);
     }
-    ++ptr_r;
+    results[i] = sum;
     ++ptr_m;
   }
 }
-TEST_F(test_opt, dmatrix_times_dvector_large_array)
+TEST_F(test_opt, dmatrix_times_dvector_large_array_core)
 {
   int size = 10000;
   dmatrix m(1, size, 1, size);
   dvector v(1, size);
 
   dvector results(1, size);
-  results.initialize();
 
   results = m * v;
 }
@@ -255,4 +368,100 @@ TEST_F(test_opt, vector_times_matrix_core)
   {
     ASSERT_DOUBLE_EQ(0.5 * size, tmp(i));
   }
+}
+dvector myrange2(const int imin, const int imax, dvector& v, dmatrix& m)
+{
+  dvector results(imin, imax);
+  for (int i = imin; i <= imax; ++i)
+  {
+    int j = v.indexmin();
+    double* pmi = &m(i)(j);
+    double* pv = &v(j);
+    double sum = *pmi * *pv;
+    for (; j <= v.indexmax(); ++j)
+    {
+      sum += *(++pmi) * *(++pv);
+    }
+    results[i] = sum;
+  }
+
+  return results;
+}
+TEST_F(test_opt, dmatrix_times_dvector_large_ptrs_async_return_dvector2)
+{
+  int size = 10000;
+  dmatrix m(1, size, 1, size);
+  dvector v(1, size);
+
+  dvector results(1, size);
+
+  std::future<dvector>a(std::async(&myrange2,
+    1, 5000, std::ref(v), std::ref(m)));
+  std::future<dvector> b(std::async(&myrange2,
+    5001, 10000, std::ref(v), std::ref(m)));
+  dvector dva = a.get();
+  dvector dvb = b.get();
+}
+TEST_F(test_opt, dmatrix_times_dvector_large_ptrs_async_return_dvector3)
+{
+  int size = 10000;
+  dmatrix m(1, size, 1, size);
+  dvector v(1, size);
+
+  dvector results(1, size);
+
+  std::future<dvector>a(std::async(&myrange2,
+    1, 3333, std::ref(v), std::ref(m)));
+  std::future<dvector> b(std::async(&myrange2,
+    3334, 6666, std::ref(v), std::ref(m)));
+  std::future<dvector> c(std::async(&myrange2,
+    6667, 10000, std::ref(v), std::ref(m)));
+  dvector dva = a.get();
+  dvector dvb = b.get();
+  dvector dvc = c.get();
+}
+TEST_F(test_opt, dmatrix_times_dvector_large_ptrs_async_return_dvector4)
+{
+  int size = 10000;
+  dmatrix m(1, size, 1, size);
+  dvector v(1, size);
+
+  dvector results(1, size);
+
+  std::future<dvector>a(std::async(&myrange2,
+    1, 2500, std::ref(v), std::ref(m)));
+  std::future<dvector> b(std::async(&myrange2,
+    2501, 5000, std::ref(v), std::ref(m)));
+  std::future<dvector> c(std::async(&myrange2,
+    5001, 7500, std::ref(v), std::ref(m)));
+  std::future<dvector> d(std::async(&myrange2,
+    7501, 10000, std::ref(v), std::ref(m)));
+  dvector dva = a.get();
+  dvector dvb = b.get();
+  dvector dvc = c.get();
+  dvector dvd = d.get();
+}
+TEST_F(test_opt, dmatrix_times_dvector_large_ptrs_async_return_dvector5)
+{
+  int size = 10000;
+  dmatrix m(1, size, 1, size);
+  dvector v(1, size);
+
+  dvector results(1, size);
+
+  std::future<dvector>a(std::async(&myrange2,
+    1, 2000, std::ref(v), std::ref(m)));
+  std::future<dvector> b(std::async(&myrange2,
+    2001, 4000, std::ref(v), std::ref(m)));
+  std::future<dvector> c(std::async(&myrange2,
+    4001, 6000, std::ref(v), std::ref(m)));
+  std::future<dvector> d(std::async(&myrange2,
+    6001, 8000, std::ref(v), std::ref(m)));
+  std::future<dvector> e(std::async(&myrange2,
+    8001, 10000, std::ref(v), std::ref(m)));
+  dvector dva = a.get();
+  dvector dvb = b.get();
+  dvector dvc = c.get();
+  dvector dvd = d.get();
+  dvector dve = e.get();
 }

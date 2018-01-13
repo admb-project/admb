010a1d3d39bcb8b882673a98efded174a62205f9
johnoel <johnoel@hawaii.edu>
2018-01-12 17:15:20 -1000

    Add scripts for rtools distribution.

---
+++ scripts/mingw/set-admb-rtools.bat
@@ -0,0 +1,11 @@
+@echo off
+
+@set "ADMB_HOME=%CD%"
+
+@set "PATH=%CD%\bin;%CD%\utilities;%PATH%"
+
+@cd "%USERPROFILE%"
+
+@echo.
+@echo ADMB Command Prompt for Rtools
+@echo For help, type command "admb".

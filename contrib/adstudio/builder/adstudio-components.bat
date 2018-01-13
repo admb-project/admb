0a6f697f7a1b5a64051eac08757953d448eee407
Arni Magnusson <arnima@hafro.is>
2018-01-13 23:46:31 +0000

    Update components for AD Studio

--- contrib/adstudio/builder/adstudio-components.bat
+++ contrib/adstudio/builder/adstudio-components.bat
@@ -16,9 +16,9 @@ REM                                                                            #
 REM ############################################################################
 
 set ADMB=admb120-gcc493-win64.zip
-set AUCTEX=auctex-1191-texsite.zip
-set EMACS=emacs-25.2-x86_64.zip
-set ESS=ess-1610-github.zip
+set AUCTEX=auctex121-built.zip
+set EMACS=emacs-25.3_1-x86_64.zip
+set ESS=ess-17.11.zip
 set INNOEXTRACT=innoextract-1.6-windows.zip
 set RTOOLS=Rtools34.exe
 
@@ -39,39 +39,34 @@ echo on
 %WGET% %COMPS%/autodif.pdf
 %WGET% http://admb-project.org/tools/adstudio/adstudio.pdf
 %WGET% %COMPS%/%AUCTEX%
-%WGET% %COMPS%/%ESS%
+%WGET% http://ess.r-project.org/downloads/ess/%ESS%
 %WGET% %COMPS%/unzip.exe
 %WGET% http://ftp.gnu.org/gnu/emacs/windows/%EMACS%
 %WGET% http://constexpr.org/innoextract/files/%INNOEXTRACT%
 %WGET% https://jblevins.org/projects/markdown-mode/markdown-mode.el
 %WGET% https://cran.r-project.org/bin/windows/Rtools/%RTOOLS%
-%WGET% %GITHUB%/admb-project/admb/master/contrib/emacs/admb.el
-%WGET% %GITHUB%/admb-project/admb/master/contrib/emacs/LICENSE
-%WGET% %GITHUB%/admb-project/admb/master/contrib/emacs/NEWS
+%WGET% -P admb %GITHUB%/admb-project/admb/master/contrib/emacs/admb.el
+%WGET% -P admb %GITHUB%/admb-project/admb/master/contrib/emacs/LICENSE
+%WGET% -P admb %GITHUB%/admb-project/admb/master/contrib/emacs/NEWS
+%WGET% -P tmb %GITHUB%/kaskr/adcomp/master/emacs/tmb.el
+%WGET% -P tmb %GITHUB%/kaskr/adcomp/master/emacs/LICENSE
+%WGET% -P tmb %GITHUB%/kaskr/adcomp/master/emacs/NEWS
 %WGET% %GITHUB%/admb-project/admb/master/contrib/adstudio/dot/.emacs
-%WGET% %GITHUB%/admb-project/admb/master/contrib/ide/dot/LICENSE
-%WGET% %GITHUB%/admb-project/admb/master/contrib/ide/dot/NEWS
 %WGET% %GITHUB%/admb-project/admb/master/contrib/adstudio/icons/admb64.ico
 %WGET% %GITHUB%/admb-project/admb/master/contrib/adstudio/NEWS
-%WGET% %GITHUB%/kaskr/adcomp/master/emacs/tmb.el
-%WGET% %GITHUB%/kaskr/adcomp/master/emacs/LICENSE
-%WGET% %GITHUB%/kaskr/adcomp/master/emacs/NEWS
 @echo.
 
 @echo *** Unpacking components ...
 unzip -q -d %ADMB:.zip=% %ADMB%
 unzip -q -d auctex %AUCTEX%
-@rename %AUCTEX% temp.zip
-@move auctex* auctex >NUL
-@rename temp.zip %AUCTEX%
 unzip -q -d emacs %EMACS%
-unzip -q -d ess %ESS%
-@rename %ESS% temp.zip
-@move ess* ess >NUL
-@rename temp.zip %ESS%
-unzip -q %INNOEXTRACT%
-innoextract -q -dtemp %RTOOLS%
-@move temp\{app} Rtools >NUL
+unzip -q %ESS%
+@rename %ESS:~0,-4% ess
+unzip -q -d innoextract %INNOEXTRACT%
+@move innoextract\innoextract.exe . >NUL
+@rd /q /s innoextract
+innoextract -s -d temp %RTOOLS%
+@move temp\app Rtools >NUL
 @rd /q /s temp
 @popd
 @echo.

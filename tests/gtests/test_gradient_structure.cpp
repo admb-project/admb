13ba458db3f4a8794943bc7a775ebcd8efab4426
johnoel <johnoel@admb-project.org>
2018-02-05 10:51:11 -1000

    Redo test laters...need a gradient_structure::initialize().

--- tests/gtests/test_gradient_structure.cpp
+++ tests/gtests/test_gradient_structure.cpp
@@ -13,7 +13,6 @@ TEST_F(test_gradient_structure, checktypes)
 {
   ASSERT_EQ(sizeof(long long), sizeof(long long int));
 }
-/*
 TEST_F(test_gradient_structure, default_constructor)
 {
   gradient_structure gs;
@@ -27,19 +26,15 @@ TEST_F(test_gradient_structure, default_constructor2x)
     gradient_structure gs;
   }
 }
+/*
 TEST_F(test_gradient_structure, default_constructorb2b)
 {
-  ad_exit=&test_ad_exit;
-  try
-  {
+  ad_exit = &test_ad_exit;
+
+  ASSERT_ANY_THROW({
     gradient_structure gs;
     gradient_structure gs2;
-  }
-  catch (const int exit_code)          
-  {   
-    return;
-  }
-
-  FAIL();
+  });
+  gradient_structure gs;
 }
 */

7219ff4e1f348383c603b342125c5f8e480b0cea
johnoel <johnoel@admb-project.org>
2018-02-14 11:26:22 -1000

    Starting testing for gradcalc.

--- tests/gtests/test_simple_async.cpp
+++ tests/gtests/test_simple_async.cpp
@@ -215,7 +215,109 @@ TEST_F(test_simple_async, simple_final_values)
   ASSERT_EQ(gradient_structure::get_fp()->offset, 0);
 
   ASSERT_EQ(gradient_structure::GRAD_LIST->total_addresses(),
-            25 * gradient_structure::RETURN_ARRAYS_SIZE);
+            25 * gradient_structure::RETURN_ARRAYS_SIZE + 3);
+
+  ASSERT_DOUBLE_EQ(independents(1), 4.07817738582);
+  ASSERT_DOUBLE_EQ(independents(2), 1.90909098475);
+  ASSERT_DOUBLE_EQ(gradients(1), -2.0898189765639064e-05);
+  ASSERT_DOUBLE_EQ(gradients(2), -7.0013678170888216e-05);
+}
+TEST_F(test_simple_async, simple)
+{
+
+  independent_variables independents(1, 2);
+  independents(1) = 4.07817738582;
+  independents(2) = 1.90909098475;
+
+  gradient_structure gs;
+
+  dvar_vector variables(independents);
+
+  dvector x(1, 10); 
+  x(1) = -1.0;
+  x(2) = 0.0;
+  x(3) = 1.0;
+  x(4) = 2.0;
+  x(5) = 3.0;
+  x(6) = 4.0;
+  x(7) = 5.0;
+  x(8) = 6.0;
+  x(9) = 7.0;
+  x(10) = 8.0;
+
+  dvar_vector yhat(1, 10);
+
+  yhat = variables(1) + variables(2) * x; 
+
+  dvector y(1, 10); 
+  y(1) = 1.4;
+  y(2) = 4.7;
+  y(3) = 5.1;
+  y(4) = 8.3;
+  y(5) = 9.0;
+  y(6) = 14.5;
+  y(7) = 14.0;
+  y(8) = 13.4;
+  y(9) = 19.2;
+  y(10) = 18.0;
+
+  objective_function_value f;
+  f = regression(y, yhat);
+
+  dvector gradients(independents.indexmin(), independents.indexmax());
+
+  gradcalc(gradients.size(), gradients);
+
+  ASSERT_DOUBLE_EQ(independents(1), 4.07817738582);
+  ASSERT_DOUBLE_EQ(independents(2), 1.90909098475);
+  ASSERT_DOUBLE_EQ(gradients(1), -2.0898189765639064e-05);
+  ASSERT_DOUBLE_EQ(gradients(2), -7.0013678170888216e-05);
+}
+TEST_F(test_simple_async, simple)
+{
+
+  independent_variables independents(1, 2);
+  independents(1) = 4.07817738582;
+  independents(2) = 1.90909098475;
+
+  gradient_structure gs;
+
+  dvar_vector variables(independents);
+
+  dvector x(1, 10); 
+  x(1) = -1.0;
+  x(2) = 0.0;
+  x(3) = 1.0;
+  x(4) = 2.0;
+  x(5) = 3.0;
+  x(6) = 4.0;
+  x(7) = 5.0;
+  x(8) = 6.0;
+  x(9) = 7.0;
+  x(10) = 8.0;
+
+  dvar_vector yhat(1, 10);
+
+  yhat = variables(1) + variables(2) * x; 
+
+  dvector y(1, 10); 
+  y(1) = 1.4;
+  y(2) = 4.7;
+  y(3) = 5.1;
+  y(4) = 8.3;
+  y(5) = 9.0;
+  y(6) = 14.5;
+  y(7) = 14.0;
+  y(8) = 13.4;
+  y(9) = 19.2;
+  y(10) = 18.0;
+
+  objective_function_value f;
+  f = regression(y, yhat);
+
+  dvector gradients(independents.indexmin(), independents.indexmax());
+
+  gradcalc(gradients.size(), gradients);
 
   ASSERT_DOUBLE_EQ(independents(1), 4.07817738582);
   ASSERT_DOUBLE_EQ(independents(2), 1.90909098475);

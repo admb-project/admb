160de64e7835ce8361ba91353e67460ce0a867cd
johnoel <johnoel@admb-project.org>
2018-03-02 15:17:58 -1000

    Save for laters...

--- tests/gtests/test_arr_link.cpp
+++ tests/gtests/test_arr_link.cpp
@@ -50,3 +50,39 @@ TEST_F(test_arr_link, dvar_vector1to4)
   ASSERT_EQ(last->get_offset(), 0);
   ASSERT_EQ(last->get_status(), 1);
 }
+/*
+TEST_F(test_arr_link, initial_state_xpool)
+{
+  ASSERT_TRUE(arr_link::get_xpool() == NULL);
+
+  gradient_structure gs;
+
+  vector_shape_pool* xpool = arr_link::get_xpool();
+  ASSERT_TRUE(xpool != NULL);
+  ASSERT_EQ(xpool->nvar, 0);
+  ASSERT_TRUE(xpool->last_chunk == NULL);
+  ASSERT_EQ(xpool->num_allocated, 0);
+  ASSERT_EQ(xpool->num_chunks, 0);
+  ASSERT_EQ(xpool->nelem, 0);
+  ASSERT_EQ(xpool->size, sizeof(arr_link));
+  ASSERT_TRUE(xpool->head == NULL);
+  ASSERT_TRUE(xpool->first == NULL);
+
+  dvar_vector v(1, 4);
+
+  ASSERT_EQ(xpool->nvar, 0);
+  ASSERT_EQ(xpool->num_allocated, 1);
+  ASSERT_EQ(xpool->num_chunks, 1);
+  ASSERT_EQ(xpool->size, sizeof(arr_link));
+  const size_t overhead = 12 + sizeof(char*);
+  const size_t chunk_size = 65000 - overhead;
+  size_t expected = chunk_size / xpool->size;
+  ASSERT_EQ(xpool->nelem, expected);
+  ASSERT_TRUE(static_cast<void*>(xpool->last_chunk + sizeof(char*)) == static_cast<void*>(xpool->first));
+  void* ptr = static_cast<void*>(xpool->last_chunk + sizeof(char*));
+  dfpool::link* k = static_cast<dfpool::link*>(ptr);
+  cout << k << ' ' << xpool->head << endl;
+  cout << k->next << ' ' << xpool->head << endl;
+  ASSERT_TRUE(k->next == xpool->head);
+}
+*/

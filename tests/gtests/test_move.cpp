c1f00d0ddc1d790e53388ef77d77dbfb1a9ada3f
johnoel <johnoel@hawaii.edu>
2018-03-14 22:47:46 -1000

    Save...

--- tests/gtests/test_move.cpp
+++ tests/gtests/test_move.cpp
@@ -208,6 +208,42 @@ TEST_F(test_move, independents_gradcalc)
     return a + b;
   };
 
+
+  dvariable result = sum();
+  double v = value(result);
+
+  ASSERT_DOUBLE_EQ(value(variables(1)), 1.5);
+  ASSERT_DOUBLE_EQ(value(variables(2)), 3.5);
+
+  dvector g(1, 2);
+  gradcalc(2, g);
+
+  ASSERT_DOUBLE_EQ(g(1), 1.0);
+  ASSERT_DOUBLE_EQ(g(2), 1.0);
+  ASSERT_DOUBLE_EQ(value(variables(1)), 1.0);
+  ASSERT_DOUBLE_EQ(value(variables(2)), 1.0);
+}
+/*
+TEST_F(test_move, independents_gradmanual)
+{
+  independent_variables independents(1, 2);
+  independents(1) = 1.5;
+  independents(2) = 3.5;
+
+  gradient_structure gs;
+
+  ASSERT_EQ(0, gradient_structure::GRAD_STACK1->total());
+  ASSERT_EQ(1750, gradient_structure::GRAD_LIST->total_addresses());
+
+  dvar_vector variables(independents);
+
+  auto sum = [&variables]()
+  {
+    dvariable a(variables(1));
+    dvariable b(variables(2));
+    return a + b;
+  };
+
   dvariable result = sum();
   ASSERT_EQ(4, gradient_structure::GRAD_STACK1->total());
   ASSERT_EQ(1752, gradient_structure::GRAD_LIST->total_addresses());
@@ -215,48 +251,64 @@ TEST_F(test_move, independents_gradcalc)
   ASSERT_DOUBLE_EQ(value(result), 5.0);
 
   --gradient_structure::GRAD_STACK1->ptr;
-  ASSERT_DOUBLE_EQ(*(gradient_structure::GRAD_STACK1->ptr->dep_addr), 5.0);
-  ASSERT_DOUBLE_EQ(*(gradient_structure::GRAD_STACK1->ptr->ind_addr1), 5.0);
-  (*(gradient_structure::GRAD_STACK1->ptr->func))();
-  ASSERT_DOUBLE_EQ(*(gradient_structure::GRAD_STACK1->ptr->dep_addr), 0.0);
-  ASSERT_DOUBLE_EQ(*(gradient_structure::GRAD_STACK1->ptr->ind_addr1), 10.0);
-  ASSERT_TRUE(gradient_structure::GRAD_STACK1->ptr->ind_addr2 == NULL);
-  ASSERT_DOUBLE_EQ(gradient_structure::GRAD_STACK1->ptr->mult1, 0.0);
-  ASSERT_DOUBLE_EQ(gradient_structure::GRAD_STACK1->ptr->mult2, 0.0);
 
-  --gradient_structure::GRAD_STACK1->ptr;
-  ASSERT_DOUBLE_EQ(*(gradient_structure::GRAD_STACK1->ptr->dep_addr), 10.0);
+  double_and_int* ptr = (double_and_int*)gradient_structure::ARRAY_MEMBLOCK_BASE;
+  unsigned long int imax = gradient_structure::ARR_LIST1->get_max_last_offset() / sizeof(double_and_int);
+  for (unsigned int i = 0; i < imax; ++i)
+  {
+     ptr->x = 0.0;
+     ++ptr;
+  }
+  gradient_structure::GRAD_LIST->initialize();
+  *gradient_structure::GRAD_STACK1->ptr->dep_addr = 1.0;
+
+  //Sum
+  ASSERT_DOUBLE_EQ(*(gradient_structure::GRAD_STACK1->ptr->dep_addr), 1.0);
   ASSERT_DOUBLE_EQ(*(gradient_structure::GRAD_STACK1->ptr->ind_addr1), 0.0);
-  ASSERT_DOUBLE_EQ(*(gradient_structure::GRAD_STACK1->ptr->ind_addr2), 3.5);
   (*(gradient_structure::GRAD_STACK1->ptr->func))();
   ASSERT_DOUBLE_EQ(*(gradient_structure::GRAD_STACK1->ptr->dep_addr), 0.0);
-  ASSERT_DOUBLE_EQ(*(gradient_structure::GRAD_STACK1->ptr->ind_addr1), 10.0);
-  ASSERT_DOUBLE_EQ(*(gradient_structure::GRAD_STACK1->ptr->ind_addr2), 13.5);
-  ASSERT_DOUBLE_EQ(gradient_structure::GRAD_STACK1->ptr->mult1, 0.0);
-  ASSERT_DOUBLE_EQ(gradient_structure::GRAD_STACK1->ptr->mult2, 0.0);
-
-  --gradient_structure::GRAD_STACK1->ptr;
-  ASSERT_DOUBLE_EQ(*(gradient_structure::GRAD_STACK1->ptr->dep_addr), 13.5);
-  ASSERT_DOUBLE_EQ(*(gradient_structure::GRAD_STACK1->ptr->ind_addr1), 3.5);
-  (*(gradient_structure::GRAD_STACK1->ptr->func))();
-  ASSERT_DOUBLE_EQ(*(gradient_structure::GRAD_STACK1->ptr->dep_addr), 0.0);
-  ASSERT_DOUBLE_EQ(*(gradient_structure::GRAD_STACK1->ptr->ind_addr1), 17.0);
+  ASSERT_DOUBLE_EQ(*(gradient_structure::GRAD_STACK1->ptr->ind_addr1), 1.0);
   ASSERT_TRUE(gradient_structure::GRAD_STACK1->ptr->ind_addr2 == NULL);
   ASSERT_DOUBLE_EQ(gradient_structure::GRAD_STACK1->ptr->mult1, 0.0);
   ASSERT_DOUBLE_EQ(gradient_structure::GRAD_STACK1->ptr->mult2, 0.0);
 
+  //Addition
   --gradient_structure::GRAD_STACK1->ptr;
-  ASSERT_DOUBLE_EQ(*(gradient_structure::GRAD_STACK1->ptr->dep_addr), 10.0);
-  ASSERT_DOUBLE_EQ(*(gradient_structure::GRAD_STACK1->ptr->ind_addr1), 1.5);
+  ASSERT_DOUBLE_EQ(*(gradient_structure::GRAD_STACK1->ptr->dep_addr), 1.0);
+  ASSERT_DOUBLE_EQ(*(gradient_structure::GRAD_STACK1->ptr->ind_addr1), 0.0);
+  ASSERT_DOUBLE_EQ(*(gradient_structure::GRAD_STACK1->ptr->ind_addr2), 0.0);
   (*(gradient_structure::GRAD_STACK1->ptr->func))();
   ASSERT_DOUBLE_EQ(*(gradient_structure::GRAD_STACK1->ptr->dep_addr), 0.0);
-  ASSERT_DOUBLE_EQ(*(gradient_structure::GRAD_STACK1->ptr->ind_addr1), 11.5);
+  ASSERT_DOUBLE_EQ(*(gradient_structure::GRAD_STACK1->ptr->ind_addr1), 1.0);
+  ASSERT_DOUBLE_EQ(*(gradient_structure::GRAD_STACK1->ptr->ind_addr2), 1.0);
+  ASSERT_DOUBLE_EQ(gradient_structure::GRAD_STACK1->ptr->mult1, 0.0);
+  ASSERT_DOUBLE_EQ(gradient_structure::GRAD_STACK1->ptr->mult2, 0.0);
+
+  //Constructor
+  --gradient_structure::GRAD_STACK1->ptr;
+  ASSERT_DOUBLE_EQ(*(gradient_structure::GRAD_STACK1->ptr->dep_addr), 1.0);
+  ASSERT_DOUBLE_EQ(*(gradient_structure::GRAD_STACK1->ptr->ind_addr1), 0.0);
+  (*(gradient_structure::GRAD_STACK1->ptr->func))();
+  ASSERT_DOUBLE_EQ(*(gradient_structure::GRAD_STACK1->ptr->dep_addr), 0.0);
+  ASSERT_DOUBLE_EQ(*(gradient_structure::GRAD_STACK1->ptr->ind_addr1), 1.0);
+  ASSERT_TRUE(gradient_structure::GRAD_STACK1->ptr->ind_addr2 == NULL);
+  ASSERT_DOUBLE_EQ(gradient_structure::GRAD_STACK1->ptr->mult1, 0.0);
+  ASSERT_DOUBLE_EQ(gradient_structure::GRAD_STACK1->ptr->mult2, 0.0);
+
+  //Constructor
+  --gradient_structure::GRAD_STACK1->ptr;
+  ASSERT_DOUBLE_EQ(*(gradient_structure::GRAD_STACK1->ptr->dep_addr), 1.0);
+  ASSERT_DOUBLE_EQ(*(gradient_structure::GRAD_STACK1->ptr->ind_addr1), 0.0);
+  (*(gradient_structure::GRAD_STACK1->ptr->func))();
+  ASSERT_DOUBLE_EQ(*(gradient_structure::GRAD_STACK1->ptr->dep_addr), 0.0);
+  ASSERT_DOUBLE_EQ(*(gradient_structure::GRAD_STACK1->ptr->ind_addr1), 1.0);
   ASSERT_TRUE(gradient_structure::GRAD_STACK1->ptr->ind_addr2 == NULL);
   ASSERT_DOUBLE_EQ(gradient_structure::GRAD_STACK1->ptr->mult1, 0.0);
   ASSERT_DOUBLE_EQ(gradient_structure::GRAD_STACK1->ptr->mult2, 0.0);
 
   //ASSERT_DOUBLE_EQ(*gradient_structure::INDVAR_LIST->get_address(1), 0.0);
   //ASSERT_DOUBLE_EQ(*gradient_structure::INDVAR_LIST->get_address(2), 0.0);
-  ASSERT_DOUBLE_EQ(value(variables(1)), 11.5);
-  ASSERT_DOUBLE_EQ(value(variables(2)), 17.0);
+  ASSERT_DOUBLE_EQ(value(variables(1)), 1.0);
+  ASSERT_DOUBLE_EQ(value(variables(2)), 1.0);
 }
+*/

397b8d37b708c3cc8bfdaadad5f1f36e1accc56b
johnoel <johnoel@admb-project.org>
2018-04-05 09:59:26 -1000

    Update docs.

--- src/nh99/adsleep.cpp
+++ src/nh99/adsleep.cpp
@@ -1,12 +1,6 @@
-/*
- * $Id$
- *
+/**
  * Author: David Fournier
  * Copyright (c) 2008-2012 Regents of the University of California
- */
-/**
- * \file
- * Description not yet available.
  */
 #include <admodel.h>
 #include <stdlib.h>

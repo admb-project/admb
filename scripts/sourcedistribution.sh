ca90815acc9088ac1f7c55bd2406092d5f8c96fd
johnoel <johnoel@admb-project.org>
2018-06-21 14:36:09 -1000

    Use git checksum.

--- scripts/sourcedistribution.sh
+++ scripts/sourcedistribution.sh
@@ -2,5 +2,5 @@
 
 mkdir -p build/admb
 git show -s --format=%H HEAD > build/admb/GITVERSION
-git archive --verbose --format zip --prefix=admb/ --output build/admb-"`cat VERSION`"-src.zip HEAD .
-cd build && zip -u admb-"`cat ../VERSION`"-src.zip admb/GITVERSION
+git archive --verbose --format zip --prefix=admb/ --output build/admb-"`cat VERSION`"-"`git show -s --format=%h`"-src.zip HEAD .
+cd build && zip -u admb-"`cat ../VERSION`"-"`git show -s --format=%h`"-src.zip admb/GITVERSION

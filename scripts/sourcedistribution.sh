41f390e2e4c568e529d20618c74c77138ffc6625
johnoel <johnoel@admb-project.org>
2018-06-21 14:12:31 -1000

    Should put source distribution zip file in build.

--- scripts/sourcedistribution.sh
+++ scripts/sourcedistribution.sh
@@ -2,5 +2,5 @@
 
 mkdir -p build/admb
 git show -s --format=%H HEAD > build/admb/GITVERSION
-git archive --verbose --format zip --prefix=admb/ --output admb-"`cat VERSION`"-src.zip HEAD .
-cd build && zip -u ../admb-"`cat ../VERSION`"-src.zip admb/GITVERSION
+git archive --verbose --format zip --prefix=admb/ --output build/admb-"`cat VERSION`"-src.zip HEAD .
+cd build && zip -u admb-"`cat ../VERSION`"-src.zip admb/GITVERSION

b9ecb2b73d62a1f8eb034879657cb86d7cdfab6b
johnoel <johnoel@admb-project.org>
2018-06-21 15:12:28 -1000

    Fix path.

--- scripts/sourcedistribution.sh
+++ scripts/sourcedistribution.sh
@@ -4,5 +4,4 @@ mkdir -p build/admb
 git show -s --format=%H HEAD > build/admb/GITVERSION
 mkdir -p build/sourcedistribution
 git archive --verbose --format zip --prefix=admb/ --output build/sourcedistribution/admb-"`cat VERSION`"-"`git show -s --format=%h`"-src.zip HEAD .
-cd build && zip -u sourcedistribution/admb-"`cat ../VERSION`"-"`git show -s --format=%h`"-src.zip admb/GITVERSION
-cd build/sourcedistribution && ln -sf admb-"`cat ../VERSION`"-"`git show -s --format=%h`"-src.zip admb-latest-src.zip
+cd build && zip -u sourcedistribution/admb-"`cat ../VERSION`"-"`git show -s --format=%h`"-src.zip admb/GITVERSION && cd sourcedistribution && ln -sf admb-"`cat ../../VERSION`"-"`git show -s --format=%h`"-src.zip admb-latest-src.zip 

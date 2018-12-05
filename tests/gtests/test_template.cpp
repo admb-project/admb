d2c777083661e7d2324ee6135e40fc1794eab2d5
johnoel <johnoel@admb-project.org>
2018-12-05 11:27:37 -1000

    Save...

--- tests/gtests/test_template.cpp
+++ tests/gtests/test_template.cpp
@@ -1,19 +1,64 @@
 #include <gtest/gtest.h>
 #include <functional>
 #include <future>
+#include <admodel.h>
 
 class test_template: public ::testing::Test {};
 
 namespace admb
 {
+	/*
+  class independent_variable: public initial_params
+  {
+  public:
+  independent_variable()
+  {
+    initial_params::allocate(1);
+  }
+  independent_variable(const independent_variable&) = delete;
+  ~independent_variable()
+  {
+  }
+  independent_variable& operator=(const independent_variable&) = delete;
+  independent_variable& operator=(independent_variable&&) = delete;
+  };
+  */
+  class model: public function_minimizer
+  {
+  std::function<void()> _run;
+  public:
+  model()
+  {
+  }
+  model(const model&) = delete;
+  model(model&&) = delete;
+  ~model()
+  {
+  }
+  model& operator=(const model&) = delete;
+  model& operator=(model&&) = delete;
+  void set_userfunction(std::function<void()> run)
+  {
+    _run = run;
+  }
+  void userfunction()
+  {
+    _run();
+  }
+  };
+
   template <typename procedure_section>
   void minimize(procedure_section&& run)
   {
-    //begin loop
-    //gradient_structure gs(get_id());
-    run();    
-    //gradcalc();
-    //end loop
+    model m;
+
+    param_init_number b0;
+    b0.allocate("bo");
+
+    objective_function_value f;
+
+    m.set_userfunction(run);
+    m.minimize();
   }
 }
 
@@ -47,8 +92,11 @@ void simple()
   cout << __FILE__ << ':' << __LINE__ << endl;
 /*
   const int n = 10;
-  dvector x{-1, 0, 1, 2, 3, 4, 5, 6, 7, 8};
-  dvector y{1.4, 4.7, 5.1, 8.3, 9.0, 14.5, 14.0, 13.4, 19.2, 18.0};
+  dvector x("{-1, 0, 1, 2, 3, 4, 5, 6, 7, 8}");
+  cout << x << endl;
+  //dvector y{1.4, 4.7, 5.1, 8.3, 9.0, 14.5, 14.0, 13.4, 19.2, 18.0};
+*/
+/*
   independent_variable b0;   
   independent_variable b1;   
   dvar_vector yhat;
@@ -62,6 +110,11 @@ TEST_F(test_template, lamda)
 {
   admb::minimize([]() { return simple(); });
 }
+TEST_F(test_template, lamda2)
+{
+  admb::minimize([]() { return simple(); });
+}
+/*
 TEST_F(test_template, simple)
 {
   admb::minimize(simple);
@@ -70,6 +123,7 @@ TEST_F(test_template, async_minimize_simple)
 {
   std::async([]()
   {
-    admb::minimize(simple);
+    admb::minimize([]() { return simple(); });
   });
 }
+*/

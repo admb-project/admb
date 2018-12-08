a797d48fbb4d4082015ca6c96b05f6b3d2396d1c
johnoel <johnoel@admb-project.org>
2018-12-07 15:28:56 -1000

    Update.

--- tests/gtests/test_template.cpp
+++ tests/gtests/test_template.cpp
@@ -7,33 +7,26 @@ class test_template: public ::testing::Test {};
 
 namespace admb
 {
-	/*
-  class independent_variable: public initial_params
+  class independent_variable: public named_dvariable
   {
   public:
-  independent_variable()
-  {
-    initial_params::allocate(1);
-  }
+  independent_variable() { }
   independent_variable(const independent_variable&) = delete;
-  ~independent_variable()
-  {
-  }
+  independent_variable(independent_variable&&) = delete;
+  ~independent_variable() { }
   independent_variable& operator=(const independent_variable&) = delete;
   independent_variable& operator=(independent_variable&&) = delete;
   };
-  */
+
   class model: public function_minimizer
   {
   std::function<void()> _run;
 
   public:
-  model() 
-  { 
-    initial_params::varsptr.initialize();
-  }
+  model() = delete;
   model(const std::function<void()>& run): _run(run) 
   { 
+    // Points to independent variables
     initial_params::varsptr.initialize();
   }
   model(const model&) = delete;
@@ -42,10 +35,6 @@ namespace admb
 
   model& operator=(const model&) = delete;
   model& operator=(model&&) = delete;
-  void set_userfunction(std::function<void()> run)
-  {
-    _run = run;
-  }
   void userfunction()
   {
     _run();
@@ -91,37 +80,50 @@ PROCEDURE_SECTION
   f=regression(y,yhat);
 */
 
+void empty() {}
+
+//void parameters(independent_variable& b0, independent_variable& b1) {}
+
 void simple()
 {
   cout << __FILE__ << ':' << __LINE__ << endl;
 /*
-  const int n = 10;
   dvector x("{-1, 0, 1, 2, 3, 4, 5, 6, 7, 8}");
-  cout << x << endl;
-  //dvector y{1.4, 4.7, 5.1, 8.3, 9.0, 14.5, 14.0, 13.4, 19.2, 18.0};
-*/
-/*
+  dvector y{1.4, 4.7, 5.1, 8.3, 9.0, 14.5, 14.0, 13.4, 19.2, 18.0};
+
   independent_variable b0;   
   independent_variable b1;   
-  dvar_vector yhat;
-  yhat = b0 + b1 * x;
-  dvariable f = regression(y, yhat);
-  return std::move(f);
+  dvar_vector yhat = b0 + b1 * x;
+  return regression(y, yhat);
 */
 }
 
-TEST_F(test_template, simple)
+TEST_F(test_template, empty)
 {
-  admb::minimize(simple);
+  admb::minimize(empty);
 }
 TEST_F(test_template, lamda)
 {
-  admb::minimize([]() { return simple(); });
+  admb::minimize([]() { return empty(); });
 }
-TEST_F(test_template, async_minimize_simple)
+TEST_F(test_template, async_minimize_empty)
 {
   std::async([]()
   {
-    admb::minimize([]() { return simple(); });
+    admb::minimize([]() { return empty(); });
   });
 }
+TEST_F(test_template, sequence_2xempty)
+{
+  admb::minimize(empty);
+  admb::minimize(empty);
+}
+/*
+TEST_F(test_template, parameters)
+{
+  independent_variable a;
+  independent_variable b;
+
+  admb::minimize([&a, &b]() { return parameters(a, b); });
+}
+*/

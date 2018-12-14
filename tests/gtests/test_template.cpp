687732ee272452d9c534704f70b92e423fa59b8f
johnoel <johnoel@admb-project.org>
2018-12-13 15:46:15 -1000

    Use base...

--- tests/gtests/test_template.cpp
+++ tests/gtests/test_template.cpp
@@ -7,28 +7,24 @@ class test_template: public ::testing::Test {};
 
 namespace admb
 {
-  /// variable has a value, derivative and name.
-  typedef std::tuple<double, double, std::string> variable;
-  /// bounds has a value, minimum and maximum bounds.
-  typedef std::tuple<double, double, double> bounds;
-  /// bounded_variable has bounds, derivative and name.
-  typedef std::tuple<bounds, double, std::string> bounded_variable;
+/// variable has a value, derivative and name.
+typedef std::tuple<double, double, std::string> parameter;
+/// bounds has a value, minimum and maximum bounds.
+typedef std::tuple<double, double, double> bounds;
+/// bounded_variable has bounds, derivative and name.
+typedef std::tuple<bounds, double, std::string> bounded_parameter;
 
-  class model: public function_minimizer
-  {
-  std::function<dvariable(dvariable&, dvariable&)> _run;
-  param_init_number b0;
-  param_init_number b1;
+template <typename base>
+class model: public function_minimizer, base
+{
   objective_function_value f;
 
-  public:
-  model(const std::function<dvariable(dvariable&, dvariable&)>& run): _run(run) 
+public:
+  model(): function_minimizer(), base()
   { 
     // Points to independent variables
     initial_params::varsptr.initialize();
-
-    b0.allocate("b0");
-    b1.allocate("b1");
+    base::allocate();
   }
   model(const model&) = delete;
   model(model&&) = delete;
@@ -38,18 +34,16 @@ namespace admb
   model& operator=(model&&) = delete;
   void userfunction()
   {
-    //f = _parameters.run();
-    f = _run(b0, b1);
+    f = base::run();
   }
-  };
+};
 
-  template <typename procedure_section>
-  void minimize(const procedure_section& run)
-  {
-    model m(run);
-
-    m.minimize();
-  }
+template <typename parameters>
+void minimize()
+{
+  model<parameters> m;
+  m.minimize();
+}
 }
 
 using std::cout;
@@ -77,23 +71,30 @@ PROCEDURE_SECTION
   f=regression(y,yhat);
 */
 
-TEST_F(test_template, lamda)
+TEST_F(test_template, model)
 {
-	/*
-  gradient_structure gs;
-
-  param_init_number a0;
-  param_init_number a1;
-  a0.allocate("a0");
-  a1.allocate("a1");
-  */
-
-  dvector x("{-1, 0, 1, 2, 3, 4, 5, 6, 7, 8}");
-  dvector y("{1.4, 4.7, 5.1, 8.3, 9.0, 14.5, 14.0, 13.4, 19.2, 18.0}");
-  admb::minimize([&x, &y](dvariable& b0, dvariable& b1)
+  struct model
   {
-    dvar_vector yhat = b0 + b1 * x;
-    return regression(y, yhat);
+    param_init_number b0;
+    param_init_number b1;
+
+    void allocate()
+    {
+      b0.allocate("b0");
+      b1.allocate("b1");
+    }
+    auto run()
+    { 
+      dvector x("{-1, 0, 1, 2, 3, 4, 5, 6, 7, 8}");
+      dvector y("{1.4, 4.7, 5.1, 8.3, 9.0, 14.5, 14.0, 13.4, 19.2, 18.0}");
+
+      dvar_vector yhat = b0 + b1 * x;
+      return regression(y, yhat);
+    }
+  };
+  std::async([]()
+  {
+    admb::minimize<model>();
   });
 }
 

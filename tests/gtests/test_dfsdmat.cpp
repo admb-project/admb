17dbb1220f6a69e6d1cfafb2081b02401fa47c33
johnoel <johnoel@admb-project.org>
2019-02-02 16:58:55 -1000

    Add tests for dfsdmat and gradient_structure::get_hessian_ptr().

--- tests/gtests/test_dfsdmat.cpp
+++ tests/gtests/test_dfsdmat.cpp
@@ -7,8 +7,28 @@ TEST_F(test_dfsdmat, default_constructor)
 {
   gradient_structure gs;
 
+  ASSERT_FALSE(gradient_structure::get_USE_FOR_HESSIAN());
   dfsdmat mat;
   ASSERT_TRUE(!mat);
+}
+TEST_F(test_dfsdmat, default_int_constructor_without_USE_FOR_HESSIAN)
+{
+  gradient_structure gs;
 
+  ASSERT_FALSE(gradient_structure::get_USE_FOR_HESSIAN());
+
+  dfsdmat mat(1);
+  ASSERT_FALSE(!mat);
+  ASSERT_TRUE(mat.get_ptr() != gradient_structure::get()->hessian_ptr);
+}
+TEST_F(test_dfsdmat, default_int_constructor_with_USE_FOR_HESSIAN)
+{
+  gradient_structure gs;
+
+  gradient_structure::set_USE_FOR_HESSIAN(1);
   ASSERT_TRUE(gradient_structure::get_USE_FOR_HESSIAN());
+
+  dfsdmat mat(1);
+  ASSERT_FALSE(!mat);
+  ASSERT_TRUE(mat.get_ptr() == gradient_structure::get()->hessian_ptr);
 }

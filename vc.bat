56c07a6ef3cf4f33a62272c5df2958237e662a65
johnoel <johnoel@admb-project.org>
2019-02-05 20:21:19 -1000

    use correct option.

--- vc.bat
+++ vc.bat
@@ -1,3 +1,3 @@
 
-CALL "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\VC\Auxiliary\Build\vcvarsall.bat" x86_64
+CALL "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\VC\Auxiliary\Build\vcvarsall.bat" amd64
 nmake

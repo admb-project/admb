dd11164ee48f26214f2ff4937cb6e52ecd26d3cb
johnoel <johnoel@admb-project.org>
2019-02-07 13:32:01 -1000

    Call gtests.

--- utilities/travis-windows-vc-build.bat
+++ utilities/travis-windows-vc-build.bat
@@ -1,3 +1,3 @@
 
 CALL "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\VC\Auxiliary\Build\vcvarsall.bat" amd64
-nmake CXX=cl CC=cl clean dist test
+nmake CXX=cl CC=cl clean dist gtests test

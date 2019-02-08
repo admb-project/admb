7201648934ea6ab6ca5afdaebf9ab80b4241b07e
johnoel <johnoel@admb-project.org>
2019-02-07 16:57:27 -1000

    Try...

--- utilities/travis-windows-vc-build.bat
+++ utilities/travis-windows-vc-build.bat
@@ -1,3 +1,4 @@
 
 CALL "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\VC\Auxiliary\Build\vcvarsall.bat" amd64
-nmake CXX=cl CC=cl clean dist gtests test
+nmake CXX=cl CC=cl clean dist test
+pushd tests & pushd gtests & nmake all

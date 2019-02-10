40822e1d9bcc17b5b3a37edb81bcbe6d7b9e3933
johnoel <johnoel@admb-project.org>
2019-02-10 10:58:33 -1000

    Do only safe for cl /analyze.

--- utilities/travis-windows-vc-build-analyze.bat
+++ utilities/travis-windows-vc-build-analyze.bat
@@ -1,4 +1,4 @@
 
 CALL "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\VC\Auxiliary\Build\vcvarsall.bat" amd64
 nmake clean
-pushd src & nmake CXX=cl CC=cl CXXFLAGS=/analyze objects
+pushd src & nmake CXX=cl CC=cl SAFE_ONLY=yes CXXFLAGS=/analyze objects

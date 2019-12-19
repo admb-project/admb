0408801dce9b0861c1380de673ef1ec94aac7a73
johnoel <johnoel@admb-project.org>
2019-12-19 09:31:01 -1000

    Remove unused file.

--- scripts/ci/travis-windows-vc-build-analyze.bat
+++
@@ -1,4 +0,0 @@
-
-CALL "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\VC\Auxiliary\Build\vcvarsall.bat" amd64
-nmake CXX=cl clean
-pushd src & nmake CXX=cl CC=cl SAFE_ONLY=yes CXXFLAGS=/analyze %1

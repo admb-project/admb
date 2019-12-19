f771278b2af3a0b22fde07e43c5d5b03a1e346e1
johnoel <johnoel@hawaii.edu>
2019-12-18 19:33:09 -1000

    Used to compare output values.

--- tests/outputs/compare_outputs.py
+++ tests/outputs/compare_outputs.py
@@ -35,11 +35,34 @@ if __name__ == '__main__':
     if len(list_lines) == 0:
       break
 
-    print(list_lines)
+    #print(list_lines)
 
-    max_len = 0
-    for l in list_lines:
-      if len(l) > max_len: 
-        max_len = len(l) 
+    jmax = 0
+    for lst in list_lines:
+      if len(lst) > jmax: 
+        jmax = len(lst) 
 
-    print(max_len)
+    print(list_lines[0][0])
+    j = 0
+    while j < jmax:
+      i = 1
+      while i < len(list_lines):
+        if list_lines[0][j] != list_lines[i][j]:
+          #print(list_lines[0][j])
+          #print(list_lines[i][j])
+          s1 = list_lines[i][j].split()
+          k = 0
+          for s0 in list_lines[0][j].split():
+            if s0 != s1[k]:
+              diff = abs(float(s0) - float(s1[k]))
+              if diff > 10e-5:
+                print('-------')
+                print(s0)
+                print(s1[k])
+                print(diff)
+                print('-------')
+            k += 1
+        i += 1
+      j += 1
+
+    #print(max_len)

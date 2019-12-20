aff79e1bebba30015d2aef153e747b048741be3c
johnoel <johnoel@hawaii.edu>
2019-12-19 23:28:36 -1000

    Simplify id for mingw.

--- scripts/admb/admb.bat
+++ scripts/admb/admb.bat
@@ -246,20 +246,10 @@ if "!CXX!"=="cl" (
     set CXXFLAGS=!CXXFLAGS! -O3
   )
   for /f %%i in ('!CXX! -dumpmachine ^| findstr /b i686') do (
-    for /f %%i in ('!CXX! --version ^| findstr MSYS2') do (
-      set CXXVERSION=-i686-msys2!CXXMAJORNUMBER!
-    )
-    if not defined CXXVERSION (
-      set CXXVERSION=-mingw32!CXXMAJORNUMBER!
-    )
+    set CXXVERSION=-mingw32!CXXMAJORNUMBER!
   )
   for /f %%i in ('!CXX! -dumpmachine ^| findstr /b x86_64') do (
-    for /f %%i in ('!CXX! --version ^| findstr MSYS2') do (
-      set CXXVERSION=-x86_64-msys2!CXXMAJORNUMBER!
-    )
-    if not defined CXXVERSION (
-      set CXXVERSION=-mingw64!CXXMAJORNUMBER!
-    )
+    set CXXVERSION=-mingw64!CXXMAJORNUMBER!
   )
   if defined fast (
     set CXXFLAGS=!CXXFLAGS! -DOPT_LIB

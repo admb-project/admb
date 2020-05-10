7d1b2be17548553e3dc8bfd593fb4ca13307d096
johnoel <johnoel@hawaii.edu>
2020-05-09 14:03:27 -1000

    Add thread tests for dlist.

--- tests/gtests/test_dlist.cpp
+++ tests/gtests/test_dlist.cpp
@@ -1,4 +1,5 @@
 #include <gtest/gtest.h>
+#include <thread>
 #include <fvar.hpp>
 
 class test_dlist: public ::testing::Test {};
@@ -31,4 +32,57 @@ TEST_F(test_dlist, create_append_remove)
   ASSERT_EQ(dl.total_addresses(), 1);
 
   ASSERT_TRUE(link == link2);
+}
+TEST_F(test_dlist, thread)
+{
+  auto f = [](const int id)
+  {
+    grad_stack gs(100, sizeof(double), 10, id);
+
+    dlink* link = gs.GRAD_LIST->create();
+    gs.GRAD_LIST->append(link);
+
+    ASSERT_EQ(gs.GRAD_LIST->total_addresses(), 1);
+    dlink* link2 = gs.GRAD_LIST->last_remove();
+    ASSERT_EQ(gs.GRAD_LIST->total_addresses(), 1);
+
+    ASSERT_TRUE(link == link2);
+  };
+  std::thread t0(f, 0);
+  t0.join();
+}
+TEST_F(test_dlist, thread5x)
+{
+  std::srand(std::time(nullptr));
+  auto f = [](const int id)
+  {
+    int random0 = std::rand() % 3;
+
+    grad_stack gs(100, sizeof(double), 10, id);
+
+    dlink* link0 = gs.GRAD_LIST->create();
+    gs.GRAD_LIST->append(link0);
+    dlink* link1 = gs.GRAD_LIST->create();
+    gs.GRAD_LIST->append(link1);
+    dlink* link2 = gs.GRAD_LIST->create();
+    gs.GRAD_LIST->append(link2);
+    ASSERT_EQ(gs.GRAD_LIST->total_addresses(), 3);
+
+    std::this_thread::sleep_for (std::chrono::seconds(random0));
+
+    dlink* link_last = gs.GRAD_LIST->last_remove();
+    ASSERT_EQ(gs.GRAD_LIST->total_addresses(), 3);
+
+    ASSERT_TRUE(link2 == link_last);
+  };
+  std::thread t0(f, 0);
+  std::thread t3(f, 3);
+  std::thread t1(f, 1);
+  std::thread t4(f, 4);
+  std::thread t2(f, 2);
+  t0.join();
+  t1.join();
+  t2.join();
+  t3.join();
+  t4.join();
 }

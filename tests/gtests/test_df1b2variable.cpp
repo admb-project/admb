3be92eb4a14fc7be82c18941ee0d4636d51b080f
johnoel <johnoel@hawaii.edu>
2021-03-24 09:06:43 -1000

    Add tests for df1b2variable and correct unitialized adpool::nvar to zero.

--- tests/gtests/test_df1b2variable.cpp
+++ tests/gtests/test_df1b2variable.cpp
@@ -1,81 +1,42 @@
 #include <gtest/gtest.h>
 #include <df1b2fun.h>
 
+extern df1b2_gradlist* f1b2gradlist;
+
 class test_df1b2variable: public ::testing::Test
 {
 public:
   virtual ~test_df1b2variable() {}
+  virtual void SetUp()
+  {
+    TearDown();
+  }
   virtual void TearDown()
   {
-    f1b2gradlist = NULL;
-    df1b2variable::pool = NULL;
+    if (df1b2variable::pool)
+    {
+      delete df1b2variable::pool;
+      df1b2variable::pool = NULL;
+    }
+    if (f1b2gradlist)
+    {
+      delete f1b2gradlist;
+      f1b2gradlist = NULL;
+    }
+    if (initial_df1b2params::varsptr)
+    {
+      delete initial_df1b2params::varsptr;
+      initial_df1b2params::varsptr = NULL;
+    }
   }
 };
 
-/*
-TEST_F(test_df1b2variable, default_constructor)
+TEST_F(test_df1b2variable, default_constructor_noallocate)
 {
-  if (df1b2variable::pool)
-  {
-    delete df1b2variable::pool;
-    df1b2variable::pool = NULL;
-  }
   ASSERT_TRUE(df1b2variable::pool == NULL);
-  if (f1b2gradlist)
-  {
-    delete f1b2gradlist;
-    f1b2gradlist = NULL;
-  }
   ASSERT_TRUE(f1b2gradlist == NULL);
-
-  if (initial_df1b2params::varsptr)
-  {
-    delete initial_df1b2params::varsptr;
-    initial_df1b2params::varsptr = NULL;
-  }
   ASSERT_TRUE(initial_df1b2params::varsptr == NULL);
-
-  adpool a;
-  const size_t n = 10;
-  size_t size = sizeof(double) * df1b2variable::get_blocksize(n);
-  a.set_size(size);
-  df1b2variable::pool = &a;
-
-  df1b2_gradlist gradlist(4000000U,200000U,8000000U,400000U,2000000U,100000U,adstring("f1b2list1"));
-  f1b2gradlist = &gradlist;;
-
-  initial_df1b2params::varsptr = new P_INITIAL_DF1B2PARAMS[1];
-
-  df1b2variable::noallocate = 1;
-  df1b2_init_number number;
-  initial_df1b2params::varsptr[0] = &number;
-
-  int ii = 0;
-  dvector y(0, 1);
-  (initial_df1b2params::varsptr[0])->set_value(y, ii);
-
-  if (initial_df1b2params::varsptr)
   {
-    delete initial_df1b2params::varsptr;
-    initial_df1b2params::varsptr = NULL;
-  }
-}
-TEST_F(test_df1b2variable, default_constructor)
-{
-  adpool* save = df1b2variable::pool;
-
-  extern df1b2_gradlist* f1b2gradlist;
-  df1b2_gradlist* save2 = f1b2gradlist;
-  {
-    adpool a;
-    const size_t n = 10;
-    size_t size = sizeof(double) * df1b2variable::get_blocksize(n);
-    a.set_size(size);
-    df1b2variable::pool = &a;
-
-    df1b2_gradlist gradlist(4000000U,200000U,8000000U,400000U,2000000U,100000U,adstring("f1b2list1"));
-    f1b2gradlist = &gradlist;;
-
     df1b2variable::noallocate = 1;
     df1b2variable v;
 
@@ -90,82 +51,74 @@ TEST_F(test_df1b2variable, default_constructor)
     ASSERT_EQ(NULL, v.u_bar_tilde);
     ASSERT_EQ(NULL, v.u_dot_bar_tilde);
     ASSERT_EQ(0, v.indindex);
-
-    v.allocate();
-    ASSERT_TRUE(v.ptr);
-    ASSERT_EQ(0, ((twointsandptr*)v.ptr)->nvar);
-    ASSERT_EQ(0, ((twointsandptr*)v.ptr)->ncopies);
-    ASSERT_TRUE(((twointsandptr*)v.ptr)->ptr == &a);
-    ASSERT_EQ(0, *v.ncopies);
-    ASSERT_TRUE(v.u == (v.ptr + 2));
-    ASSERT_TRUE(v.u_dot == (v.u + 1));
-    int nvar = v.get_local_nvar();
-    ASSERT_TRUE(v.u_bar == (v.u_dot + nvar));
-    ASSERT_TRUE(v.u_dot_bar == (v.u_bar + nvar));
-    ASSERT_TRUE(v.u_tilde == (v.u_dot_bar + nvar));
-    ASSERT_TRUE(v.u_dot_tilde == (v.u_tilde + 1));
-    ASSERT_TRUE(v.u_bar_tilde == (v.u_dot_tilde + nvar));
-    ASSERT_TRUE(v.u_dot_bar_tilde == (v.u_bar_tilde + nvar));
-    ASSERT_FALSE(0 == v.indindex);
   }
-  df1b2variable::pool = save;
-  f1b2gradlist = save2;
+  ASSERT_TRUE(df1b2variable::pool == NULL);
+  ASSERT_TRUE(f1b2gradlist == NULL);
+  ASSERT_TRUE(initial_df1b2params::varsptr == NULL);
 }
-TEST_F(test_df1b2variable, default_constructor_noallocate_false)
+TEST_F(test_df1b2variable, default_constructor_allocate_deallocate)
 {
-  adpool* save = df1b2variable::pool;
+  ASSERT_TRUE(df1b2variable::pool == NULL);
+  ASSERT_TRUE(f1b2gradlist == NULL);
+  ASSERT_TRUE(initial_df1b2params::varsptr == NULL);
 
-  extern df1b2_gradlist* f1b2gradlist;
-  df1b2_gradlist* save2 = f1b2gradlist;
+  const size_t n = 10;
+  size_t s = sizeof(double) * df1b2variable::get_blocksize(n);
+  df1b2variable::pool = new adpool();
+  df1b2variable::pool->set_size(s);
   {
-    adpool a;
-    const size_t n = 10;
-    size_t size = sizeof(double) * df1b2variable::get_blocksize(n);
-    a.set_size(size);
-    df1b2variable::pool = &a;
-
-    df1b2_gradlist gradlist(4000000U,200000U,8000000U,400000U,2000000U,100000U,adstring("f1b2list1"));
-    f1b2gradlist = &gradlist;;
-
-    df1b2variable::noallocate = 0;
+    df1b2variable::noallocate = 1;
     df1b2variable v;
+    ASSERT_TRUE(v.ptr == nullptr);
 
-    ASSERT_TRUE(v.ptr);
-    ASSERT_EQ(0, ((twointsandptr*)v.ptr)->nvar);
-    ASSERT_EQ(0, ((twointsandptr*)v.ptr)->ncopies);
-    ASSERT_TRUE(((twointsandptr*)v.ptr)->ptr == &a);
-
-    ASSERT_EQ(0, *v.ncopies);
-    ASSERT_TRUE(v.u == (v.ptr + 2));
-    ASSERT_TRUE(v.u_dot == (v.u + 1));
-    int nvar = v.get_local_nvar();
-    ASSERT_TRUE(v.u_bar == (v.u_dot + nvar));
-    ASSERT_TRUE(v.u_dot_bar == (v.u_bar + nvar));
-    ASSERT_TRUE(v.u_tilde == (v.u_dot_bar + nvar));
-    ASSERT_TRUE(v.u_dot_tilde == (v.u_tilde + 1));
-    ASSERT_TRUE(v.u_bar_tilde == (v.u_dot_tilde + nvar));
-    ASSERT_TRUE(v.u_dot_bar_tilde == (v.u_bar_tilde + nvar));
-    ASSERT_FALSE(0 == v.indindex);
+    df1b2_gradlist::no_derivatives = 1;
+    v.allocate();
+    v.deallocate();
   }
-  df1b2variable::pool = save;
-  f1b2gradlist = save2;
+  ASSERT_TRUE(df1b2variable::pool != NULL);
+  ASSERT_TRUE(f1b2gradlist == NULL);
+  ASSERT_TRUE(initial_df1b2params::varsptr == NULL);
 }
-TEST_F(test_df1b2variable, copy_constructor_null)
+
+//df1b2variable& df1b2variable::operator = (const df3_two_variable& v)
+
+TEST_F(test_df1b2variable, default_constructor2)
 {
-  adpool* save = df1b2variable::pool;
-
-  extern df1b2_gradlist* f1b2gradlist;
-  df1b2_gradlist* save2 = f1b2gradlist;
+  ASSERT_TRUE(df1b2variable::pool == NULL);
+  ASSERT_TRUE(f1b2gradlist == NULL);
+  ASSERT_TRUE(initial_df1b2params::varsptr == NULL);
+  const size_t n = 10;
+  size_t s = sizeof(double) * df1b2variable::get_blocksize(n);
+  df1b2variable::pool = new adpool();
+  df1b2variable::pool->set_size(s);
   {
-    adpool a;
-    const size_t n = 10;
-    size_t size = sizeof(double) * df1b2variable::get_blocksize(n);
-    a.set_size(size);
-    df1b2variable::pool = &a;
+    //df1b2_gradlist gradlist(4000000U,200000U,8000000U,400000U,2000000U,100000U,adstring("f1b2list1"));
+    //f1b2gradlist = &gradlist;
 
-    df1b2_gradlist gradlist(4000000U,200000U,8000000U,400000U,2000000U,100000U,adstring("f1b2list1"));
-    f1b2gradlist = &gradlist;;
+    initial_df1b2params::varsptr = new P_INITIAL_DF1B2PARAMS[1];
 
+    df1b2variable::noallocate = 1;
+    df1b2_init_number number;
+    initial_df1b2params::varsptr[0] = &number;
+
+    int ii = 0;
+    dvector y(0, 1);
+    (initial_df1b2params::varsptr[0])->set_value(y, ii);
+  }
+  ASSERT_TRUE(df1b2variable::pool != NULL);
+  ASSERT_TRUE(f1b2gradlist == NULL);
+  ASSERT_TRUE(initial_df1b2params::varsptr != NULL);
+}
+TEST_F(test_df1b2variable, copy_constructor_empty)
+{
+  ASSERT_TRUE(df1b2variable::pool == NULL);
+  ASSERT_TRUE(f1b2gradlist == NULL);
+  ASSERT_TRUE(initial_df1b2params::varsptr == NULL);
+  const size_t n = 10;
+  size_t s = sizeof(double) * df1b2variable::get_blocksize(n);
+  df1b2variable::pool = new adpool();
+  df1b2variable::pool->set_size(s);
+  {
     df1b2variable::noallocate = 1;
     df1b2variable empty;
     df1b2variable v(empty);
@@ -182,32 +135,27 @@ TEST_F(test_df1b2variable, copy_constructor_null)
     ASSERT_EQ(NULL, v.u_dot_bar_tilde);
     ASSERT_EQ(0, v.indindex);
   }
-  df1b2variable::pool = save;
-  f1b2gradlist = save2;
+  ASSERT_TRUE(df1b2variable::pool != NULL);
+  ASSERT_TRUE(f1b2gradlist == NULL);
+  ASSERT_TRUE(initial_df1b2params::varsptr == NULL);
 }
-TEST_F(test_df1b2variable, copy_constructor_noallocate_false)
+TEST_F(test_df1b2variable, copy_constructor_noallocate_nonempty)
 {
-  adpool* save = df1b2variable::pool;
-
-  extern df1b2_gradlist* f1b2gradlist;
-  df1b2_gradlist* save2 = f1b2gradlist;
+  ASSERT_TRUE(df1b2variable::pool == NULL);
+  ASSERT_TRUE(f1b2gradlist == NULL);
+  ASSERT_TRUE(initial_df1b2params::varsptr == NULL);
+  const size_t n = 10;
+  size_t s = sizeof(double) * df1b2variable::get_blocksize(n);
+  df1b2variable::pool = new adpool();
+  df1b2variable::pool->set_size(s);
   {
-    adpool a;
-    const size_t n = 10;
-    size_t size = sizeof(double) * df1b2variable::get_blocksize(n);
-    a.set_size(size);
-    df1b2variable::pool = &a;
-
-    df1b2_gradlist gradlist(4000000U,200000U,8000000U,400000U,2000000U,100000U,adstring("f1b2list1"));
-    f1b2gradlist = &gradlist;;
-
     df1b2variable::noallocate = 0;
     df1b2variable v;
 
     ASSERT_TRUE(v.ptr);
     ASSERT_EQ(0, ((twointsandptr*)v.ptr)->nvar);
     ASSERT_EQ(0, ((twointsandptr*)v.ptr)->ncopies);
-    ASSERT_TRUE(((twointsandptr*)v.ptr)->ptr == &a);
+    ASSERT_TRUE(((twointsandptr*)v.ptr)->ptr == df1b2variable::pool);
     ASSERT_EQ(0, *v.ncopies);
     ASSERT_TRUE(v.u == (v.ptr + 2));
     ASSERT_TRUE(v.u_dot == (v.u + 1));
@@ -224,7 +172,7 @@ TEST_F(test_df1b2variable, copy_constructor_noallocate_false)
     ASSERT_TRUE(v.ptr);
     ASSERT_EQ(0, ((twointsandptr*)copy.ptr)->nvar);
     ASSERT_EQ(1, ((twointsandptr*)copy.ptr)->ncopies);
-    ASSERT_TRUE(((twointsandptr*)copy.ptr)->ptr == &a);
+    ASSERT_TRUE(((twointsandptr*)copy.ptr)->ptr == df1b2variable::pool);
 
     ASSERT_EQ(1, *copy.ncopies);
     ASSERT_TRUE(v.u == copy.u);
@@ -238,32 +186,27 @@ TEST_F(test_df1b2variable, copy_constructor_noallocate_false)
     ASSERT_TRUE(v.indindex != copy.indindex);
     ASSERT_TRUE(0 == copy.indindex);
   }
-  df1b2variable::pool = save;
-  f1b2gradlist = save2;
+  ASSERT_TRUE(df1b2variable::pool != NULL);
+  ASSERT_TRUE(f1b2gradlist == NULL);
+  ASSERT_TRUE(initial_df1b2params::varsptr == NULL);
 }
 TEST_F(test_df1b2variable, constructor_double)
 {
-  adpool* save = df1b2variable::pool;
-
-  extern df1b2_gradlist* f1b2gradlist;
-  df1b2_gradlist* save2 = f1b2gradlist;
+  ASSERT_TRUE(df1b2variable::pool == NULL);
+  ASSERT_TRUE(f1b2gradlist == NULL);
+  ASSERT_TRUE(initial_df1b2params::varsptr == NULL);
+  const size_t n = 10;
+  size_t s = sizeof(double) * df1b2variable::get_blocksize(n);
+  df1b2variable::pool = new adpool();
+  df1b2variable::pool->set_size(s);
   {
-    adpool a;
-    const size_t n = 10;
-    size_t size = sizeof(double) * df1b2variable::get_blocksize(n);
-    a.set_size(size);
-    df1b2variable::pool = &a;
-
-    df1b2_gradlist gradlist(4000000U,200000U,8000000U,400000U,2000000U,100000U,adstring("f1b2list1"));
-    f1b2gradlist = &gradlist;;
-  
     double expected = 6.5;
     df1b2variable v(expected);
 
     ASSERT_TRUE(v.ptr);
     ASSERT_EQ(0, ((twointsandptr*)v.ptr)->nvar);
     ASSERT_EQ(0, ((twointsandptr*)v.ptr)->ncopies);
-    ASSERT_TRUE(((twointsandptr*)v.ptr)->ptr == &a);
+    ASSERT_TRUE(((twointsandptr*)v.ptr)->ptr == df1b2variable::pool);
     ASSERT_EQ(0, *v.ncopies);
     ASSERT_DOUBLE_EQ(*v.u, expected);
     ASSERT_TRUE(v.u == (v.ptr + 2));
@@ -277,78 +220,55 @@ TEST_F(test_df1b2variable, constructor_double)
     ASSERT_TRUE(v.u_dot_bar_tilde == (v.u_bar_tilde + nvar));
     ASSERT_FALSE(0 == v.indindex);
   }
-  df1b2variable::pool = save;
-  f1b2gradlist = save2;
+  ASSERT_TRUE(df1b2variable::pool != NULL);
+  ASSERT_TRUE(f1b2gradlist == NULL);
+  ASSERT_TRUE(initial_df1b2params::varsptr == NULL);
 }
 TEST_F(test_df1b2variable, df1b2vector)
 {
-  adpool* save = df1b2variable::pool;
-
-  extern df1b2_gradlist* f1b2gradlist;
-  df1b2_gradlist* save2 = f1b2gradlist;
+  ASSERT_TRUE(df1b2variable::pool == NULL);
+  ASSERT_TRUE(f1b2gradlist == NULL);
+  ASSERT_TRUE(initial_df1b2params::varsptr == NULL);
+  const size_t n = 10;
+  size_t s = sizeof(double) * df1b2variable::get_blocksize(n);
+  df1b2variable::pool = new adpool();
+  df1b2variable::pool->set_size(s);
   {
-    adpool a;
-    const size_t n = 10;
-    size_t size = sizeof(double) * df1b2variable::get_blocksize(n);
-    a.set_size(size);
-    df1b2variable::pool = &a;
-
-    df1b2_gradlist gradlist(4000000U,200000U,8000000U,400000U,2000000U,100000U,adstring("f1b2list1"));
-    f1b2gradlist = &gradlist;;
-  
     df1b2vector v;
     v.allocate(1, 2);
-    //df1b2vector copy(v);
-    //copy.deallocate();
-  }
-  df1b2variable::pool = save;
-  f1b2gradlist = save2;
-}
-TEST_F(test_df1b2variable, df1b2vector2)
-{
-  adpool* save = df1b2variable::pool;
-
-  extern df1b2_gradlist* f1b2gradlist;
-  df1b2_gradlist* save2 = f1b2gradlist;
-  {
-    adpool a;
-    const size_t n = 10;
-    size_t size = sizeof(double) * df1b2variable::get_blocksize(n);
-    a.set_size(size);
-    df1b2variable::pool = &a;
-
-    df1b2_gradlist gradlist(4000000U,200000U,8000000U,400000U,2000000U,100000U,adstring("f1b2list1"));
-    f1b2gradlist = &gradlist;;
-  
-    df1b2vector v;
-    v.allocate(1, 10);
+    df1b2vector copy(v);
+    ASSERT_TRUE(v.getv() == copy.getv());
+    copy.deallocate();
     v.deallocate();
   }
-  df1b2variable::pool = save;
-  f1b2gradlist = save2;
+  ASSERT_TRUE(df1b2variable::pool != NULL);
+  ASSERT_TRUE(f1b2gradlist == NULL);
+  ASSERT_TRUE(initial_df1b2params::varsptr == NULL);
 }
-TEST_F(test_df1b2variable, ncopies_is_one)
+TEST_F(test_df1b2variable, df1b2vector_allocate_deallocate)
 {
-  adpool* save = df1b2variable::pool;
-
-  extern df1b2_gradlist* f1b2gradlist;
-  df1b2_gradlist* save2 = f1b2gradlist;
+  ASSERT_TRUE(df1b2variable::pool == NULL);
+  ASSERT_TRUE(f1b2gradlist == NULL);
+  ASSERT_TRUE(initial_df1b2params::varsptr == NULL);
+  const size_t n = 10;
+  size_t s = sizeof(double) * df1b2variable::get_blocksize(n);
+  df1b2variable::pool = new adpool();
+  df1b2variable::pool->set_size(s);
   {
-    adpool a;
-    const size_t n = 10;
-    size_t size = sizeof(double) * df1b2variable::get_blocksize(n);
-    a.set_size(size);
-    df1b2variable::pool = &a;
-
-    df1b2_gradlist gradlist(4000000U,200000U,8000000U,400000U,2000000U,100000U,adstring("f1b2list1"));
-    f1b2gradlist = &gradlist;;
-  
-    df1b2variable v;
-    v.allocate();
-    df1b2variable copy(v);
-    copy.deallocate();
+    df1b2vector v;
+    ASSERT_TRUE(v.getv() == nullptr);
+    ASSERT_EQ(v.indexmin(), 1);
+    ASSERT_EQ(v.indexmax(), 0);
+    v.allocate(1, 10);
+    ASSERT_TRUE(v.getv() != nullptr);
+    ASSERT_EQ(v.indexmin(), 1);
+    ASSERT_EQ(v.indexmax(), 10);
+    v.deallocate();
+    ASSERT_TRUE(v.getv() == nullptr);
+    ASSERT_EQ(v.indexmin(), 1);
+    ASSERT_EQ(v.indexmax(), 0);
   }
-  df1b2variable::pool = save;
-  f1b2gradlist = save2;
+  ASSERT_TRUE(df1b2variable::pool != NULL);
+  ASSERT_TRUE(f1b2gradlist == NULL);
+  ASSERT_TRUE(initial_df1b2params::varsptr == NULL);
 }
-*/

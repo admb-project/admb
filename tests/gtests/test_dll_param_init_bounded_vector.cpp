7442786c5c54368e4b199c565e9c2ff921f91f6d
johnoel <johnoel@hawaii.edu>
2021-04-05 19:09:40 -1000

    Add one more.

--- tests/gtests/test_dll_param_init_bounded_vector.cpp
+++ tests/gtests/test_dll_param_init_bounded_vector.cpp
@@ -6,7 +6,7 @@ extern "C"
   void test_ad_exit(const int exit_code);
 }
 
-class test_dll_param_init_bounded_vector: public ::testing::Test 
+class test_dll_param_init_bounded_vector: public ::testing::Test
 {
 protected:
   double d;
@@ -14,12 +14,26 @@ protected:
 
 TEST_F(test_dll_param_init_bounded_vector, constructor)
 {
-  //ad_exit=&test_ad_exit;
-
   gradient_structure gs;
-  dll_param_init_bounded_vector m;
-  m.allocate(&d, 1, 4, 2.0, 3.0, 5, "m");
-  ASSERT_EQ(1, m.indexmin());
-  ASSERT_EQ(4, m.indexmax());
-  ASSERT_EQ(5, m.get_phase_start());
+  dll_param_init_bounded_vector v;
+  ASSERT_EQ(0, v.indexmin());
+  ASSERT_EQ(-1, v.indexmax());
+  ASSERT_EQ(0, v.get_phase_start());
+}
+TEST_F(test_dll_param_init_bounded_vector, allocate)
+{
+  double* values = new double[4];
+  values[0] = 0;
+  values[1] = 1;
+  values[2] = 2;
+  values[3] = 3;
+  gradient_structure gs;
+  dll_param_init_bounded_vector v;
+  v.allocate(values, 1, 4, 0, 0, 5, "v");
+  ASSERT_EQ(1, v.indexmin());
+  ASSERT_EQ(4, v.indexmax());
+  ASSERT_EQ(5, v.get_phase_start());
+
+  delete values;
+  values = nullptr;
 }

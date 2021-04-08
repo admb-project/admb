1a812ccd83cdc108e4a427b8ae34379141d68a47
johnoel <johnoel@hawaii.edu>
2021-04-08 09:48:01 -1000

    Add tests for quadratic_prior.

--- tests/gtests/test_quadratic_prior.cpp
+++ tests/gtests/test_quadratic_prior.cpp
@@ -112,3 +112,226 @@ TEST_F(test_quadratic_prior, allocate_null)
   dvar_vector empty;
   ASSERT_THROW(data.allocate(empty, "empty"), int);
 }
+class quadratic_re_penalty2: public quadratic_re_penalty
+{
+public:
+  quadratic_re_penalty2(): quadratic_re_penalty() { }
+  void get_cM(void) {}
+  void operator=(const dmatrix& M)
+  {
+    quadratic_re_penalty::operator=(M);
+  }
+};
+TEST_F(test_quadratic_prior, quadratic_re_penalty)
+{
+  ad_exit=&test_ad_exit;
+
+  gradient_structure gs;
+
+  quadratic_re_penalty2 data;
+
+  ASSERT_EQ(data.old_style_flag, 2);
+
+  dmatrix m(1, 1, 1, 1);
+  m.initialize();
+  m = 5;
+
+  objective_function_value::pobjfun = new objective_function_value();
+
+  value(*objective_function_value::pobjfun) = 0;
+  ASSERT_DOUBLE_EQ(value(*objective_function_value::pobjfun), 0);
+
+  dvar_vector v(1, 1);
+  v.initialize();
+  v = 1;
+  data.allocate(v);
+  data = m;
+
+  ASSERT_DOUBLE_EQ(value(*objective_function_value::pobjfun), 5);
+
+  delete objective_function_value::pobjfun;
+  objective_function_value::pobjfun = nullptr;
+}
+TEST_F(test_quadratic_prior, quadratic_re_penalty_not2)
+{
+  ad_exit=&test_ad_exit;
+
+  gradient_structure gs;
+
+  quadratic_re_penalty2 data;
+
+  ASSERT_EQ(data.old_style_flag, 2);
+
+  dmatrix m(1, 1, 1, 1);
+  m.initialize();
+  m = 5;
+
+  objective_function_value::pobjfun = new objective_function_value();
+
+  value(*objective_function_value::pobjfun) = 0;
+  ASSERT_DOUBLE_EQ(value(*objective_function_value::pobjfun), 0);
+
+  dvar_vector v(1, 1);
+  v.initialize();
+  v = 1;
+  data.allocate(v);
+
+  data.old_style_flag = 0;
+  ASSERT_THROW({ data = m; }, int);
+  data.old_style_flag = 1;
+  ASSERT_THROW({ data = m; }, int);
+  data.old_style_flag = 3;
+  ASSERT_THROW({ data = m; }, int);
+
+  delete objective_function_value::pobjfun;
+  objective_function_value::pobjfun = nullptr;
+}
+class constant_quadratic_re_penalty2: public constant_quadratic_re_penalty
+{
+public:
+  constant_quadratic_re_penalty2(): constant_quadratic_re_penalty() { }
+  void get_cM(void) {}
+};
+TEST_F(test_quadratic_prior, constant_quadratic_re_penalty)
+{
+  ad_exit=&test_ad_exit;
+
+  gradient_structure gs;
+
+  constant_quadratic_re_penalty2 data;
+
+  ASSERT_EQ(data.old_style_flag, 2);
+}
+class normal_quadratic_prior2: public normal_quadratic_prior
+{
+public:
+  normal_quadratic_prior2(): normal_quadratic_prior() { }
+  void get_cM(void) {}
+  void operator=(const dvar_matrix& M)
+  {
+    normal_quadratic_prior::operator=(M);
+  }
+};
+TEST_F(test_quadratic_prior, normal_quadratic_prior)
+{
+  ad_exit=&test_ad_exit;
+
+  gradient_structure gs;
+
+  normal_quadratic_prior2 data;
+
+  ASSERT_EQ(data.old_style_flag, 0);
+
+  dvar_matrix m(1, 1, 1, 1);
+  m.initialize();
+  m = 5;
+
+  objective_function_value::pobjfun = new objective_function_value();
+
+  value(*objective_function_value::pobjfun) = 0;
+  ASSERT_DOUBLE_EQ(value(*objective_function_value::pobjfun), 0);
+
+  dvar_vector v(1, 1);
+  v.initialize();
+  v = 1;
+  data.allocate(v);
+  data = m;
+
+  ASSERT_DOUBLE_EQ(value(*objective_function_value::pobjfun), 0.90471895621705012);
+
+  delete objective_function_value::pobjfun;
+  objective_function_value::pobjfun = nullptr;
+}
+TEST_F(test_quadratic_prior, normal_quadratic_prior_1)
+{
+  ad_exit=&test_ad_exit;
+
+  gradient_structure gs;
+
+  normal_quadratic_prior2 data;
+
+  ASSERT_EQ(data.old_style_flag, 0);
+
+  dvar_matrix m(1, 1, 1, 1);
+  m.initialize();
+  m = 5;
+
+  objective_function_value::pobjfun = new objective_function_value();
+
+  value(*objective_function_value::pobjfun) = 0;
+  ASSERT_DOUBLE_EQ(value(*objective_function_value::pobjfun), 0);
+
+  dvar_vector v(1, 1);
+  v.initialize();
+  v = 1;
+  data.allocate(v);
+  data.old_style_flag = 1;
+  data = m;
+
+  ASSERT_DOUBLE_EQ(value(*objective_function_value::pobjfun), 0.2);
+
+  delete objective_function_value::pobjfun;
+  objective_function_value::pobjfun = nullptr;
+}
+TEST_F(test_quadratic_prior, normal_quadratic_prior_2)
+{
+  ad_exit=&test_ad_exit;
+
+  gradient_structure gs;
+
+  normal_quadratic_prior2 data;
+
+  ASSERT_EQ(data.old_style_flag, 0);
+
+  dvar_matrix m(1, 1, 1, 1);
+  m.initialize();
+  m = 5;
+
+  objective_function_value::pobjfun = new objective_function_value();
+
+  value(*objective_function_value::pobjfun) = 0;
+  ASSERT_DOUBLE_EQ(value(*objective_function_value::pobjfun), 0);
+
+  dvar_vector v(1, 1);
+  v.initialize();
+  v = 1;
+  data.allocate(v);
+  data.old_style_flag = 2;
+  data = m;
+
+  ASSERT_DOUBLE_EQ(value(*objective_function_value::pobjfun), 5);
+
+  delete objective_function_value::pobjfun;
+  objective_function_value::pobjfun = nullptr;
+}
+TEST_F(test_quadratic_prior, normal_quadratic_prior_3)
+{
+  ad_exit=&test_ad_exit;
+
+  gradient_structure gs;
+
+  normal_quadratic_prior2 data;
+
+  ASSERT_EQ(data.old_style_flag, 0);
+
+  dvar_matrix m(1, 1, 1, 1);
+  m.initialize();
+  m = 5;
+
+  objective_function_value::pobjfun = new objective_function_value();
+
+  value(*objective_function_value::pobjfun) = 0;
+  ASSERT_DOUBLE_EQ(value(*objective_function_value::pobjfun), 0);
+
+  dvar_vector v(1, 1);
+  v.initialize();
+  v = 1;
+  data.allocate(v);
+  data.old_style_flag = 3;
+  ASSERT_THROW({ data = m; }, int);
+
+  ASSERT_DOUBLE_EQ(value(*objective_function_value::pobjfun), 0);
+
+  delete objective_function_value::pobjfun;
+  objective_function_value::pobjfun = nullptr;
+}

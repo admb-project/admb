9c3c5fedfaa063193072fe1b270e4773e00837e3
johnoel <johnoel@hawaii.edu>
2021-10-03 22:42:01 -1000

    Use one loop.

--- tests/thread_funnel/thread_funnel3.cpp
+++ tests/thread_funnel/thread_funnel3.cpp
@@ -66,50 +66,11 @@ dvar_vector funnels(
 
   gradient_structure* gs = gradient_structure::get();
 
-  for (int i = min; i < max; i += 4)
+  for (int i = min; i <= max; ++i)
   {
-    gradient_structure::_instance = gradients[1];
-    std::future<std::pair<double, dvector>> f1 =
-      thread_funnel(func, tau, nu, sigma, beta, a(i), nsteps);
-    gradient_structure::_instance = nullptr;
-
-    gradient_structure::_instance = gradients[2];
-    std::future<std::pair<double, dvector>> f2 =
-      thread_funnel(func, tau, nu, sigma, beta, a(i + 1), nsteps);
-    gradient_structure::_instance = nullptr;
-
-    gradient_structure::_instance = gradients[3];
-    std::future<std::pair<double, dvector>> f3 =
-      thread_funnel(func, tau, nu, sigma, beta, a(i + 2), nsteps);
-    gradient_structure::_instance = nullptr;
-
-    gradient_structure::_instance = gradients[4];
-    std::future<std::pair<double, dvector>> f4 =
-      thread_funnel(func, tau, nu, sigma, beta, a(i + 3), nsteps);
-    gradient_structure::_instance = nullptr;
-
-    f1.wait();
-    f2.wait();
-    f3.wait();
-    f4.wait();
-
-    std::pair<double, dvector> p1 = f1.get();
-    std::pair<double, dvector> p2 = f2.get();
-    std::pair<double, dvector> p3 = f3.get();
-    std::pair<double, dvector> p4 = f4.get();
-
-    gradient_structure::_instance = gs;
-    results(i)     = to_dvariable(p1, tau, nu, sigma, beta);
-    results(i + 1) = to_dvariable(p2, tau, nu, sigma, beta);
-    results(i + 2) = to_dvariable(p3, tau, nu, sigma, beta);
-    results(i + 3) = to_dvariable(p4, tau, nu, sigma, beta);
-    gradient_structure::_instance = nullptr;
-  }
-
-  {
-    gradient_structure::_instance = gradients[1];
+    gradient_structure::_instance = gradients[(i % 4) + 1];
     std::future<std::pair<double, dvector>> f =
-      thread_funnel(func, tau, nu, sigma, beta, a(max), nsteps);
+      thread_funnel(func, tau, nu, sigma, beta, a(i), nsteps);
     gradient_structure::_instance = nullptr;
 
     f.wait();
@@ -117,7 +78,7 @@ dvar_vector funnels(
     std::pair<double, dvector> p = f.get();
 
     gradient_structure::_instance = gs;
-    results(max) = to_dvariable(p, tau, nu, sigma, beta);
+    results(i) = to_dvariable(p, tau, nu, sigma, beta);
     gradient_structure::_instance = nullptr;
   }
   gradient_structure::_instance = gs;

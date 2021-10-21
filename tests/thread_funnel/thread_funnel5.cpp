f97645734771f64cab31558de65448917bf95aa4
johnoel <johnoel@hawaii.edu>
2021-10-21 11:23:39 -1000

    Create funnel_evaluation_nindependents for adjoint computations.

--- tests/thread_funnel/thread_funnel5.cpp
+++ tests/thread_funnel/thread_funnel5.cpp
@@ -97,29 +97,46 @@ void funnel_evaluation_nindependents()
 {
   gradient_structure* gs = gradient_structure::get();
   grad_stack* GRAD_STACK1 = gs->GRAD_STACK1;
-  // there are three independent variables
-  grad_stack_entry* grad_ptr = GRAD_STACK1->ptr;
 
-  double* ind_addr3 = grad_ptr->ind_addr1;
-  double mult3 = grad_ptr->mult1 ;
-  double* ind_addr4 = grad_ptr->ind_addr2;
-  double mult4 = grad_ptr->mult2 ;
-  if (GRAD_STACK1->ptr-- == GRAD_STACK1->ptr_first)
+  std::vector<std::pair<double*, double>> pairs;
+  double z = 0;
+
+  do
   {
-    // back up the file one buffer size and read forward
-    OFF_T offset = (OFF_T)(sizeof(grad_stack_entry) * GRAD_STACK1->length);
-    OFF_T lpos=LSEEK(GRAD_STACK1->_GRADFILE_PTR, -offset, SEEK_CUR);
+    // there are n independent variables
+    grad_stack_entry* grad_ptr = GRAD_STACK1->ptr;
 
-    GRAD_STACK1->read_grad_stack_buffer(lpos);
+    if (grad_ptr->ind_addr1)
+    {
+      pairs.push_back(std::make_pair(grad_ptr->ind_addr1, grad_ptr->mult1));
+    }
+    if (grad_ptr->ind_addr2)
+    {
+      pairs.push_back(std::make_pair(grad_ptr->ind_addr2, grad_ptr->mult2));
+    }
+    if (grad_ptr->dep_addr)
+    {
+      z = *grad_ptr->dep_addr;
+      *grad_ptr->dep_addr = 0.0;
+      break;
+    }
+    else
+    {
+      if (GRAD_STACK1->ptr-- == GRAD_STACK1->ptr_first)
+      {
+        // back up the file one buffer size and read forward
+        OFF_T offset = (OFF_T)(sizeof(grad_stack_entry) * GRAD_STACK1->length);
+        OFF_T lpos=LSEEK(GRAD_STACK1->_GRADFILE_PTR, -offset, SEEK_CUR);
+
+        GRAD_STACK1->read_grad_stack_buffer(lpos);
+      }
+    }
+  } while (GRAD_STACK1->ptr->func == nullptr);
+
+  for (int i = 0; i < pairs.size(); ++i)
+  {
+    *(pairs[i].first) += z * pairs[i].second;
   }
-
-  grad_ptr = GRAD_STACK1->ptr;
-  double z = *grad_ptr->dep_addr;
-  *grad_ptr->dep_addr=0.;
-  *grad_ptr->ind_addr1 += z * grad_ptr->mult1 ;
-  *grad_ptr->ind_addr2 += z * grad_ptr->mult2 ;
-  *ind_addr3 += z * mult3 ;
-  *ind_addr4 += z * mult4 ;
 }
 dvariable to_dvariable(std::tuple<double, dvector, std::vector<double*>>& t)
 {
@@ -158,7 +175,6 @@ dvariable to_dvariable(std::tuple<double, dvector, std::vector<double*>>& t)
     }
     if (j > max)
     {
-      //entry->func = default_evaluation4ind;
       entry->func = funnel_evaluation_nindependents;
     }
     else

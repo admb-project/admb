fd213f93376c5e5b2c83b259730f3ca11c5c2ed5
Cole-Monnahan-NOAA <cole.monnahan@noaa.gov>
2021-12-23 13:17:39 -0800

    Rework hess_step feature to be more stable and have better output and not quit if maxgrad gets worse

--- src/nh99/new_checks.cpp
+++ src/nh99/new_checks.cpp
@@ -3,9 +3,10 @@
 #include<ctime>
 
 /**
-  Experimental feature to take Newton steps using the inverse
-  Hessian to get much closer to the optimum and reduce the
-  maximum gradient arbitrarily close to 0.
+  Experimental feature to try and get closer to the optimal point
+  (minimum) using Newton steps with the inverse Hessian. If
+  successful it gives evidence that the mode has been reached and
+  the nearby curvature is well approximated by a quadratic form. 
 
   Let x be the current MLE parameter vector. Then a single step
   consists of calculating x'=x-inv(Hessian)*gradient. This
@@ -68,14 +69,25 @@ void function_minimizer::hess_step(){
       }
     }
   }
-  cout << "Experimental feature to take up to " << N_hess_steps;
-  cout << " Newton step(s) using the inverse Hessian" << endl;
+  cout <<  endl << "Experimental feature to take up to " << N_hess_steps;
+  cout << " Newton step(s) using the inverse Hessian" << endl << endl;
+
+  initial_params::current_phase=initial_params::max_number_phases;
+  int nvar=initial_params::nvarcalc(); // get the number of active parameters
+  int aiopt,biopt;
+  biopt=option_match(ad_comm::argc,ad_comm::argv,"-binp");
+  aiopt=option_match(ad_comm::argc,ad_comm::argv,"-ainp");
+  if(biopt==-1 && aiopt==-1){
+    std::string m=get_filename((char*)ad_comm::adprogram_name);
+    cout << "Warning: Parameter inputs via '-ainp' and '-binp' were not detected." << endl;
+    cout << "         It is recommended to add option '-binp " << m << ".bar' when using -hess_step." << endl;
+    cout << "         Otherwise inactive parameters (and thus gradients) may not initialize correctly." << endl << endl;
+  }
+    
   // Let x'=x-inv(Hessian)*gradient, where x is MLE in
   // unbounded space and it's corresponding gradient and
   // Hessian. Need to calculate x' then push through
   // model and calculate SD report stuff
-  initial_params::current_phase=initial_params::max_number_phases;
-  int nvar=initial_params::nvarcalc(); // get the number of active parameters
   independent_variables mle(1,nvar); // original bounded MLE
   independent_variables mle2(1,nvar); // updated bounded MLE
   independent_variables x(1,nvar); // original unbounded MLE
@@ -83,9 +95,11 @@ void function_minimizer::hess_step(){
   dvector gr0(1,nvar);		// original gradients
   dvector gr(1,nvar);		// 
   dvector gr2(1,nvar);		// updated gradients
-  double maxgrad0, mingrad0, maxgrad, maxgrad2, mingrad2;
+  dvariable nll,nll2;		// minimal values
+  double maxgrad0, mingrad0,maxgrad, maxgrad2, mingrad2;
   read_mle_hmc(nvar, mle); // takes MLE from admodel.hes file
-
+  
+ 
   // Push the original bounded MLE through the model
   initial_params::restore_all_values(mle,1);
   gradient_structure::set_YES_DERIVATIVES(); // don't know what this does
@@ -98,15 +112,21 @@ void function_minimizer::hess_step(){
   gradcalc(nvar,gr0);			      // initial unbounded gradient
   maxgrad0=max(fabs(gr0));
   mingrad0=min(fabs(gr0));
-	  
-  cout << "Initial max gradient=" << maxgrad0 << " and min gradient= " << mingrad0 << endl;
+  nll=*objective_function_value::pobjfun;
+  
+  if(maxgrad0<eps) {
+    cout << "Initial max gradient already below threshold of " << eps << " so quitting now" << endl;
+    return;
+  }
+
+  cout << "Hess step 0: Max gradient=" << maxgrad0 << " and min gradient= " << mingrad0 << endl;
+  // for(int i=1; i<=5; i++) {cout << "i=" << i << " varsptr=" << 1 << " mle=" << mle(i) << " gradient=" << gr0(i) << endl;}
   dmatrix S(1,nvar,1,nvar); // covar (inverse Hess) in unbounded space
   dvector scale(1,nvar); // dummy var
   int hbf; // dummy var
 
   // Initial for first iteration
   gr=gr0; maxgrad=maxgrad0; 
-
   int Nstep=0;
   for(int ii=1; ii<=N_hess_steps; ii++){
     Nstep++;
@@ -118,68 +138,87 @@ void function_minimizer::hess_step(){
     initial_params::reset(vx2);
     *objective_function_value::pobjfun=0.0;
     userfunction();
+    nll2=*objective_function_value::pobjfun;
     // Updated quantities after taking step
     gradcalc(nvar,gr2);
     maxgrad2=max(fabs(gr2));
     mingrad2=min(fabs(gr2));
     initial_params::copy_all_values(mle2,1.0);
     // Check whether to break out of loop early
-    if( (maxgrad2 < eps) & (ii < N_hess_steps)){
-      cout << "Step " << ii << ": Max gradient "<< maxgrad2 <<
-	" below threshold of " << eps << " so exiting early" << endl;
+    if(maxgrad2 < eps){
+      cout << "Hess step " << ii << ": Max gradient="<< maxgrad2 <<
+	" is below threshold of " << eps << " so exiting early" << endl;
       break;
-    } else if(maxgrad2>maxgrad) {
-      // which ones got worse?
-      int jj = 1;
-      for (int i = 0; i < initial_params::num_initial_params; ++i) {
-        if (active(*initial_params::varsptr[i])) {
-          int jmax = (int)initial_params::varsptr[i]->size_count();
-          for (int j = 1; j <= jmax; ++j) {
-            if(abs(gr(jj)) < abs(gr2(jj))){
-              cout << (initial_params::varsptr[i])->label();
-              if (jmax > 1) {
-                cout << "(" << j << ")";
-              }
-              cout << ": original grad=" << gr(jj) << " and updated grad=" << gr2(jj) << endl;
-            }
-	    ++jj;
-          }
-        }
-      }
-      cerr << endl <<
-	"Experimental feature -hess_step resulted in worse gradients (above)." << endl <<  
-	"Consider reoptimizing model to reset files. This suggests that the" << endl <<
-	"the negative log-likelihood is not quadratic at the mode, or that the" << endl <<
-	"Hessian does not approximate it well. In short, it suggests the model is" << endl <<
-	"not converged. Check model structure and other output files to investigate" << endl <<
-	"potential causes and solutions." << endl;
-      ad_exit(1);
-    } else {
-      // Was successful but not good enough to break early
-      cout << "Step " << ii << ": Updated max gradient=" << maxgrad2 <<
-	" and min gradient= " << mingrad2 << endl; 
-      x=x2; gr=gr2; maxgrad=maxgrad2; 
-      // If not the last step then want to skip the sd_calcs
-      // which are slow so manually update admodel.cov whereas
-      // below computations1() runs everything so do that if last
-      // step
-      if(ii!=N_hess_steps){
-	hess_routine(); // Calculate new Hessian
-	depvars_routine(); // calculate derivatives of sdreport variables
-	hess_inv(); // Invert Hess and write to admodel.cov
-      }
-    } 
-  }// end loop over hess_steps
-  // Finished successully so run the opitmizer without
-  // taking any steps to produce updated output files
-  // with this new MLE value.
-  function_minimizer::maxfn=0;
-  computations1(ad_comm::argc,ad_comm::argv);
-  cout << endl << "The " << Nstep << " Hessian step(s) reduced maxgrad from " <<
-    maxgrad0 << " to " << maxgrad2 << "." << endl <<
-    "All output files should be updated, but confirm as this is experimental still." << endl <<
-    "The fact this was successful gives strong evidence of convergence to a mode" << endl <<
-    "with quadratic log-likelihood surface." << endl;
+    }
+    // if(maxgrad2>maxgrad) {
+    //   // which ones got worse?
+    //   //int jj = 1;
+    //   // for (int i = 0; i < initial_params::num_initial_params; ++i) {
+    //   //   if (active(*initial_params::varsptr[i])) {
+    //   //     int jmax = (int)initial_params::varsptr[i]->size_count();
+    //   //     for (int j = 1; j <= jmax; ++j) {
+    //   //       if(abs(gr(jj)) < abs(gr2(jj))){
+    //   // 	       cout << (initial_params::varsptr[i])->label();
+    //   //         if (jmax > 1) {
+    //   // 		cout << "(" << j << ")";
+    //   //         }
+    //   // 	      cout << ": original grad=" << gr(jj) << " and updated grad=" << gr2(jj) << endl;
+    //   //       }
+    //   // 	    ++jj;
+    //   //     }
+    //   //   }
+    //   // }
+    // // cerr << "Hess step " << ii << ": Max gradient=" << maxgrad2 <<
+    // // 	" and min gradient= " << mingrad2 << endl << endl; 
+    // } 
+    //ad_exit(1);
+    // } else {
+    // Was successful but not good enough to break early
+    cout << "Hess step " << ii << ": Max gradient=" << maxgrad2 <<
+      " and min gradient= " << mingrad2 << endl; 
+    x=x2; gr=gr2; maxgrad=maxgrad2; 
+    // If not the last step then want to skip the sd_calcs
+    // which are slow so manually update admodel.cov whereas
+    // below computations1() runs everything so do that if last
+    // step
+    if(ii!=N_hess_steps){
+      int oldoutput = function_minimizer::output_flag;
+      function_minimizer::output_flag=0;
+      cout << " Updating Hessian for next step (output suppressed)...";
+      hess_routine(); // Calculate new Hessian
+      depvars_routine(); // calculate derivatives of sdreport variables
+      hess_inv(); // Invert Hess and write to admodel.cov
+      function_minimizer::output_flag=oldoutput;
+      cout << "done!" << endl;
+    }
+  } // end loop over hess_steps
+  
+  if(maxgrad2>maxgrad0){
+    // worse gradient
+    cerr << "Experimental feature -hess_step resulted in worse gradients." << endl <<  
+      "Consider reoptimizing model to reset files. This suggests that the" << endl <<
+      "the negative log-likelihood is not quadratic at the mode, or that the" << endl <<
+      "Hessian does not approximate it well. In short, it suggests the model is" << endl <<
+      "not converged. Check model structure and other output files to investigate" << endl <<
+      "potential causes and solutions." << endl;
+    ad_exit(1);
+  } else {
+    // Finished successully so run the opitmizer without
+    // taking any steps to produce updated output files
+    // with this new MLE value.
+    function_minimizer::maxfn=0;
+    cout << endl << "Redoing uncertainty calculations and updating files (output suppressed)...";
+    int oldoutput = function_minimizer::output_flag;
+    function_minimizer::output_flag=0;
+    computations1(ad_comm::argc,ad_comm::argv);
+    function_minimizer::output_flag=oldoutput;
+    cout << " done!" << endl;
+    cout << endl << "The " << Nstep << " Hessian step(s) reduced maxgrad from " <<
+      maxgrad0 << " to " << maxgrad2 << " and NLL by " << nll-nll2 << "." << endl <<
+      "All output files should be updated, but confirm as this is experimental still." << endl <<
+      "The fact this was successful gives strong evidence of convergence to a mode" << endl <<
+      "with quadratic log-likelihood surface." << endl;
+  }
 }
 
 // Quick epxerimental function to test whether MLE parameters are

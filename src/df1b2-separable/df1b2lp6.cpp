4ad68e2b576bec2b2ab807e593d5a94807c000f9
johnoel <johnoel@hawaii.edu>
2022-02-18 14:31:46 -1000

    Opps...missed this.

--- src/df1b2-separable/df1b2lp6.cpp
+++ src/df1b2-separable/df1b2lp6.cpp
@@ -91,7 +91,7 @@ void laplace_approximation_calculator::
     //cout << "norm(Hess) = " << norm(Hess) << endl;
     //cout << grad << endl;
     //check_pool_depths();
-    if (!initial_params::mc_phase && function_minimizer::output_flag==2)
+    if (!initial_params::mc_phase)
     {
       std::ostream& output_stream = get_output_stream();
       output_stream << "Newton raphson " << ii << "  ";

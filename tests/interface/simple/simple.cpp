d4afdf59f6fc1ea5b45f32ab68b828135c28e280
johnoel <johnoel@hawaii.edu>
2022-04-20 10:07:58 -1000

    Add header files.

--- tests/interface/simple/simple.cpp
+++ tests/interface/simple/simple.cpp
@@ -1,28 +1,4 @@
-#include "model_parameters.h"
-
-class minimizer: public model_parameters
-{
-  std::function<void(void)> f;
-
-public:
-  minimizer() = delete;
-  minimizer(const minimizer&) = delete;
-  minimizer(const minimizer&&) = delete;
-
-  minimizer(int argc, char * argv[]):
-    model_parameters(argc, argv)
-  {
-  }
-  void minimize(std::function<void(void)> user_function)
-  {
-    f = std::move(user_function);
-    model_parameters::minimize(ad_comm::argc, ad_comm::argv);
-  }
-  void userfunction()
-  {
-    f();
-  }
-};
+#include "admb.h"
 
 int main(int argc,char * argv[])
 {

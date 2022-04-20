e1659049243bc714d05c35347b191bd8071698be
johnoel <johnoel@hawaii.edu>
2022-04-20 12:30:52 -1000

    Add output.

--- tests/interface/simple/simple.cpp
+++ tests/interface/simple/simple.cpp
@@ -2,8 +2,8 @@
 
 int main(int argc,char * argv[])
 {
-  dvector x("{ -1,  0,  1,  2,  3,  4,  5,  6,  7,  8 }");
-  dvector y("{ 1.4,  4.7,  5.1,  8.3,  9.0,  14.5,  14.0,  13.4,  19.2,  18.0 }");
+  dvector x("{ -1, 0, 1, 2, 3, 4, 5, 6, 7, 8 }");
+  dvector y("{ 1.4, 4.7, 5.1, 8.3, 9.0, 14.5, 14.0, 13.4, 19.2, 18.0 }");
 
   minimizer m(argc,argv);
 
@@ -16,6 +16,10 @@ int main(int argc,char * argv[])
     auto yhat = b0 + b1 * x;
     f = regression(y, yhat);
   });
+
+  cout << "b0: " << value(b0) << endl;
+  cout << "b1: " << value(b1) << endl;
+  cout << "f: " << value(f) << endl;
 
   return 0;
 }

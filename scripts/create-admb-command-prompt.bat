161af1ecaa503415262688a3955d3beb62f683d8
johnoel@hawaii.edu <johnoel@hawaii.edu>
2022-07-06 14:15:03 -1000

    Issue #260 - Use full path in shortcuts.

--- scripts/create-admb-command-prompt.bat
+++ scripts/create-admb-command-prompt.bat
@@ -1,1 +1,9 @@
-..\..\scripts\create-admb-command-prompt.vbs %CD%
+@echo off
+
+if exist "build\admb\bin\admb.cmd" (
+  scripts\create-admb-command-prompt.vbs %CD%\build\admb
+) else (
+  if exist "bin\admb.cmd" (
+    ..\..\scripts\create-admb-command-prompt.vbs %CD%
+  )
+)

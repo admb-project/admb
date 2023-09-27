47839750e83fbfdede6becbc7d233889e4a9d1d3
johnoel <johnoel@hawaii.edu>
2023-09-26 20:26:20 -1000

    Make same as unix.

--- scripts/get-outputs.sh
+++ scripts/get-outputs.sh
@@ -4,9 +4,9 @@ TARGET=$1
 
 echo "# target: ${TARGET}"
 
-if [ -e ${TARGET}.std ]; then cat ${TARGET}.std; fi
 if [ -e ${TARGET}.par ]; then cat ${TARGET}.par; fi
 if [ -e ${TARGET}.p01 ]; then cat ${TARGET}.p01; fi
+if [ -e ${TARGET}.std ]; then cat ${TARGET}.std; fi
 if [ -e ${TARGET}.cor ]; then cat ${TARGET}.cor; fi
 if [ -e ${TARGET}.eva ]; then cat ${TARGET}.eva; fi
 if [ -e ${TARGET}.prf ]; then cat ${TARGET}.prf; fi

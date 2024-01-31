4cbc33e345d0d5f0b75d918350c143388423dd13
johnoel <johnoel@hawaii.edu>
2024-01-30 19:15:07 -1000

    Fix MSVC Conversion error.

--- tests/gtests/test_option_match.cpp
+++ tests/gtests/test_option_match.cpp
@@ -6,7 +6,7 @@ class test_option_match: public ::testing::Test {};
 TEST_F(test_option_match, option_negative)
 {
   int argc = 3;
-  char* argv[] = { "./simple", "-option", "-1"};
+  const char* argv[3] = { "./simple", "-option", "-1"};
 
   int nopt = 0;
 
@@ -30,20 +30,20 @@ TEST_F(test_option_match, calls)
   ASSERT_EQ(-1, option_match(1, 0, "-check", nopt));
 
   int argc = 1;
-  char* argv[] = { "./simple"};
+  const char* argv[1] = { "./simple"};
 
   char* option = "-none";
   ASSERT_EQ(-1, option_match(argc, argv, option, nopt));
   ASSERT_EQ(0, nopt);
 
   int argc2 = 2;
-  char* argv2[] = { "./simple", "-myoption" };
+  const char* argv2[2] = { "./simple", "-myoption" };
   char* myoption = "-myoption";
   ASSERT_EQ(1, option_match(argc2, argv2, myoption, nopt));
   ASSERT_EQ(0, nopt);
 
   int argc3 = 3;
-  char* argv3[] = { "./simple", "\0", "-myoption" };
+  const char* argv3[3] = { "./simple", "\0", "-myoption" };
   char* option3 = "-myoption";
   ASSERT_EQ(2, option_match(argc3, argv3, option3, nopt));
   ASSERT_EQ(0, nopt);

e9ac611ae3580cd021d24a280730afa5e64a4b37
johnoel <johnoel@hawaii.edu>
2024-02-28 09:11:29 -1000

    Update for admb-13.2

--- scripts/zipadmb.ps1
+++ scripts/zipadmb.ps1
@@ -1,1 +1,1 @@
-Compress-Archive -Path admb-13.1 -CompressionLevel Fastest -DestinationPath admb-13.1-windows.zip
+Compress-Archive -Path admb-13.2 -CompressionLevel Fastest -DestinationPath admb-13.2-windows.zip
